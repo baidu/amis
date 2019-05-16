7dfeda1bb6a4ff3b2e796b029cc976b5e973aee6
catchonme <catchonme@163.com>
2019-05-16 16:54:36 +0800

    代码修正

--- mock/crud/list.js
+++ mock/crud/list.js
@@ -23,7 +23,7 @@ module.exports = function(req, res) {
                 list: repeat(() => ({
                     title: '{{name.title}}',
                     description: '{{lorem.words}}'
-                }), Math.round(Math.random() * 3)),
+                }), Math.round(Math.random() * 10)),
                 carousel: [
                     {
                         image: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3893101144,2877209892&fm=23&gp=0.jpg'
