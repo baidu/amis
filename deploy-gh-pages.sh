ce1d2bfa5934450f48f7094f9f08dad9e1c7d172
liaoxuezhi <liaoxuezhi@baidu.com>
2019-06-05 10:45:36 +0800

    添加 gh-pages 更新脚本

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+if [ -z "$(git status --porcelain)" ]; then 
+  # Working directory clean
+  echo "Working directory clean"
+else 
+  # Uncommitted changes
+  echo "Skiped: You got uncommitted changes"
+  exit
+fi
+
+rm -rf gh-pages
+
+fis3 release gh-pages -c
+
+node ./build/uplaod2cdn.js $2
+
+git add gh-pages -f
+
+git commit -m "更新 gh-pages"
+
+git push
+
+git subtree push --prefix gh-pages origin gh-pages
+
+git commit -m 'rebuild pages' --allow-empty
+
+git push origin
+
+echo "done"
