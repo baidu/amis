82a49b2cfdf7b673856de9eab9c473513d74a6ca
catchonme <catchonme@163.com>
2019-06-18 09:50:05 +0800

    panel 优化affixFooter，初始加载数据后，固定底部

---
+++ __tests__/renderers/Alert.test.tsx
@@ -0,0 +1,28 @@
+import React = require('react');
+import {render} from 'react-testing-library';
+import '../../src/themes/default';
+import {
+    render as amisRender
+} from '../../src/index';
+import { makeEnv } from '../helper';
+
+test('Renderer:alert', () => {
+    const {
+        container
+    } = render(amisRender({
+        type: 'page',
+        body: {
+            type: 'alert',
+            level: 'success',
+            body: [
+                {
+                    type: 'tpl',
+                    tpl: '提示内容'
+                }
+            ]
+        }
+    }, {}, makeEnv({
+    })));
+
+    expect(container).toMatchSnapshot();
+});
