63cbffbe6a42831acc4db3c6d775de445765fa12
liaoxuezhi <liaoxuezhi@baidu.com>
2019-06-28 10:11:06 +0800

    有修改内容时,可以按 y 继续执行

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -6,8 +6,13 @@ if [ -z "$(git status --porcelain)" ]; then
   echo "Working directory clean"
 else 
   # Uncommitted changes
-  echo "Skiped: You got uncommitted changes"
-  exit
+  read -p "You got uncommitted changes, press y to continue? " -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+    echo "Skiped!"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+  fi
 fi
 
 rm -rf gh-pages
