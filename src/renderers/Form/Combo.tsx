5c39d6bfc8b4dc7cc5500b73c467fc23b612a5d2
liaoxuezhi <liaoxuezhi@baidu.com>
2019-07-11 16:45:31 +0800

    修复 validateOnChange 在 combo 中无效的问题

--- src/renderers/Form/Combo.tsx
+++ src/renderers/Form/Combo.tsx
@@ -270,7 +270,8 @@ export default class ComboControl extends React.Component<ComboProps> {
             store,
             joinValues,
             delimiter,
-            disabled
+            disabled,
+            validateOnChange
         } = this.props;
 
         if (disabled) {
@@ -286,7 +287,7 @@ export default class ComboControl extends React.Component<ComboProps> {
 
         this.props.onChange(value);
 
-        if (formItem && formItem.validated) {
+        if (validateOnChange !== false && formItem && formItem.validated) {
             this.subForms.forEach(item => item.validate());
         }
 
