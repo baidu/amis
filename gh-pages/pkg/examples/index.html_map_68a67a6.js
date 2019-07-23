f882138f43eddaeffae09b2cfe525e4d0910814b
liaoxuezhi <liaoxuezhi@baidu.com>
2019-07-23 16:17:39 +0800

    更新 gh-pages

--- gh-pages/pkg/examples/index.html_map_68a67a6.js
+++ gh-pages/pkg/examples/index.html_map_68a67a6.js
@@ -4845,7 +4845,7 @@ require.resourceMap({
       "type": "js"
     },
     "docs/renderers/Types.md": {
-      "url": "https://bce.bdstatic.com/fex/amis-gh-pages/docs/renderers/Types_8761cbc.js",
+      "url": "https://bce.bdstatic.com/fex/amis-gh-pages/docs/renderers/Types_332ad75.js",
       "type": "js"
     },
     "docs/sdk.md": {
