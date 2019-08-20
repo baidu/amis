8c47e847071995bcaef25021c28f4df76660ea42
liaoxuezhi <liaoxuezhi@baidu.com>
2019-08-20 10:04:50 +0800

    ts 修复

--- src/types.ts
+++ src/types.ts
@@ -124,7 +124,7 @@ export interface JSONSchema {
     [propsName: string]: any;
 };
 
-export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export type Omit<T, K extends keyof T & any> = Pick<T, Exclude<keyof T, K>>;
 export type Override<M, N> = Omit<M, Extract<keyof M, keyof N>> & N;
 export type ExtractProps<TComponentOrTProps> = TComponentOrTProps extends React.ComponentType<
   infer P
