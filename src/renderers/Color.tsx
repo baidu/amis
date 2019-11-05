eb860068de1408ecf422634ec37f7db9068fb1f9
liaoxuezhi <liaoxuezhi@baidu.com>
2019-11-05 18:04:27 +0800

    添加颜色预览控件

---
+++ src/renderers/Color.tsx
@@ -0,0 +1,38 @@
+/**
+ * @file 用来展示颜色块。
+ */
+import React from 'react';
+import {Renderer, RendererProps} from '../factory';
+import {resolveVariableAndFilter} from '../utils/tpl-builtin';
+
+export interface ColorProps extends RendererProps {
+    className: string;
+    defaultColor: string;
+    showValue: boolean;
+}
+
+export class ColorField extends React.Component<ColorProps, object> {
+    static defaultProps = {
+        className: '',
+        defaultColor: '#ccc',
+        showValue: true
+    };
+
+    render() {
+        const {className, data, classnames: cx, name, value, defaultColor, showValue} = this.props;
+        const color = value || (name ? resolveVariableAndFilter(name, data, '| raw') : null);
+
+        return (
+            <div className={cx('ColorField', className)}>
+                <i className={cx('ColorField-previewIcon')} style={{backgroundColor: color || defaultColor}} />
+                {showValue ? <span className={cx('ColorField-value')}>{color}</span> : null}
+            </div>
+        );
+    }
+}
+
+@Renderer({
+    test: /(^|\/)color$/,
+    name: 'color'
+})
+export class ColorFieldRenderer extends ColorField {}
