c83290e22dbe8f46c5eff6db68e91912ca32134e
2betop <2betop.cn@gmail.com>
2020-01-21 11:18:11 +0800

    sdk 用法 worker 加载问题修复

--- fis-conf.js
+++ fis-conf.js
@@ -410,6 +410,40 @@ if (fis.project.currentMedia() === 'publish') {
     isMod: false
   });
 
+  env.match('::packager', {
+    prepackager: [
+      fis.plugin('stand-alone-pack', {
+        '/pkg/editor.worker.js': 'monaco-editor/esm/vs/editor/editor.worker.js',
+        '/pkg/json.worker.js': 'monaco-editor/esm/vs/language/json/json.worker',
+        '/pkg/css.worker.js': 'monaco-editor/esm/vs/language/css/css.worker',
+        '/pkg/html.worker.js': 'monaco-editor/esm/vs/language/html/html.worker',
+        '/pkg/ts.worker.js':
+          'monaco-editor/esm/vs/language/typescript/ts.worker',
+
+        // 替换这些文件里面的路径引用。
+        // 如果不配置，源码中对于打包文件的引用是不正确的。
+        'replaceFiles': ['src/components/Editor.tsx']
+      }),
+      function(ret) {
+        const root = fis.project.getProjectPath();
+        [
+          '/pkg/editor.worker.js',
+          '/pkg/json.worker.js',
+          '/pkg/css.worker.js',
+          '/pkg/html.worker.js',
+          '/pkg/ts.worker.js'
+        ].forEach(function(pkgFile) {
+          const packedFile = fis.file.wrap(path.join(root, pkgFile));
+          const file = ret.pkg[packedFile.subpath];
+          let contents = file.getContent();
+
+          contents = contents.replace(/amis\.define/g, 'define');
+          file.setContent(contents);
+        });
+      }
+    ]
+  });
+
   env.match('*', {
     domain: '.',
     deploy: [
