6d1ec3d34556393c136e375564db85aa815ebf76
rickcole <rickcole21@outlook.com>
2020-04-10 10:04:21 +0800

    fix

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -26,7 +26,7 @@ export interface ValidateFn {
 export const validations: {
   [propsName: string]: ValidateFn;
 } = {
-  isRequired: function (values, value: any) {
+  isRequired: function(values, value: any) {
     return (
       value !== undefined &&
       value !== '' &&
@@ -34,113 +34,113 @@ export const validations: {
       (!Array.isArray(value) || !!value.length)
     );
   },
-  isExisty: function (values, value) {
+  isExisty: function(values, value) {
     return isExisty(value);
   },
-  matchRegexp: function (values, value, regexp) {
+  matchRegexp: function(values, value, regexp) {
     return !isExisty(value) || isEmpty(value) || makeRegexp(regexp).test(value);
   },
-  isUndefined: function (values, value) {
+  isUndefined: function(values, value) {
     return value === undefined;
   },
-  isEmptyString: function (values, value) {
+  isEmptyString: function(values, value) {
     return isEmpty(value);
   },
-  isEmail: function (values, value) {
+  isEmail: function(values, value) {
     return validations.matchRegexp(
       values,
       value,
       /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i
     );
   },
-  isUrl: function (values, value) {
+  isUrl: function(values, value) {
     return validations.matchRegexp(
       values,
       value,
       /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i
     );
   },
-  isTrue: function (values, value) {
+  isTrue: function(values, value) {
     return value === true;
   },
-  isFalse: function (values, value) {
+  isFalse: function(values, value) {
     return value === false;
   },
-  isNumeric: function (values, value) {
+  isNumeric: function(values, value) {
     if (typeof value === 'number') {
       return true;
     }
     return validations.matchRegexp(values, value, /^[-+]?(?:\d*[.])?\d+$/);
   },
-  isAlpha: function (values, value) {
+  isAlpha: function(values, value) {
     return validations.matchRegexp(values, value, /^[A-Z]+$/i);
   },
-  isAlphanumeric: function (values, value) {
+  isAlphanumeric: function(values, value) {
     return validations.matchRegexp(values, value, /^[0-9A-Z]+$/i);
   },
-  isInt: function (values, value) {
+  isInt: function(values, value) {
     return validations.matchRegexp(values, value, /^(?:[-+]?(?:0|[1-9]\d*))$/);
   },
-  isFloat: function (values, value) {
+  isFloat: function(values, value) {
     return validations.matchRegexp(
       values,
       value,
       /^(?:[-+]?(?:\d+))?(?:\.\d*)?(?:[eE][\+\-]?(?:\d+))?$/
     );
   },
-  isWords: function (values, value) {
+  isWords: function(values, value) {
     return validations.matchRegexp(values, value, /^[A-Z\s]+$/i);
   },
-  isSpecialWords: function (values, value) {
+  isSpecialWords: function(values, value) {
     return validations.matchRegexp(values, value, /^[A-Z\s\u00C0-\u017F]+$/i);
   },
-  isLength: function (values, value, length) {
+  isLength: function(values, value, length) {
     return !isExisty(value) || isEmpty(value) || value.length === length;
   },
-  equals: function (values, value, eql) {
+  equals: function(values, value, eql) {
     return !isExisty(value) || isEmpty(value) || value == eql;
   },
-  equalsField: function (values, value, field) {
+  equalsField: function(values, value, field) {
     return value == values[field];
   },
-  maxLength: function (values, value, length) {
+  maxLength: function(values, value, length) {
     return !isExisty(value) || value.length <= length;
   },
-  minLength: function (values, value, length) {
+  minLength: function(values, value, length) {
     return !isExisty(value) || isEmpty(value) || value.length >= length;
   },
-  isUrlPath: function (values, value, regexp) {
+  isUrlPath: function(values, value, regexp) {
     return !isExisty(value) || isEmpty(value) || /^[a-z0-9_\\-]+$/i.test(value);
   },
-  maximum: function (values, value, maximum) {
+  maximum: function(values, value, maximum) {
     return (
       !isExisty(value) ||
       isEmpty(value) ||
       (parseFloat(value) || 0) <= (parseFloat(maximum) || 0)
     );
   },
-  lt: function (values, value, maximum) {
+  lt: function(values, value, maximum) {
     return (
       !isExisty(value) ||
       isEmpty(value) ||
       (parseFloat(value) || 0) < (parseFloat(maximum) || 0)
     );
   },
-  minimum: function (values, value, minimum) {
+  minimum: function(values, value, minimum) {
     return (
       !isExisty(value) ||
       isEmpty(value) ||
       (parseFloat(value) || 0) >= (parseFloat(minimum) || 0)
     );
   },
-  gt: function (values, value, minimum) {
+  gt: function(values, value, minimum) {
     return (
       !isExisty(value) ||
       isEmpty(value) ||
       (parseFloat(value) || 0) > (parseFloat(minimum) || 0)
     );
   },
-  isJson: function (values, value, minimum) {
+  isJson: function(values, value, minimum) {
     if (isExisty(value) && !isEmpty(value)) {
       try {
         JSON.parse(value);
@@ -150,24 +150,24 @@ export const validations: {
     }
     return true;
   },
-  isPhoneNumber: function (values, value) {
+  isPhoneNumber: function(values, value) {
     return (
       !isExisty(value) || isEmpty(value) || /^[1]([3-9])[0-9]{9}$/.test(value)
     );
   },
-  isTelNumber: function (values, value) {
+  isTelNumber: function(values, value) {
     return (
       !isExisty(value) ||
       isEmpty(value) ||
       /^(\(\d{3,4}\)|\d{3,4}-|\s)?\d{7,14}$/.test(value)
     );
   },
-  isZipcode: function (values, value) {
+  isZipcode: function(values, value) {
     return (
       !isExisty(value) || isEmpty(value) || /^[1-9]{1}(\d+){5}$/.test(value)
     );
   },
-  isId: function (values, value) {
+  isId: function(values, value) {
     return (
       !isExisty(value) ||
       isEmpty(value) ||
@@ -176,34 +176,34 @@ export const validations: {
       )
     );
   },
-  notEmptyString: function (values, value) {
+  notEmptyString: function(values, value) {
     return !isExisty(value) || !(String(value) && String(value).trim() === '');
   },
-  matchRegexp1: function (values, value, regexp) {
+  matchRegexp1: function(values, value, regexp) {
     return validations.matchRegexp(values, value, regexp);
   },
-  matchRegexp2: function (values, value, regexp) {
+  matchRegexp2: function(values, value, regexp) {
     return validations.matchRegexp(values, value, regexp);
   },
-  matchRegexp3: function (values, value, regexp) {
+  matchRegexp3: function(values, value, regexp) {
     return validations.matchRegexp(values, value, regexp);
   },
-  matchRegexp4: function (values, value, regexp) {
+  matchRegexp4: function(values, value, regexp) {
     return validations.matchRegexp(values, value, regexp);
   },
-  matchRegexp5: function (values, value, regexp) {
+  matchRegexp5: function(values, value, regexp) {
     return validations.matchRegexp(values, value, regexp);
   },
-  matchRegexp6: function (values, value, regexp) {
+  matchRegexp6: function(values, value, regexp) {
     return validations.matchRegexp(values, value, regexp);
   },
-  matchRegexp7: function (values, value, regexp) {
+  matchRegexp7: function(values, value, regexp) {
     return validations.matchRegexp(values, value, regexp);
   },
-  matchRegexp8: function (values, value, regexp) {
+  matchRegexp8: function(values, value, regexp) {
     return validations.matchRegexp(values, value, regexp);
   },
-  matchRegexp9: function (values, value, regexp) {
+  matchRegexp9: function(values, value, regexp) {
     return validations.matchRegexp(values, value, regexp);
   }
 };
@@ -285,7 +285,7 @@ export function validate(
   return errors;
 }
 
-const splitValidations = function (str: string): Array<string> {
+const splitValidations = function(str: string): Array<string> {
   let i = 0;
   const placeholder: {[propName: string]: string} = {};
 
@@ -303,33 +303,33 @@ export function str2rules(
 ): {[propName: string]: any} {
   if (typeof validations === 'string') {
     return validations
-      ? splitValidations(validations).reduce(function (
-        validations: {[propName: string]: any},
-        validation
-      ) {
-        const idx = validation.indexOf(':');
-        let validateMethod = validation;
-        let args = [];
+      ? splitValidations(validations).reduce(function(
+          validations: {[propName: string]: any},
+          validation
+        ) {
+          const idx = validation.indexOf(':');
+          let validateMethod = validation;
+          let args = [];
 
-        if (~idx) {
-          validateMethod = validation.substring(0, idx);
-          args = /^matchRegexp/.test(validateMethod)
-            ? [validation.substring(idx + 1).trim()]
-            : validation
-              .substring(idx + 1)
-              .split(',')
-              .map(function (arg) {
-                try {
-                  return JSON.parse(arg);
-                } catch (e) {
-                  return arg;
-                }
-              });
-        }
+          if (~idx) {
+            validateMethod = validation.substring(0, idx);
+            args = /^matchRegexp/.test(validateMethod)
+              ? [validation.substring(idx + 1).trim()]
+              : validation
+                  .substring(idx + 1)
+                  .split(',')
+                  .map(function(arg) {
+                    try {
+                      return JSON.parse(arg);
+                    } catch (e) {
+                      return arg;
+                    }
+                  });
+          }
 
-        validations[validateMethod] = args.length ? args : true;
-        return validations;
-      },
+          validations[validateMethod] = args.length ? args : true;
+          return validations;
+        },
         {})
       : {};
   }
