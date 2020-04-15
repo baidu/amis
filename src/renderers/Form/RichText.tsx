bb27f6c6c90d23f2c1961302f5673b18a39e2efb
2betop <2betop.cn@gmail.com>
2020-04-15 16:04:08 +0800

    集成 tinymce 为默认 rich-text

--- src/renderers/Form/RichText.tsx
+++ src/renderers/Form/RichText.tsx
@@ -6,14 +6,22 @@ import {noop} from '../../utils/helper';
 
 export interface RichTextProps extends FormControlProps {
   options?: any;
+  vendor?: 'froala' | 'tinymce';
 }
 
-function loadComponent(): Promise<React.ReactType> {
-  return new Promise(resolve =>
-    (require as any)(['../../components/RichText'], (component: any) =>
-      resolve(component.default)
-    )
-  );
+function loadRichText(
+  type: 'tinymce' | 'froala' = 'froala'
+): () => Promise<React.ReactType> {
+  return () =>
+    new Promise(resolve =>
+      type === 'tinymce'
+        ? (require as any)(['../../components/Tinymce'], (component: any) =>
+            resolve(component.default)
+          )
+        : (require as any)(['../../components/RichText'], (component: any) =>
+            resolve(component.default)
+          )
+    );
 }
 
 export default class RichTextControl extends React.Component<
@@ -106,53 +114,78 @@ export default class RichTextControl extends React.Component<
   constructor(props: RichTextProps) {
     super(props);
 
+    const finnalVendor =
+      props.vendor || (props.env.richTextToken ? 'froala' : 'tinymce');
     this.handleFocus = this.handleFocus.bind(this);
     this.handleBlur = this.handleBlur.bind(this);
 
-    this.config = {
-      imageAllowedTypes: ['jpeg', 'jpg', 'png', 'gif'],
-      imageDefaultAlign: 'left',
-      imageEditButtons: props.imageEditable
-        ? [
-            'imageReplace',
-            'imageAlign',
-            'imageRemove',
-            '|',
-            'imageLink',
-            'linkOpen',
-            'linkEdit',
-            'linkRemove',
-            '-',
-            'imageDisplay',
-            'imageStyle',
-            'imageAlt',
-            'imageSize'
-          ]
-        : [],
-      key: props.env.richTextToken,
-      ...props.options,
-      editorClass: props.editorClass,
-      placeholderText: props.placeholder,
-      imageUploadURL: props.reciever,
-      imageUploadParams: {
-        from: 'rich-text'
-      },
-      videoUploadURL: props.videoReciever,
-      videoUploadParams: {
-        from: 'rich-text'
-      },
-      events: {
-        ...(props.options && props.options.events),
-        'froalaEditor.focus': this.handleFocus,
-        'froalaEditor.blur': this.handleBlur
-      }
-    };
+    if (finnalVendor === 'froala') {
+      this.config = {
+        imageAllowedTypes: ['jpeg', 'jpg', 'png', 'gif'],
+        imageDefaultAlign: 'left',
+        imageEditButtons: props.imageEditable
+          ? [
+              'imageReplace',
+              'imageAlign',
+              'imageRemove',
+              '|',
+              'imageLink',
+              'linkOpen',
+              'linkEdit',
+              'linkRemove',
+              '-',
+              'imageDisplay',
+              'imageStyle',
+              'imageAlt',
+              'imageSize'
+            ]
+          : [],
+        key: props.env.richTextToken,
+        ...props.options,
+        editorClass: props.editorClass,
+        placeholderText: props.placeholder,
+        imageUploadURL: props.reciever,
+        imageUploadParams: {
+          from: 'rich-text'
+        },
+        videoUploadURL: props.videoReciever,
+        videoUploadParams: {
+          from: 'rich-text'
+        },
+        events: {
+          ...(props.options && props.options.events),
+          'froalaEditor.focus': this.handleFocus,
+          'froalaEditor.blur': this.handleBlur
+        }
+      };
 
-    if (props.buttons) {
-      this.config.toolbarButtonsSM = props.buttons;
-      this.config.toolbarButtonsMD = props.buttons;
-      this.config.toolbarButtonsXS = props.buttons;
-      this.config.toolbarButtons = props.buttons;
+      if (props.buttons) {
+        this.config.toolbarButtonsSM = props.buttons;
+        this.config.toolbarButtonsMD = props.buttons;
+        this.config.toolbarButtonsXS = props.buttons;
+        this.config.toolbarButtons = props.buttons;
+      }
+    } else {
+      const fetcher = props.env.fetcher;
+      this.config = {
+        images_upload_url: props.reciever,
+        images_upload_handler: async (
+          blobInfo: any,
+          ok: (locaiton: string) => void,
+          fail: (reason: string) => void
+        ) => {
+          const formData = new FormData();
+          formData.append('file', blobInfo.blob(), blobInfo.filename());
+          try {
+            const response = await fetcher(props.reciever, formData);
+            if (response.ok) {
+              ok(response.data?.link || response.data?.value || (response as any).link);
+            }
+          } catch (e) {
+            fail(e);
+          }
+        }
+      };
     }
   }
 
@@ -175,8 +208,12 @@ export default class RichTextControl extends React.Component<
       value,
       onChange,
       disabled,
-      size
+      size,
+      vendor,
+      env
     } = this.props;
+
+    const finnalVendor = vendor || (env.richTextToken ? 'froala' : 'tinymce');
 
     return (
       <div
@@ -186,7 +223,7 @@ export default class RichTextControl extends React.Component<
         })}
       >
         <LazyComponent
-          getComponent={loadComponent}
+          getComponent={loadRichText(finnalVendor)}
           model={value}
           onModelChange={disabled ? noop : onChange}
           onFocus={this.handleFocus}
