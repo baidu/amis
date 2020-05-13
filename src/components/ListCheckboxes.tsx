a47ba7c603ad1c01b3a8920d346324981c12155d
2betop <2betop.cn@gmail.com>
2020-05-13 12:35:21 +0800

    ListCheckboxes 支持 Group

--- src/components/ListCheckboxes.tsx
+++ src/components/ListCheckboxes.tsx
@@ -3,49 +3,80 @@ import {themeable} from '../theme';
 import React from 'react';
 import uncontrollable from 'uncontrollable';
 import Checkbox from './Checkbox';
+import {Option} from './Select';
 
 export class ListCheckboxes extends Checkboxes {
+  valueArray: Array<Option>;
+
+  renderOption(option: Option, index: number) {
+    const {
+      labelClassName,
+      disabled,
+      classnames: cx,
+      itemClassName,
+      itemRender
+    } = this.props;
+    const valueArray = this.valueArray;
+
+    if (Array.isArray(option.children)) {
+      return (
+        <div
+          key={index}
+          className={cx('ListCheckboxes-group', option.className)}
+        >
+          <div className={cx('ListCheckboxes-itemLabel')}>
+            {itemRender(option)}
+          </div>
+
+          <div className={cx('ListCheckboxes-items', option.className)}>
+            {option.children.map((child, index) =>
+              this.renderOption(child, index)
+            )}
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <div
+        key={index}
+        className={cx(
+          'ListCheckboxes-item',
+          itemClassName,
+          option.className,
+          disabled || option.disabled ? 'is-disabled' : ''
+        )}
+        onClick={() => this.toggleOption(option)}
+      >
+        <div className={cx('ListCheckboxes-itemLabel')}>
+          {itemRender(option)}
+        </div>
+
+        <Checkbox
+          checked={!!~valueArray.indexOf(option)}
+          disabled={disabled || option.disabled}
+          labelClassName={labelClassName}
+          description={option.description}
+        />
+      </div>
+    );
+  }
+
   render() {
     const {
       value,
       options,
       className,
       placeholder,
-      labelClassName,
-      disabled,
       classnames: cx,
-      option2value,
-      itemClassName,
-      itemRender
+      option2value
     } = this.props;
 
-    let valueArray = Checkboxes.value2array(value, options, option2value);
+    this.valueArray = Checkboxes.value2array(value, options, option2value);
     let body: Array<React.ReactNode> = [];
 
     if (Array.isArray(options) && options.length) {
-      body = options.map((option, key) => (
-        <div
-          key={key}
-          className={cx(
-            'ListCheckboxes-item',
-            itemClassName,
-            option.className,
-            disabled || option.disabled ? 'is-disabled' : ''
-          )}
-          onClick={() => this.toggleOption(option)}
-        >
-          <div className={cx('ListCheckboxes-itemLabel')}>
-            {itemRender(option)}
-          </div>
-
-          <Checkbox
-            checked={!!~valueArray.indexOf(option)}
-            disabled={disabled || option.disabled}
-            labelClassName={labelClassName}
-            description={option.description}
-          />
-        </div>
-      ));
+      body = options.map((option, key) => this.renderOption(option, key));
     }
 
     return (
