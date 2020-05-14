c77f8c5ff212974040b112b6673f72a2e4dc2f36
2betop <2betop.cn@gmail.com>
2020-05-14 20:47:05 +0800

    添加 ResultBox 组件

--- examples/components/Page/Form.jsx
+++ examples/components/Page/Form.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import ResultBox from '../../../src/components/ResultBox';
+
 export default {
   type: 'page',
   title: '表单页面',
@@ -17,6 +20,38 @@ export default {
         label: 'Email',
         type: 'email',
         name: 'email'
+      },
+
+      {
+        name: 'checkboxes',
+        type: 'checkboxes',
+        joinValues: false,
+        options: [
+          {
+            label: '张学友',
+            value: 'a'
+          },
+          {
+            label: '刘德华',
+            value: 'b'
+          },
+          {
+            label: '黎明',
+            value: 'c'
+          },
+          {
+            label: '郭富城',
+            value: 'd'
+          }
+        ]
+      },
+
+      {
+        label: 'Result',
+        name: 'checkboxes',
+        component: ({value, onChange}) => {
+          return <ResultBox value={value} onChange={onChange} allowInput />;
+        }
       }
     ]
   }
