1da842fde581c39e8140e2631586ec9aa751dacf
2betop <2betop.cn@gmail.com>
2020-05-28 20:28:11 +0800

    添加 AssocicatedCheckboxes 草稿

---
+++ src/components/AssociatedCheckboxes.tsx
@@ -0,0 +1,17 @@
+/**
+ * 关联多选框，仅支持两层关联选择。
+ * 左边先点选，然后右边再次点选。
+ * 可以满足，先从 tree 中选中一个元素，然后查出来一个列表再次勾选。
+ */
+
+import React from 'react';
+
+export interface AssociatedCheckboxesProps {}
+
+export class AssociatedCheckboxes extends React.Component<
+  AssociatedCheckboxesProps
+> {
+  render() {
+    return <div>todo</div>;
+  }
+}
