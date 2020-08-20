42d8a971de5b59679bfbb3495960d4dd938d93ef
wuduoyi <wuduoyi@baidu.com>
2020-08-20 23:56:28 +0800

    实现简易前端文档检索

--- examples/components/DocSearch.jsx
+++ examples/components/DocSearch.jsx
@@ -1,47 +1,79 @@
+/**
+ * @file 实现前端文档搜索
+ */
 import React from 'react';
-import makeSchemaRenderer from './SchemaRender';
-
-const FormComponent = makeSchemaRenderer({
-  type: 'form',
-  mode: 'inline',
-  wrapWithPanel: false,
-  className: ':Doc-search',
-  controls: [
-    {
-      type: 'input-group',
-      size: 'sm',
-      controls: [
-        {
-          type: 'icon',
-          addOnclassName: 'no-bg no-border p-r-none p-l',
-          className: 'text-sm',
-          icon: 'search',
-          vendor: 'iconfont'
-        },
-        {
-          type: 'text',
-          placeholder: '搜索...',
-          inputClassName: 'no-border',
-          name: 'docsearch'
-        }
-      ]
-    }
-  ]
-});
+import Axios from 'axios';
+import SearchBox from '../../src/components/SearchBox';
 
 export default class DocSearch extends React.Component {
+  docs;
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [],
+      loadError: false
+    };
+    this.onSearch = this.onSearch.bind(this);
+    this.onSearchCancel = this.onSearchCancel.bind(this);
+  }
   componentDidMount() {
-    // const inputSelector = 'input[name="docsearch"]';
-    // docsearch({
-    //   appId: 'S08MJHBHFJ',
-    //   apiKey: '5fba814bb773d08b5d2a3f6074f926a5',
-    //   indexName: 'gh_pages',
-    //   inputSelector,
-    //   debug: false
-    // });
+    Axios.get('/docs/docs.json')
+      .then(result => {
+        this.docs = result.data.docs;
+      })
+      .catch(err => {
+        this.setState({loadError: true});
+      });
+  }
+
+  onSearch(query) {
+    if (query === '') {
+      this.setState({searchResults: []});
+      return;
+    }
+    let results = [];
+    for (let doc of this.docs) {
+      let index = doc.body.indexOf(query);
+
+      if (index !== -1) {
+        results.push({
+          title: doc.title,
+          path: doc.path,
+          abstract: doc.body
+            .substring(Math.max(0, index - 20), index + 60)
+            .replace(query, `<strong>${query}</strong>`)
+        });
+      } else if (doc.title.indexOf(query) !== -1) {
+        results.push({
+          title: doc.title,
+          path: doc.path,
+          abstract: ''
+        });
+      }
+    }
+    this.setState({searchResults: results});
+  }
+
+  onSearchCancel() {
+    this.setState({searchResults: []});
   }
 
   render() {
-    return <FormComponent showCode={false} theme={this.props.theme} />;
+    const searchResults = this.state.searchResults;
+    return (
+      <div className="p-l p-t">
+        <SearchBox onSearch={this.onSearch} onCancel={this.onSearchCancel} />
+        <div className="search-result">
+          {searchResults.map(item => {
+            return (
+              <a href={'/' + item.path} key={`list_${item.path}`}>
+                <h5>{item.title}</h5>
+                <p dangerouslySetInnerHTML={{__html: item.abstract}} />
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    );
   }
 }
