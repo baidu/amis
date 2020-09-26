c9b4d13f1f2533e24a7a8ace44b83fdc37ef494f
liaoxuezhi <2betop.cn@gmail.com>
2020-09-26 23:30:40 +0800

    拆出来方便编辑器实现

--- src/renderers/Form/ButtonToolbar.tsx
+++ src/renderers/Form/ButtonToolbar.tsx
@@ -21,18 +21,23 @@ export interface ButtonToolbarProps
 export class ButtonToolbarControl extends React.Component<ButtonToolbarProps> {
   static defaultProps = {};
 
+  renderButtons() {
+    const {render, classPrefix: ns, buttons} = this.props;
+    return Array.isArray(buttons)
+      ? buttons.map((button, key) =>
+          render(`button/${key}`, button, {
+            key: key
+          })
+        )
+      : null;
+  }
+
   render() {
     const {render, className, classPrefix: ns, buttons} = this.props;
 
     return (
       <div className={cx(`${ns}ButtonToolbar`, className)}>
-        {Array.isArray(buttons)
-          ? buttons.map((button, key) =>
-              render(`button/${key}`, button, {
-                key: key
-              })
-            )
-          : null}
+        {this.renderButtons()}
       </div>
     );
   }
