a395ec60381a0a8fb7384a7dc0f62706c632cd31
liaoxuezhi <2betop.cn@gmail.com>
2020-11-06 23:59:44 +0800

    表达式中可以使用props数据

--- src/utils/filter-schema.ts
+++ src/utils/filter-schema.ts
@@ -1,6 +1,7 @@
 import {evalExpression, filter} from './tpl';
 
 import {Schema, PlainObject} from '../types';
+import {injectPropsToObject} from './helper';
 
 /**
  * 处理 Props 数据，所有带 On 结束的做一次
@@ -15,9 +16,11 @@ import {Schema, PlainObject} from '../types';
 export default function getExprProperties(
   schema: PlainObject,
   data: object = {},
-  blackList: Array<string> = ['addOn']
+  blackList: Array<string> = ['addOn'],
+  props?: any
 ): PlainObject {
   const exprProps: PlainObject = {};
+  let ctx: any = null;
 
   Object.getOwnPropertyNames(schema).forEach(key => {
     if (blackList && ~blackList.indexOf(key)) {
@@ -36,6 +39,8 @@ export default function getExprProperties(
       key = parts[1];
 
       if (parts[2] === 'On' || parts[2] === 'Expr') {
+        ctx = ctx || (props ? injectPropsToObject(data, props) : data);
+
         value =
           parts[2] === 'On' ? evalExpression(value, data) : filter(value, data);
       }
