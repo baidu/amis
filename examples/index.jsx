ce7e2d229154384c158227b29b7e8464d31319c0
吴多益 <wuduoyi@baidu.com>
2020-11-18 11:41:04 +0800

    polyfills 换成 core-js；IE 11 下文档和 sdk 基本可用 (#1081)

--- examples/index.jsx
+++ examples/index.jsx
@@ -2,6 +2,7 @@
  * @file entry of this example.
  * @author liaoxuezhi@cloud.com
  */
+import './polyfills/index';
 import React from 'react';
 import {render} from 'react-dom';
 import App from './components/App';
