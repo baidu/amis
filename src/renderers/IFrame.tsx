30d0c33dcc11786ef17f00661de4be2a169dc302
2betop <2betop.cn@gmail.com>
2020-12-10 19:02:20 +0800

    encode 的地址做个兼容

--- src/renderers/IFrame.tsx
+++ src/renderers/IFrame.tsx
@@ -162,7 +162,7 @@ export default class IFrame extends React.Component<IFrameProps, object> {
 
     if (
       typeof finalSrc === 'string' &&
-      !/^(\.\/|\.\.\/|\/|https?\:\/\/)/.test(finalSrc)
+      !/^(\.\/|\.\.\/|\/|https?\:\/\/|https?\%3A\%2F\%2F)/.test(finalSrc)
     ) {
       return <p>请填写合法的 iframe 地址</p>;
     }
