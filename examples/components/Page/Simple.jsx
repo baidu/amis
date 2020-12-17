48981435d9f38b20ca5879e2a0073c73eb2bc4c2
liaoxuezhi <2betop.cn@gmail.com>
2020-12-18 02:33:22 +0800

    还原一下代码

--- examples/components/Page/Simple.jsx
+++ examples/components/Page/Simple.jsx
@@ -1,95 +1,18 @@
 export default {
   type: 'page',
-  title: '',
-  body: [
-    {
-      type: 'panel',
-      title: '',
-      body: [
-        {
-          type: 'hbox',
-          columns: [
-            {
-              type: 'container',
-              body: [
-                {
-                  type: 'container',
-                  bodyClassName: 'flex flex-row justify-between items-center',
-                  body: [
-                    {
-                      type: 'tpl',
-                      inline: false,
-                      tpl: `<span class="text-loud text-base">费用趋势</span> <span class="text-muted text-sm">单位：元</span>`
-                    },
-
-                    {
-                      type: 'form',
-                      wrapWithPanel: false,
-                      mode: 'inline',
-                      submitOnInit: true,
-                      submitOnChange: true,
-                      controls: [
-                        {
-                          type: 'button-group',
-                          className: 'm-b-none',
-                          name: 'range',
-                          value: 'hafe-year',
-                          options: [
-                            {
-                              label: '近半年',
-                              value: 'hafe-year'
-                            },
-
-                            {
-                              label: '近一年',
-                              value: 'year'
-                            }
-                          ]
-                        },
-
-                        {
-                          type: 'select',
-                          name: 'series',
-                          className: 'm-b-none',
-                          value: 'sumary',
-                          // inputClassName: 'w-24',
-                          options: [
-                            {
-                              label: '总费用',
-                              value: 'sumary'
-                            },
-
-                            {
-                              label: '现金支付',
-                              value: 'cash'
-                            },
-
-                            {
-                              label: '赠送金支付',
-                              value: 'gift'
-                            },
-
-                            {
-                              label: '代金券支付',
-                              value: 'voucher'
-                            }
-                          ]
-                        }
-                      ]
-                    }
-                  ]
-                },
-
-                {
-                  type: 'chart',
-                  api:
-                    'https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/chart/sample'
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    }
-  ]
+  type: 'page',
+  title: '标题',
+  remark: {
+    title: '标题',
+    body:
+      '这是一段描述问题，注意到了没，还可以设置标题。而且只有点击了才弹出来。',
+    icon: 'question-mark',
+    placement: 'right',
+    trigger: 'click',
+    rootClose: true
+  },
+  body: '内容部分. 可以使用 \\${var} 获取变量。如: `\\$date`: ${date}',
+  aside: '边栏部分',
+  toolbar: '工具栏',
+  initApi: '/api/mock2/page/initData'
 };
