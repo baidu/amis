88ae9fbcb328a9291abedaa21d4bc4b709c3b038
2betop <2betop.cn@gmail.com>
2020-12-21 19:03:57 +0800

    Panel 支持更多的 className 配置

--- src/renderers/Panel.tsx
+++ src/renderers/Panel.tsx
@@ -53,6 +53,11 @@ export interface PanelSchema extends BaseSchema {
   footerClassName?: SchemaClassName;
 
   /**
+   * footer 和 actions 外层 div 类名。
+   */
+  footerWrapClassName?: SchemaClassName;
+
+  /**
    * 头部内容, 和 title 二选一。
    */
   header?: SchemaCollection;
@@ -80,6 +85,7 @@ export default class Panel extends React.Component<PanelProps> {
     'header',
     'headerClassName',
     'footerClassName',
+    'footerWrapClassName',
     'actionsClassName',
     'bodyClassName'
   ];
@@ -210,6 +216,7 @@ export default class Panel extends React.Component<PanelProps> {
       headerClassName,
       actionsClassName,
       footerClassName,
+      footerWrapClassName,
       children,
       title,
       footer,
@@ -244,7 +251,12 @@ export default class Panel extends React.Component<PanelProps> {
       );
 
     let footerDom = footerDoms.length ? (
-      <div ref={this.footerDom}>{footerDoms}</div>
+      <div
+        className={cx('Panel-footerWrap', footerWrapClassName)}
+        ref={this.footerDom}
+      >
+        {footerDoms}
+      </div>
     ) : null;
 
     return (
@@ -268,7 +280,13 @@ export default class Panel extends React.Component<PanelProps> {
         {footerDom}
 
         {affixFooter && footerDoms.length ? (
-          <div ref={this.affixDom} className={cx('Panel-fixedBottom')}>
+          <div
+            ref={this.affixDom}
+            className={cx(
+              'Panel-fixedBottom Panel-footerWrap',
+              footerWrapClassName
+            )}
+          >
             {footerDoms}
           </div>
         ) : null}
