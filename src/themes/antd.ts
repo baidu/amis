3a39492a5f0829734d9d7adbd5bff97d3326cae7
wuduoyi <wuduoyi@baidu.com>
2021-01-21 00:24:16 +0800

    tab 效果

--- src/themes/antd.ts
+++ src/themes/antd.ts
@@ -1,8 +1,44 @@
-import {theme, ClassNamesFn, makeClassnames} from '../theme';
-export const classPrefix: string = 'antd-';
-export const classnames: ClassNamesFn = makeClassnames(classPrefix);
+import {theme} from '../theme';
 
 theme('antd', {
-  classPrefix,
-  classnames
+  classPrefix: 'antd-',
+  components: {
+    toast: {
+      closeButton: true
+    }
+  },
+
+  renderers: {
+    'form': {
+      horizontal: {
+        leftFixed: true
+      }
+    },
+
+    'pagination': {
+      maxButtons: 9,
+      showPageInput: false
+    },
+
+    'fieldset': {
+      collapsable: false
+    },
+
+    'remark': {
+      placement: 'right'
+    },
+
+    'tabs': {
+      mode: 'line'
+    },
+
+    'tabs-control': {
+      mode: 'line'
+    },
+
+    'range-control': {
+      showInput: true,
+      clearable: true
+    }
+  }
 });
