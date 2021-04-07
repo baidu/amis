6c6a1bd1fe1c32cbdbc1aff95635bd03bef534bd
rickcole <rickcole21@outlook.com>
2021-04-07 14:46:23 +0800

    修复 headerToolbar 是null 问题

--- src/renderers/Cards.tsx
+++ src/renderers/Cards.tsx
@@ -381,7 +381,7 @@ export default class Cards extends React.Component<GridProps, object> {
     const offsetY =
       this.props.affixOffsetTop ?? this.props.env.affixOffsetTop ?? 0;
     // 50 是 headerToolbar 的高度
-    const toolbarHeight = this.props.headerToolbar.length ? 50 : 0;
+    const toolbarHeight = this.props.headerToolbar?.length ? 50 : 0;
     const affixed =
       clip.top - toolbarHeight < offsetY &&
       clip.top + clip.height - 40 > offsetY;
