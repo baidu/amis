6702253f992c59161c7b2d6ccd399f2fd508f093
wuduoyi <wuduoyi@baidu.com>
2021-04-08 21:31:18 +0800

    统一成 adaptor

--- examples/embed.tsx
+++ examples/embed.tsx
@@ -122,13 +122,13 @@ export function embed(
     return response;
   };
 
-  const responseAdapter = (api: any) => (value: any) => {
+  const responseAdaptor = (api: any) => (value: any) => {
     let response = value.data;
     // 之前拼写错了，需要兼容
     if (env.responseAdpater) {
-      env.responseAdapter = env.responseAdpater;
+      env.responseAdaptor = env.responseAdpater;
     }
-    if (env && env.responseAdapter) {
+    if (env && env.responseAdaptor) {
       const url = api.url;
       const idx = api.url.indexOf('?');
       const query = ~idx ? qs.parse(api.url.substring(idx)) : {};
@@ -137,7 +137,7 @@ export function embed(
         query: query,
         body: api.data
       };
-      response = env.responseAdapter(api, response, query, request);
+      response = env.responseAdaptor(api, response, query, request);
     } else {
       if (response.hasOwnProperty('errno')) {
         response.status = response.errno;
@@ -280,7 +280,7 @@ export function embed(
             data && (config.data = data);
             return axios(url, config)
               .then(attachmentAdpator)
-              .then(responseAdapter(api));
+              .then(responseAdaptor(api));
           },
           isCancel: (value: any) => (axios as any).isCancel(value),
           copy: (contents: string, options: any = {}) => {
