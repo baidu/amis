8cb867ffc94b8cdb12ce690fc56581ca81c83942
吴多益 <wuduoyi@baidu.com>
2021-04-22 10:55:55 +0800

    typo：fieldSet (#1850)

--- src/renderers/Form/HBox.tsx
+++ src/renderers/Form/HBox.tsx
@@ -92,7 +92,7 @@ export class HBoxRenderer extends React.Component<HBoxProps, any> {
   renderChild(region: string, node: Schema, index: number) {
     const {render, renderFormItems, formMode, store, $path} = this.props;
 
-    if (node && !node.type && (node.controls || node.tabs || node.feildSet)) {
+    if (node && !node.type && (node.controls || node.tabs || node.fieldSet)) {
       return renderFormItems(
         node as any,
         ($path as string).replace(/^.*form\//, ''),
