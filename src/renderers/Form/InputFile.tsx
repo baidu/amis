200041492e0bfc568b7297fe3c4d7f4bd8cd3bb0
rickcole <rickcole21@outlook.com>
2021-06-17 17:38:39 +0800

    fix: 1.2.x 修复 File disabled 失效问题

--- src/renderers/Form/InputFile.tsx
+++ src/renderers/Form/InputFile.tsx
@@ -1203,7 +1203,7 @@ export default class FileControl extends React.Component<FileProps, FileState> {
                 'is-active': isDragActive
               })}
             >
-              <input {...getInputProps()} />
+              <input disabled={disabled} {...getInputProps()} />
 
               {isDragActive ? (
                 <div className={cx('FileControl-acceptTip')}>
