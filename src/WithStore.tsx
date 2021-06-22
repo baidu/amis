e02bef184096e46c2ac60bdcd71e770f42848841
2betop <2betop.cn@gmail.com>
2021-06-22 14:39:44 +0800

    combo 数据同步问题调整

--- src/WithStore.tsx
+++ src/WithStore.tsx
@@ -116,13 +116,13 @@ export function HocStoreFactory(renderer: {
         }
       }
 
-      componentWillReceiveProps(nextProps: RendererProps) {
+      componentDidUpdate(prevProps: RendererProps) {
         const props = this.props;
         const store = this.store;
         const shouldSync = renderer.shouldSyncSuperStore?.(
           store,
-          nextProps,
-          props
+          props,
+          prevProps
         );
 
         if (shouldSync === false) {
@@ -132,80 +132,80 @@ export function HocStoreFactory(renderer: {
         if (renderer.extendsData === false) {
           if (
             shouldSync === true ||
-            props.defaultData !== nextProps.defaultData ||
-            isObjectShallowModified(props.data, nextProps.data) ||
+            prevProps.defaultData !== props.defaultData ||
+            isObjectShallowModified(prevProps.data, props.data) ||
             //
             // 特殊处理 CRUD。
             // CRUD 中 toolbar 里面的 data 是空对象，但是 __super 会不一样
-            (nextProps.data &&
-              props.data &&
-              nextProps.data.__super !== props.data.__super)
+            (props.data &&
+              prevProps.data &&
+              props.data.__super !== prevProps.data.__super)
           ) {
             store.initData(
-              extendObject(nextProps.data, {
+              extendObject(props.data, {
                 ...(store.hasRemoteData ? store.data : null), // todo 只保留 remote 数据
-                ...this.formatData(nextProps.defaultData),
-                ...this.formatData(nextProps.data)
+                ...this.formatData(props.defaultData),
+                ...this.formatData(props.data)
               })
             );
           }
         } else if (
           shouldSync === true ||
-          isObjectShallowModified(props.data, nextProps.data)
+          isObjectShallowModified(prevProps.data, props.data)
         ) {
-          if (nextProps.store && nextProps.store.data === nextProps.data) {
+          if (props.store && props.store.data === props.data) {
             store.initData(
               createObject(
-                nextProps.store.data,
-                nextProps.syncSuperStore === false
+                props.store.data,
+                props.syncSuperStore === false
                   ? {
                       ...store.data
                     }
                   : syncDataFromSuper(
                       store.data,
-                      nextProps.store.data,
-                      props.scope,
+                      props.store.data,
+                      prevProps.scope,
                       store,
-                      nextProps.syncSuperStore === true
+                      props.syncSuperStore === true
                     )
               )
             );
-          } else if (nextProps.data && (nextProps.data as any).__super) {
-            store.initData(extendObject(nextProps.data));
+          } else if (props.data && (props.data as any).__super) {
+            store.initData(extendObject(props.data));
           } else {
-            store.initData(createObject(nextProps.scope, nextProps.data));
+            store.initData(createObject(props.scope, props.data));
           }
         } else if (
           (shouldSync === true ||
-            !nextProps.store ||
-            nextProps.data !== nextProps.store.data) &&
-          nextProps.data &&
-          nextProps.data.__super
+            !props.store ||
+            props.data !== props.store.data) &&
+          props.data &&
+          props.data.__super
         ) {
           // 这个用法很少，当 data.__super 值发生变化时，更新 store.data
-          (!props.data ||
+          (!prevProps.data ||
             isObjectShallowModified(
-              nextProps.data.__super,
               props.data.__super,
+              prevProps.data.__super,
               false
             )) &&
             // nextProps.data.__super !== props.data.__super) &&
             store.initData(
-              createObject(nextProps.data.__super, {
-                ...nextProps.data,
+              createObject(props.data.__super, {
+                ...props.data,
                 ...store.data
               }),
 
               store.storeType === 'FormStore' &&
-                props.store?.storeType === 'CRUDStore'
+                prevProps.store?.storeType === 'CRUDStore'
             );
         } else if (
-          nextProps.scope &&
-          nextProps.data === nextProps.store!.data &&
-          (shouldSync === true || props.data !== nextProps.data)
+          props.scope &&
+          props.data === props.store!.data &&
+          (shouldSync === true || prevProps.data !== props.data)
         ) {
           store.initData(
-            createObject(nextProps.scope, {
+            createObject(props.scope, {
               // ...nextProps.data,
               ...store.data
             })
