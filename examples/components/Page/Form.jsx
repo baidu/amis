0f6d72a8ab674ab1d5fec7b26f2fbd78203955af
RickCole <rickcole21@outlook.com>
2021-06-24 17:31:44 +0800

    feat: 单个表单项支持后端校验 (#2127)
    * fix: 1.2.x 修复 File disabled 失效问题
    
    * feat: 单个表单项支持后端校验
    
    * update snapshot
    
    * fix snapshot
    
    * fix
    
    * update error
    
    * 补一下用例
    
    * fix test

--- examples/components/Page/Form.jsx
+++ examples/components/Page/Form.jsx
@@ -1,23 +1,25 @@
 export default {
   type: 'page',
   title: '表单页面',
-  body: {
-    type: 'form',
-    mode: 'horizontal',
-    api: '/api/mock2/form/saveForm',
-    body: [
-      {
-        label: 'Name',
-        type: 'input-text',
-        name: 'name'
-      },
+  body: [
+    {
+      type: 'form',
+      mode: 'horizontal',
+      api: '/api/mock2/form/saveForm',
+      body: [
+        {
+          label: 'Name',
+          type: 'input-text',
+          name: 'name'
+        },
 
-      {
-        label: 'Email',
-        type: 'input-email',
-        placeholder: '请输入邮箱地址',
-        name: 'email'
-      }
-    ]
-  }
+        {
+          label: 'Email',
+          type: 'input-email',
+          placeholder: '请输入邮箱地址',
+          name: 'email'
+        }
+      ]
+    }
+  ]
 };
