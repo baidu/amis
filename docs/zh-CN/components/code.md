3d2f4ac9cbec6e8e1c85a80baa5304b2cce9fb1f
wuduoyi <wuduoyi@baidu.com>
2021-07-04 19:20:48 +0800

    feat: 代码高亮支持自定义语言

--- docs/zh-CN/components/code.md
+++ docs/zh-CN/components/code.md
@@ -86,6 +86,53 @@ language 支持从上下文获取数据
 }
 ```
 
+## 自定义语言高亮
+
+还可以通过 `customLang` 参数来自定义高亮，比如下面这个示例
+
+```schema
+{
+  "body": {
+    "type": "code",
+    "customLang": {
+      "name": "myLog",
+      "tokens": [
+        {
+          "name": "custom-error",
+          "regex": "\\[error.*",
+          "color": "#ff0000",
+          "fontStyle": "bold"
+        },
+        {
+          "name": "custom-notice",
+          "regex": "\\[notice.*",
+          "color": "#FFA500"
+        },
+        {
+          "name": "custom-info",
+          "regex": "\\[info.*",
+          "color": "#808080"
+        },
+        {
+          "name": "custom-date",
+          "regex": "\\[[a-zA-Z 0-9:]+\\]",
+          "color": "#008800"
+        }
+      ]
+    },
+    "value": "[Sun Mar 7 16:02:00 2021] [notice] Apache/1.3.29 (Unix) configured -- resuming normal operations\n[Sun Mar 7 16:02:00 2021] [info] Server built: Feb 27 2021 13:56:37\n[Sun Mar 7 16:02:00 2021] [notice] Accept mutex: sysvsem (Default: sysvsem)\n[Sun Mar 7 17:21:44 2021] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection\n[Sun Mar 7 17:23:53 2021] statistics: Use of uninitialized value in concatenation (.) or string at /home/httpd line 528.\n[Sun Mar 7 17:23:53 2021] statistics: Can't create file /home/httpd/twiki/data/Main/WebStatistics.txt - Permission denied\n[Sun Mar 7 17:27:37 2021] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection\n[Sun Mar 7 17:31:39 2021] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection\n[Sun Mar 7 21:16:17 2021] [error] [client xx.xx.xx.xx] File does not exist: /home/httpd/twiki/view/Main/WebHome"
+  }
+}
+```
+
+`customLang` 中主要是 `tokens` 设置，这里是语言词法配置，它有 4 个配置项：
+
+- `name`：词法名称
+- `regex`：词法的正则匹配，注意因为是在字符串中，这里正则中如果遇到 `\` 需要写成 `\\`
+- `regexFlags`: 可选，正则的标志参数
+- `color`：颜色
+- `fontStyle`: 可选，字体样式，比如 `bold` 代表加粗
+
 ## 属性表
 
 | 属性名      | 类型     | 默认值 | 说明                               |
--- src/renderers/Code.tsx
+++ src/renderers/Code.tsx
@@ -2,10 +2,57 @@
  * @file 代码高亮
  */
 import React from 'react';
+import isEqual from 'lodash/isEqual';
 import {BaseSchema} from '../Schema';
 import {Renderer, RendererProps} from '../factory';
 import {detectPropValueChanged, getPropValue} from '../utils/helper';
 import {isPureVariable, resolveVariableAndFilter} from '../utils/tpl-builtin';
+
+// 自定义语言的 token
+export interface Token {
+  /**
+   * token 的正则
+   */
+  regex: string;
+
+  /**
+   * 正则的 flag
+   */
+  regexFlags?: string;
+
+  /**
+   * token 名称
+   */
+  name: string;
+
+  /**
+   * 文字颜色
+   */
+  color?: string;
+
+  /**
+   * 背景色，不过不知道为何没效果
+   */
+  background?: string;
+
+  /**
+   * 文字样式
+   */
+  fontStyle?: string;
+}
+
+// 自定义语言
+export interface CustomLang {
+  /**
+   * 语言名字
+   */
+  name: string;
+
+  /**
+   * token
+   */
+  tokens: Token[];
+}
 
 /**
  * 代码高亮组件
@@ -56,11 +103,20 @@ export interface CodeSchema extends BaseSchema {
     | 'typescript'
     | 'vb'
     | 'xml'
-    | 'yaml';
+    | 'yaml'
+    | string;
 
   editorTheme?: string;
 
+  /**
+   * tab 大小
+   */
   tabSize?: number;
+
+  /**
+   * 自定义语言
+   */
+  customLang?: CustomLang;
 }
 
 export interface CodeProps
@@ -74,9 +130,12 @@ export default class Code extends React.Component<CodeProps> {
     tabSize: 4
   };
 
-  editor: any;
+  monaco: any;
   toDispose: Array<Function> = [];
   codeRef = React.createRef<HTMLElement>();
+  customLang: CustomLang;
+  sourceCode: string;
+
   constructor(props: CodeProps) {
     super(props);
   }
@@ -88,32 +147,96 @@ export default class Code extends React.Component<CodeProps> {
   componentDidUpdate(preProps: CodeProps) {
     const props = this.props;
 
-    detectPropValueChanged(props, preProps, () => {
-      const dom = this.codeRef.current;
-      this.editor.colorizeElement(dom, {
-        tabSize: this.props.tabSize,
-        theme: this.props.editorTheme
-      });
-    });
-  }
+    const sourceCode = getPropValue(this.props);
+    const preSourceCode = getPropValue(this.props);
 
-  handleMonaco(monaco: any) {
-    this.editor = monaco.editor;
-    if (this.codeRef.current) {
-      const dom = this.codeRef.current;
-      monaco.editor.colorizeElement(dom, {
-        tabSize: this.props.tabSize,
-        theme: this.props.editorTheme
-      });
+    if (
+      sourceCode !== preSourceCode ||
+      (props.customLang && !isEqual(props.customLang, preProps.customLang))
+    ) {
+      const dom = this.codeRef.current!;
+      dom.innerHTML = sourceCode;
+      const theme = this.registTheme() || this.props.editorTheme || 'vs';
+      setTimeout(() => {
+        this.monaco.editor.colorizeElement(dom, {
+          tabSize: this.props.tabSize,
+          theme
+        });
+      }, 16);
     }
   }
 
+  handleMonaco(monaco: any) {
+    this.monaco = monaco;
+    if (this.codeRef.current) {
+      const dom = this.codeRef.current;
+      const theme = this.registTheme() || this.props.editorTheme || 'vs';
+      // 这里必须是异步才能准确，可能是因为 monaco 里注册主题是异步的
+      setTimeout(() => {
+        monaco.editor.colorizeElement(dom, {
+          tabSize: this.props.tabSize,
+          theme
+        });
+      }, 16);
+    }
+  }
+
+  registTheme() {
+    const monaco = this.monaco;
+    if (!monaco) {
+      return null;
+    }
+    if (
+      this.customLang &&
+      this.customLang.name &&
+      this.customLang.tokens &&
+      this.customLang.tokens.length
+    ) {
+      const langName = this.customLang.name;
+      monaco.languages.register({id: langName});
+
+      const tokenizers = [];
+      const rules = [];
+      for (const token of this.customLang.tokens) {
+        const regex = new RegExp(token.regex, token.regexFlags || undefined);
+        tokenizers.push([regex, token.name]);
+        rules.push({
+          token: token.name,
+          foreground: token.color,
+          background: token.background,
+          fontStyle: token.fontStyle
+        });
+      }
+
+      monaco.languages.setMonarchTokensProvider(langName, {
+        tokenizer: {
+          root: tokenizers
+        }
+      });
+      monaco.editor.defineTheme(langName, {
+        base: 'vs',
+        inherit: false,
+        rules: rules
+      });
+
+      return langName;
+    }
+    return null;
+  }
+
   render() {
-    const {className, classnames: cx, data} = this.props;
-    const sourceCode = getPropValue(this.props);
+    const {className, classnames: cx, data, customLang} = this.props;
     let language = this.props.language;
+    const sourceCode = getPropValue(this.props);
     if (isPureVariable(language)) {
       language = resolveVariableAndFilter(language, data);
+    }
+
+    if (customLang) {
+      if (customLang.name) {
+        language = customLang.name;
+      }
+      this.customLang = customLang;
     }
 
     return (
