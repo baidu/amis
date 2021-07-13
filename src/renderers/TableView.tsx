9b90eda7bdf831c63959f63e3e8623a62c090c0a
wuduoyi <wuduoyi@baidu.com>
2021-07-13 15:00:23 +0800

    docs: 补充 table-view 作为布局使用的文档

--- src/renderers/TableView.tsx
+++ src/renderers/TableView.tsx
@@ -208,7 +208,7 @@ export default class TableView extends React.Component<TableViewProps, object> {
     return render('td', body || '');
   }
 
-  renderTds(tds: TdObject[] = [], rowIndex: number) {
+  renderTds(tds: TdObject[], rowIndex: number) {
     return tds.map((td, colIndex) => this.renderTd(td, colIndex, rowIndex));
   }
 
@@ -219,7 +219,7 @@ export default class TableView extends React.Component<TableViewProps, object> {
         style={{height: tr.height, background: tr.background, ...tr.style}}
         key={key}
       >
-        {this.renderTds(tr.tds, rowIndex)}
+        {this.renderTds(tr.tds || [], rowIndex)}
       </tr>
     );
   }
