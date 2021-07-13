b91ad043ae2deff2fd2ad042bcee5f739a0f7cbe
cyboning <34802622+cyboning@users.noreply.github.com>
2021-07-13 10:39:06 +0800

    上传时 formdata 不添加空元素 (#2259)
    Co-authored-by: cy <chenyi08@kuaishou.com>

--- src/renderers/Form/InputImage.tsx
+++ src/renderers/Form/InputImage.tsx
@@ -1043,6 +1043,7 @@ export default class ImageControl extends React.Component<
     if (api.data) {
       qsstringify(api.data)
         .split('&')
+        .filter(item => item !== '')
         .forEach(item => {
           let parts = item.split('=');
           fd.append(parts[0], decodeURIComponent(parts[1]));
