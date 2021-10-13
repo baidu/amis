4cd472f719c965887b240ecd3a98b38607527ca4
吴多益 <wuduoyi@baidu.com>
2021-10-13 12:31:11 +0800

    chore: 编译分一下阶段方便排查错误

--- build.sh
+++ build.sh
@@ -6,14 +6,17 @@ export NODE_ENV=production
 rm -rf lib
 rm -rf output
 
+echo "===fis release==="
 ./node_modules/.bin/fis3 release publish -c
 rm -rf lib/node_modules
 
 # 生成 sdk
+echo "===fis sdk==="
 rm -rf sdk && ./node_modules/.bin/fis3 release publish-sdk -c
 
 cp -r node_modules/monaco-editor/min/vs/base/browser sdk/thirds/monaco-editor/min/vs/base
 
+echo "===postcss ie11==="
 # 生成去掉变量的 css
 ./node_modules/.bin/postcss sdk/sdk.css >sdk/sdk-ie11.css
 ./node_modules/.bin/postcss sdk/ang.css >sdk/ang-ie11.css
@@ -31,9 +34,12 @@ cp examples/static/iconfont.eot sdk/
 
 mkdir sdk/locale
 
+echo "===sdk locale==="
 node scripts/generate-sdk-locale.js src/locale/de-DE.ts > sdk/locale/de-DE.js
 
 # 生成 .d.ts 文件
+echo "===generate .d.ts==="
 ./node_modules/.bin/tsc --declaration --emitDeclarationOnly --outDir ./lib --project ./tsconfig-for-declaration.json
 
+echo "===build-schemas==="
 npm run build-schemas
