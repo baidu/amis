917006ae9ccd00a6762646a8e243ea8b1d4c140b
吴多益 <wuduoyi@baidu.com>
2021-11-02 14:54:08 +0800

    feat: tpl 内置函数补充乘法和除法 (#2818)
    * feat: tpl 内置函数补充乘法和除法
    
    * 四则运算支持变量

--- src/utils/tpl-builtin.ts
+++ src/utils/tpl-builtin.ts
@@ -383,10 +383,26 @@ export const filters: {
   first: input => input && input[0],
   nth: (input, nth = 0) => input && input[nth],
   last: input => input && (input.length ? input[input.length - 1] : null),
-  minus: (input, step = 1) =>
-    stripNumber((parseInt(input, 10) || 0) - parseInt(step, 10)),
-  plus: (input, step = 1) =>
-    stripNumber((parseInt(input, 10) || 0) + parseInt(step, 10)),
+  minus(input, step = 1) {
+    return stripNumber(
+      (Number(input) || 0) - Number(getStrOrVariable(step, this))
+    );
+  },
+  plus(input, step = 1) {
+    return stripNumber(
+      (Number(input) || 0) + Number(getStrOrVariable(step, this))
+    );
+  },
+  times(input, step = 1) {
+    return stripNumber(
+      (Number(input) || 0) * Number(getStrOrVariable(step, this))
+    );
+  },
+  division(input, step = 1) {
+    return stripNumber(
+      (Number(input) || 0) / Number(getStrOrVariable(step, this))
+    );
+  },
   count: (input: any) =>
     Array.isArray(input) || typeof input === 'string' ? input.length : 0,
   sum: (input, field) => {
