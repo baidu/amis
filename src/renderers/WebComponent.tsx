cf4991e6aefb6a58a4bd1d21a58e37aee6abc80c
wuduoyi <wuduoyi@baidu.com>
2021-11-17 17:09:26 +0800

    fix: 修复 Avatar 等组件的动态变量不支持 ${xx} 写法问题

--- src/renderers/WebComponent.tsx
+++ src/renderers/WebComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Renderer, RendererProps} from '../factory';
 import {BaseSchema, SchemaCollection} from '../Schema';
-import {resolveVariable} from '../utils/tpl-builtin';
+import {resolveVariable, resolveVariableAndFilter} from '../utils/tpl-builtin';
 import mapValues from 'lodash/mapValues';
 
 /**
@@ -43,7 +43,7 @@ export default class WebComponent extends React.Component<RendererProps> {
 
     const propsValues = mapValues(props, s => {
       if (typeof s === 'string') {
-        return resolveVariable(s, data) || s;
+        return resolveVariableAndFilter(s, data, '| raw') || s;
       } else {
         return s;
       }
