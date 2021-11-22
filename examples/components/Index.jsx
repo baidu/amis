edd066937aa470af6657f90eb4f5347e50addd9a
wuduoyi <wuduoyi@baidu.com>
2021-11-22 12:17:41 +0800

    避免修改 examples

--- examples/components/Index.jsx
+++ examples/components/Index.jsx
@@ -1,34 +1,16 @@
 export default {
   type: 'page',
-  body: {
-    type: 'crud',
-    api: '/examples/index',
-    syncLocation: false,
-    columns: [
-      {
-        name: 'id',
-        label: 'ID'
-      },
-      {
-        name: 'engine',
-        label: 'Rendering engine'
-      },
-      {
-        name: 'browser',
-        label: 'Browser'
-      },
-      {
-        name: 'platform',
-        label: 'Platform(s)'
-      },
-      {
-        name: 'version',
-        label: 'Engine version'
-      },
-      {
-        name: 'grade',
-        label: 'CSS grade'
-      }
-    ]
-  }
+  title: '标题',
+  remark: {
+    title: '标题',
+    body: '这是一段描述问题，注意到了没，还可以设置标题。而且只有点击了才弹出来。',
+    icon: 'question-mark',
+    placement: 'right',
+    trigger: 'click',
+    rootClose: true
+  },
+  body: '内容部分. 可以使用 \\${var} 获取变量。如: `\\$date`: ${date}',
+  aside: '边栏部分',
+  toolbar: '工具栏',
+  initApi: '/api/mock2/page/initData'
 };
