3bcf3da85999d7a35bfb4208f858dc6560c7cbe8
hongyang03 <hongyang03@baidu.com>
2022-01-11 14:47:53 +0800

    fix: test修复

--- src/renderers/Calendar.tsx
+++ src/renderers/Calendar.tsx
@@ -3,23 +3,23 @@ import {Renderer} from '../factory';
 import {SchemaObject, BaseSchema} from '../Schema';
 import {DateControlRenderer} from './Form/InputDate';
 
+interface scheduleItem {
+  startTime: Date;
+  endTime: Date;
+  content: any;
+  className?: string;
+}
+
 export interface CalendarSchema extends BaseSchema {
   /**
    * 指定为日历选择控件
    */
   type: 'calendar';
- 
+
   /**
    * 日程
    */
-  schedules?:
-    | Array<{
-        startTime: Date;
-        endTime: Date;
-        content: any;
-        className?: string;
-      }>
-    | string;
+  schedules?: Array<scheduleItem> | string;
 
   /**
    * 日程显示颜色自定义
@@ -37,6 +37,7 @@ export interface CalendarSchema extends BaseSchema {
 })
 export class CalendarRenderer extends DateControlRenderer {
   static defaultProps = {
+    ...DateControlRenderer.defaultProps,
     embed: true
   }
 }
