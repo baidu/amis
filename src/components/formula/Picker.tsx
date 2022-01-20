f02e582a995e0f93d9b39b1ad34b73c182c19bc1
panzheng <panzheng@baidu.com>
2022-01-20 10:21:09 +0800

    函数列表支持上下文变量

--- src/components/formula/Picker.tsx
+++ src/components/formula/Picker.tsx
@@ -115,11 +115,17 @@ export class FormulaPicker extends React.Component<FormulaPickerProps> {
       title,
       clearable,
       variables,
+      functions,
       ...rest
     } = this.props;
     if (isPureVariable(variables)) {
       // 如果是 ${xxx} 这种形式，将其处理成实际的值
       variables = resolveVariableAndFilter(variables, this.props.data, '| raw');
+    }
+
+    if (isPureVariable(functions)) {
+      // 如果是 ${xxx} 这种形式，将其处理成实际的值
+      functions = resolveVariableAndFilter(functions, this.props.data, '| raw');
     }
     const iconElement = generateIcon(cx, icon, 'Icon');
 
@@ -132,6 +138,7 @@ export class FormulaPicker extends React.Component<FormulaPickerProps> {
             <Editor
               {...rest}
               variables={variables}
+              functions={functions}
               value={value}
               onChange={onChange}
             />
