9a05a10bfbbb7ea2f2eddacca8dba1d78d26809c
wuduoyi <wuduoyi@baidu.com>
2022-02-10 19:25:18 +0800

    补充部分组件的单元测试

--- __tests__/renderers/Image.test.tsx
+++ __tests__/renderers/Image.test.tsx
@@ -22,3 +22,39 @@ test('Renderer:image', async () => {
 
   expect(container).toMatchSnapshot();
 });
+
+test('Renderer:images', async () => {
+  const {container} = render(
+    amisRender(
+      {
+        type: 'page',
+        data: {
+          imageList: [
+            'https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395692722/4f3cb4202335.jpeg@s_0,w_216,l_1,f_jpg,q_80',
+            'https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395692942/d8e4992057f9.jpeg@s_0,w_216,l_1,f_jpg,q_80',
+            'https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395693148/1314a2a3d3f6.jpeg@s_0,w_216,l_1,f_jpg,q_80',
+            'https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395693379/8f2e79f82be0.jpeg@s_0,w_216,l_1,f_jpg,q_80',
+            'https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395693566/552b175ef11d.jpeg@s_0,w_216,l_1,f_jpg,q_80'
+          ]
+        },
+        body: [
+          {
+            type: 'images',
+            source: '${imageList}'
+          },
+          {
+            type: 'divider'
+          },
+          {
+            type: 'images',
+            name: 'imageList'
+          }
+        ]
+      },
+      {},
+      makeEnv({})
+    )
+  );
+
+  expect(container).toMatchSnapshot();
+});
