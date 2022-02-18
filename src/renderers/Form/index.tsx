56c895096bc152a9a62e0edb9a1646eec65e1b79
pianruijie <pianruijie@baidu.com>
2022-02-18 15:02:41 +0800

    提交表单校验失败事件优化

--- src/renderers/Form/index.tsx
+++ src/renderers/Form/index.tsx
@@ -862,10 +862,6 @@ export default class Form extends React.Component<FormProps, object> {
     }
   }
 
-  // 问题1：sumbit的时候校验成功事件和提交成功事件的顺序问题 已解决
-  // 问题2：itme开启validateOnChange的时候，如何触发事件 
-  // 问题3：提交到target时候是否需要触发事件，ajax请求里面、drawconfirm里面应该都不需要吧 需要确认
-  // 问题4：确定props.data是如何同步到store.data里面的
   emitChange(submit: boolean) {
     const {onChange, store, submitOnChange, dispatchEvent, data} = this.props;
 
