4965b018d08d48bc195b536d0c5f16eeaa260960
wuduoyi <wuduoyi@baidu.com>
2022-03-09 16:16:50 +0800

    fix: 避免 table/nav 里数据 id 不一致导致页面报错，将之前的 id 换成更不容易冲突的 __id 字段

--- __tests__/renderers/Nav.test.tsx
+++ __tests__/renderers/Nav.test.tsx
@@ -20,28 +20,28 @@ test('Renderer:nav', () => {
         },
         links: [
           {
-            id: 0,
+            __id: 0,
             label: 'Nav 1',
             to: '/docs/index',
             icon: 'https://suda.cdn.bcebos.com/images%2F2021-01%2Fdiamond.svg',
             active: true
           },
           {
-            id: 1,
+            __id: 1,
             label: 'Nav 2',
             to: '/docs/api',
             customText: 'HOT',
             customLevel: 'danger'
           },
           {
-            id: 2,
+            __id: 2,
             label: 'Nav 3',
             to: '/docs/renderers',
             customText: 'SUC',
             customLevel: 'success'
           },
           {
-            id: 3,
+            __id: 3,
             label: '外部地址',
             to: 'http://www.baidu.com/',
             target: '_blank'
