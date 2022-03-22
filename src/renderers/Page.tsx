fc2b653a20cbbfd4b87fab55838ac5ecb2bf158d
hongyang03 <hongyang03@baidu.com>
2022-03-22 16:20:06 +0800

    refactor: 代码优化

--- src/renderers/Page.tsx
+++ src/renderers/Page.tsx
@@ -12,7 +12,7 @@ import {
   FunctionPropertyNames
 } from '../types';
 import {filter, evalExpression} from '../utils/tpl';
-import {isVisible, autobind, bulkBindFunctions, createObject} from '../utils/helper';
+import {isVisible, autobind, bulkBindFunctions} from '../utils/helper';
 import {ScopedContext, IScopedContext} from '../Scoped';
 import Alert from '../components/Alert2';
 import {isApiOutdated, isEffectiveApi} from '../utils/api';
