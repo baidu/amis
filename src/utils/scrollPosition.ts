1776aba963f654a6439569db897509a2d401cad8
2betop <2betop.cn@gmail.com>
2022-03-24 18:53:24 +0800

    scrollParent 获取不对修复

--- src/utils/scrollPosition.ts
+++ src/utils/scrollPosition.ts
@@ -1,15 +1,34 @@
 import position from './position';
 
-export function getScrollParent(node: HTMLElement | null): HTMLElement {
-  if (node == null) {
+export function getScrollParent(element: HTMLElement | null): HTMLElement {
+  if (!element) {
     return document.body;
   }
 
-  if (node.scrollHeight > node.clientHeight) {
-    return node;
-  } else {
-    return getScrollParent(node.parentElement);
+  const position = getComputedStyle(element).getPropertyValue('position');
+  const excludeStatic = position === 'absolute';
+  const ownerDoc = element.ownerDocument;
+
+  if (position === 'fixed') return document.body;
+
+  // @ts-ignore
+  while ((element = element.parentNode) && element !== document.body) {
+    const currentStyle = getComputedStyle(element);
+    const isStatic =
+      excludeStatic && currentStyle.getPropertyValue('position') === 'static';
+    const style =
+      (currentStyle.getPropertyValue('overflow') || '') +
+      (currentStyle.getPropertyValue('overflow-y') || '') +
+      currentStyle.getPropertyValue('overflow-x');
+
+    if (isStatic) continue;
+
+    if (/(auto|scroll)/.test(style)) {
+      return element;
+    }
   }
+
+  return document.body;
 }
 
 export function scrollPosition(dom: HTMLElement) {
