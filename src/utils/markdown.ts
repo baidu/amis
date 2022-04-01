5bdba906a70fe4a954587fb2bc26d30140434731
吴多益 <wuduoyi@baidu.com>
2022-04-01 12:06:31 +0800

    feat: markdown 渲染支持高级配置，比如是否支持 html (#3935)

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -6,12 +6,13 @@ import markdownIt from 'markdown-it';
 // @ts-ignore
 import {html5Media} from 'markdown-it-html5-media';
 
-const markdown = markdownIt({
-  linkify: true
-});
+const markdown = markdownIt();
 
 markdown.use(html5Media);
 
-export default function (content: string) {
+export default function (content: string, options?: markdownIt.Options) {
+  if (options) {
+    markdown.set(options);
+  }
   return markdown.render(content);
 }
