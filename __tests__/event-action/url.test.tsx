644275d03b92233b8b95514a704d658fd929b56d
hsm-lv <lvxiaojiao@baidu.com>
2022-05-15 20:56:16 +0800

    test:补充事件动作测试用例

---
+++ __tests__/event-action/url.test.tsx
@@ -0,0 +1,80 @@
+import {fireEvent, render, waitFor} from '@testing-library/react';
+import '../../src/themes/default';
+import {render as amisRender} from '../../src/index';
+import {makeEnv} from '../helper';
+
+test('EventAction:url & link', async () => {
+  const jumpTo = jest.fn();
+  const {getByText, container}: any = render(
+    amisRender(
+      {
+        type: 'page',
+        data: {
+          myname: 'lvxj',
+          myjon: 'player'
+        },
+        body: [
+          {
+            type: 'button',
+            label: '跳转',
+            level: 'primary',
+            className: 'ml-2',
+            onEvent: {
+              click: {
+                actions: [
+                  {
+                    actionType: 'url',
+                    args: {
+                      url: 'http://www.baidu.com',
+                      blank: true,
+                      params: {
+                        name: 'jack',
+                        jon: '${myjon}'
+                      },
+                      name: '${myname}',
+                      age: 18
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        ]
+      },
+      {},
+      makeEnv({
+        jumpTo
+      })
+    )
+  );
+
+  fireEvent.click(getByText('跳转'));
+  await waitFor(() => {
+    expect(jumpTo).toHaveBeenCalled();
+  });
+  expect(jumpTo.mock.calls[0][0]).toEqual(
+    'http://www.baidu.com?name=lvxj&jon=player&age=18'
+  );
+  expect(jumpTo.mock.calls[0][1]).toEqual({
+    actionType: 'url',
+    type: 'button',
+    url: 'http://www.baidu.com',
+    blank: true,
+    params: {
+      name: 'jack',
+      jon: 'player'
+    },
+    name: 'lvxj',
+    age: 18
+  });
+  expect(jumpTo.mock.calls[0][2]).toEqual({
+    url: 'http://www.baidu.com',
+    blank: true,
+    params: {
+      name: 'jack',
+      jon: 'player'
+    },
+    name: 'lvxj',
+    age: 18
+  });
+});
