2c0210c2301f48b3e14a74f224d4f3c69af087d1
2betop <2betop.cn@gmail.com>
2022-05-17 14:00:36 +0800

    目录调整

--- packages/amis/lib/icons/filter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tslib_1 = require("tslib");
-var react_1 = (0, tslib_1.__importDefault)(require("react"));
-var Filter = function (props) { return react_1.default.createElement("svg", (0, tslib_1.__assign)({ viewBox: "0 0 150 100", className: "icon" }, props),
-    react_1.default.createElement("g", { fill: "currentColor", fillRule: "nonzero" },
-        react_1.default.createElement("path", { d: "M58.3333334,100 L91.6666666,100 L91.6666666,83.3333334 L58.3333334,83.3333334 L58.3333334,100 L58.3333334,100 Z M0,0 L0,16.6666666 L150,16.6666666 L150,0 L0,0 Z M25,58.3333334 L125,58.3333334 L125,41.6666666 L25,41.6666666 L25,58.3333334 Z", id: "\\u5F62\\u72B6" }))); };
-exports.default = Filter;
