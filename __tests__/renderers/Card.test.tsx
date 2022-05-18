32ac4f4f75c328dbaf796e7aceb5681a24d265a7
RickCole21 <rickcole21@outlook.com>
2022-05-18 19:26:09 +0800

    fix: Card 高亮失效修复

--- __tests__/renderers/Card.test.tsx
+++ __tests__/renderers/Card.test.tsx
@@ -197,3 +197,89 @@ test('Renderer:cards media', () => {
 
   expect(container).toMatchSnapshot();
 });
+
+test('Renderer:cards hightlight', () => {
+  const {container} = render(
+    amisRender(
+      {
+        type: 'page',
+        body: {
+          type: 'card',
+          header: {
+            title: '标题',
+            highlight: true,
+            highlightClassName: 'test-highlight-class'
+          },
+          media: {
+            type: 'image',
+            className: 'w-36 h-24',
+            url: 'https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395692722/4f3cb4202335.jpeg@s_0,w_216,l_1,f_jpg,q_80',
+            position: 'left'
+          },
+          body: '这里是内容',
+          secondary: '次要说明',
+          actions: [
+            {
+              type: 'button',
+              label: '操作',
+              actionType: 'dialog',
+              className: 'mr-4',
+              dialog: {
+                title: '操作',
+                body: '你正在编辑该卡片'
+              }
+            },
+            {
+              type: 'button',
+              label: '操作',
+              actionType: 'dialog',
+              className: 'mr-2.5',
+              dialog: {
+                title: '操作',
+                body: '你正在编辑该卡片'
+              }
+            },
+            {
+              type: 'dropdown-button',
+              level: 'link',
+              icon: 'fa fa-ellipsis-h',
+              className: 'pr-1 flex',
+              hideCaret: true,
+              buttons: [
+                {
+                  type: 'button',
+                  label: '编辑',
+                  actionType: 'dialog',
+                  dialog: {
+                    title: '编辑',
+                    body: '你正在编辑该卡片'
+                  }
+                },
+                {
+                  type: 'button',
+                  label: '删除',
+                  actionType: 'dialog',
+                  dialog: {
+                    title: '提示',
+                    body: '你删掉了该卡片'
+                  }
+                }
+              ]
+            }
+          ],
+          toolbar: [
+            {
+              type: 'tpl',
+              tpl: '标签',
+              className: 'label label-warning'
+            }
+          ]
+        }
+      },
+      {},
+      makeEnv({})
+    )
+  );
+
+  expect(container).toMatchSnapshot();
+});
