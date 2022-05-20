c847ab5e1ad555dd991de5117ec1d1923bb47817
2betop <2betop.cn@gmail.com>
2022-05-20 15:20:29 +0800

    rm lib 目录

--- packages/amis/lib/icons/caret.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tslib_1 = require("tslib");
-var react_1 = (0, tslib_1.__importDefault)(require("react"));
-var Caret = function (props) { return react_1.default.createElement("svg", (0, tslib_1.__assign)({ viewBox: "0 0 12 8", className: "icon" }, props),
-    react_1.default.createElement("g", { id: "04\\u4E0B\\u62C9/24px/\\u9ED8\\u8BA4\\u7981\\u7528", transform: "translate(-115.000000, -8.000000)" },
-        react_1.default.createElement("polygon", { id: "Shape", fill: "currentColor", fillRule: "nonzero", points: "125.472727 8 121 12.8 116.527273 8 115 9.6 121 16 127 9.6" }))); };
-exports.default = Caret;
--- packages/amis/lib/icons/close.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tslib_1 = require("tslib");
-var react_1 = (0, tslib_1.__importDefault)(require("react"));
-var Close = function (props) { return react_1.default.createElement("svg", (0, tslib_1.__assign)({ viewBox: "0 0 12 12", className: "icon" }, props),
-    react_1.default.createElement("polygon", { id: "path-1", points: "6.0003653 5.2970518 10.5993691 0.6980479600000002 11.3064759 1.4051547400000004 6.7074721 6.0041586 11.3009516 10.5976381 10.5938448 11.3047449 6.0003653 6.7112654 1.4056713299999997 11.3059593 0.6985645500000004 10.5988525 5.2932585 6.0041586 0.6956119200000002 1.4065120000000002 1.4027187000000003 0.69940522" })); };
-exports.default = Close;
