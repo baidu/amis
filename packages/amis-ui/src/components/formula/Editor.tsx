05d90a6e9369736160a9c0278cf38f69747a98de
hsm-lv <lvxiaojiao@baidu.com>
2022-05-26 17:47:38 +0800

    fix:input-formula多余dom

--- packages/amis-ui/src/components/formula/Editor.tsx
+++ packages/amis-ui/src/components/formula/Editor.tsx
@@ -291,7 +291,6 @@ export class FormulaEditor extends React.Component<
           'is-focused': focused
         })}
       >
-        (
         <section className={cx(`FormulaEditor-content`)}>
           <header className={cx(`FormulaEditor-header`)}>
             {__(header || 'FormulaEditor.title')}
@@ -307,7 +306,7 @@ export class FormulaEditor extends React.Component<
             onBlur={this.handleBlur}
           />
         </section>
-        )
+
         <section className={cx('FormulaEditor-settings')}>
           <div className={cx('FormulaEditor-panel')}>
             {variableMode !== 'tabs' ? (
