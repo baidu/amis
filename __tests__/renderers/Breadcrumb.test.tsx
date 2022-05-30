ed977e02b6e69c340201dcf5cd2e45dd356610f4
meerkat <kit_hack@outlook.com>
2022-05-30 21:40:25 +0800

    feat: 补充 renderers [Action, Card, BreadCrumb] 属性单测

--- __tests__/renderers/Breadcrumb.test.tsx
+++ __tests__/renderers/Breadcrumb.test.tsx
@@ -1,8 +1,9 @@
 import React = require('react');
-import {render} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import '../../src/themes/default';
 import {render as amisRender} from '../../src/index';
 import {makeEnv} from '../helper';
+import * as renderer from 'react-test-renderer';
 
 test('Renderer:breadcrumb', () => {
   const {container} = render(
@@ -113,10 +114,10 @@ test('Renderer:breadcrumb dropdown', () => {
               dropdown: [
                 {
                   label: '选项一',
-                  href: 'https://baidu.gitee.com/',
+                  href: 'https://baidu.gitee.com/'
                 },
                 {
-                  label: '选项二',
+                  label: '选项二'
                 }
               ]
             },
@@ -131,5 +132,86 @@ test('Renderer:breadcrumb dropdown', () => {
     )
   );
 
+  expect(container).toMatchSnapshot();
+});
+
+test('Renderer:breadcrumb tooltip labelMaxLength', () => {
+  const schema = {
+    type: 'page',
+    body: {
+      type: 'breadcrumb',
+      separator: '>',
+      separatorClassName: 'text-black',
+      labelMaxLength: 16,
+      tooltipPosition: 'bottom',
+      items: [
+        {
+          href: 'https://baidu.gitee.com/',
+          icon: 'fa fa-home'
+        },
+        {
+          label: '上级页面上级页面上级页面上级页面上级页面'
+        },
+        {
+          label: '当前页面'
+        }
+      ]
+    }
+  };
+  const component = renderer.create(amisRender(schema, {}, makeEnv({})));
+  expect(component.toJSON()).toMatchSnapshot();
+
+  schema.body.tooltipPosition = 'top';
+  component.update(amisRender(schema, {}, makeEnv({})));
+  expect(component.toJSON()).toMatchSnapshot();
+
+  schema.body.tooltipPosition = 'left';
+  component.update(amisRender(schema, {}, makeEnv({})));
+  expect(component.toJSON()).toMatchSnapshot();
+
+  schema.body.tooltipPosition = 'right';
+  component.update(amisRender(schema, {}, makeEnv({})));
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('Renderer:breadcrumb className', () => {
+  const {container} = render(
+    amisRender(
+      {
+        type: 'breadcrumb',
+        dropdownClassName: 'dropdownClassName',
+        dropdownItemClassName: 'dropdownItemClassName',
+        itemClassName: 'itemClassName',
+        className: 'className',
+        items: [
+          {
+            label: '首页',
+            href: 'https://baidu.gitee.com/',
+            icon: 'fa fa-home'
+          },
+          {
+            label: '上级页面',
+            dropdown: [
+              {
+                label: '选项一',
+                href: 'https://baidu.gitee.com/'
+              },
+              {
+                label: '选项二'
+              }
+            ]
+          },
+          {
+            label: '<b>当前页面</b>'
+          }
+        ]
+      },
+      {},
+      makeEnv({})
+    )
+  );
+  fireEvent.click(container.querySelector('.cxd-Breadcrumb-item-caret')!);
+  expect(!container.querySelector('.dropdownClassName')).toBeFalsy();
+  expect(!container.querySelector('.dropdownItemClassName')).toBeFalsy();
   expect(container).toMatchSnapshot();
 });
