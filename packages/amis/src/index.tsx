a9afb047b5a8e2c4ae7766c47d2b0ed5428f63e9
liaoxuezhi <2betop.cn@gmail.com>
2022-06-06 17:28:39 +0800

    补充发布内网脚本

--- packages/amis/src/index.tsx
+++ packages/amis/src/index.tsx
@@ -1,3 +1,10 @@
+/** @license amis v@version
+ *
+ * Copyright Baidu
+ *
+ * This source code is licensed under the Apache license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 export * from 'amis-core';
 export * from 'amis-ui';
 
