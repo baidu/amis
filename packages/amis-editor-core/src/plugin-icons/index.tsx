1c5206e1a33ac7eaba16abe4dc4b3bb4e9e147c3
wibetter <365533093@qq.com>
2022-06-21 16:18:34 +0800

    feat: 添加新版组件icon
    Change-Id: Id8c7406a49fbba1ef9451a358c9db4651747f3b4

---
+++ packages/amis-editor-core/src/plugin-icons/index.tsx
@@ -0,0 +1,220 @@
+import {registerIcon, Icon} from 'amis';
+
+/**
+ * 新版组件 icon
+ */
+
+// 功能类组件 icon x 10
+import audio from './feat/audio.svg';
+import custom from './feat/custom.svg';
+import each from './feat/each.svg';
+import form from './feat/form.svg';
+import nav from './feat/nav.svg';
+import qrcode from './feat/qrcode.svg';
+import service from './feat/service.svg';
+import table from './feat/table.svg';
+import tasks from './feat/tasks.svg';
+import video from './feat/video.svg';
+
+// 容器类组件 icon x 11
+import anchorNav from './container/anchor-nav.svg';
+import collapse from './container/collapse.svg';
+import container from './container/container.svg';
+import flexContainer from './container/flex-container.svg';
+import formGroup from './container/form-group.svg';
+import grid from './container/grid.svg';
+import iframe from './container/iframe.svg';
+import panel from './container/panel.svg';
+import tableView from './container/table-view.svg';
+import tabs from './container/tabs.svg';
+import webComponent from './container/web-component.svg';
+
+// 按钮类组件 icon
+import btnGroup from './btn/btn-group.svg';
+import btnSelect from './btn/btn-select.svg';
+import btnToolbar from './btn/btn-toolbar.svg';
+import button from './btn/button.svg';
+import dropdownBtn from './btn/dropdown-btn.svg';
+
+// 展示类组件 icon x 15
+import breadcrumb from './show/breadcrumb.svg';
+import card from './show/card.svg';
+import cards from './show/cards.svg';
+import carousel from './show/carousel.svg';
+import chart from './show/chart.svg';
+import code from './show/code.svg';
+import date from './show/date.svg';
+import datetime from './show/datetime.svg';
+import time from './show/time.svg';
+import image from './show/image.svg';
+import images from './show/images.svg';
+import list from './show/list.svg';
+import log from './show/log.svg';
+import mapping from './show/mapping.svg';
+import sparkline from './show/sparkline.svg';
+
+// 表单类组件 icon x 51
+import chainedSelect from './form/chained-select.svg';
+import checkbox from './form/checkbox.svg';
+import checkboxs from './form/checkboxs.svg';
+import combo from './form/combo.svg';
+import conditionBuilder from './form/condition-builder.svg';
+import diffEditor from './form/diff-editor.svg';
+import editor from './form/editor.svg';
+import hidden from './form/hidden.svg';
+import inputCity from './form/input-city.svg';
+import inputColor from './form/input-color.svg';
+import inputDateRange from './form/input-date-range.svg';
+import inputDate from './form/input-date.svg';
+import inputDatetime from './form/input-datetime.svg';
+import inputEmail from './form/input-email.svg';
+import inputExcel from './form/input-excel.svg';
+import inputFile from './form/input-file.svg';
+import inputGroup from './form/input-group.svg';
+import inputImage from './form/input-image.svg';
+import inputKv from './form/input-kv.svg';
+import inputMonthRange from './form/input-month-range.svg';
+import inputNumber from './form/input-number.svg';
+import inputPassword from './form/input-password.svg';
+import inputQuarterRange from './form/input-quarter-range.svg';
+import inputRange from './form/input-range.svg';
+import inputRating from './form/input-rating.svg';
+import inputRepeat from './form/input-repeat.svg';
+import inputRichText from './form/input-rich-text.svg';
+import inputTag from './form/input-tag.svg';
+import inputText from './form/input-text.svg';
+import inputTimeRange from './form/input-time-range.svg';
+import inputTime from './form/input-time.svg';
+import inputTree from './form/input-tree.svg';
+import inputUrl from './form/input-url.svg';
+import inputYear from './form/input-year.svg';
+import inputSelect from './form/list-select.svg';
+import locationPicker from './form/location-picker.svg';
+import matrixCheckboxes from './form/matrix-checkboxes.svg';
+import month from './form/month.svg';
+import nestedSelect from './form/nested-select.svg';
+import picker from './form/picker.svg';
+import quarter from './form/quarter.svg';
+import radios from './form/radios.svg';
+import select from './form/select.svg';
+import staticIcon from './form/static.svg';
+import subForm from './form/sub-form.svg';
+import switchIcon from './form/switch.svg';
+import tabsTransfer from './form/tabs-transfer.svg';
+import textarea from './form/textarea.svg';
+import transfer from './form/transfer.svg';
+import treeSelect from './form/tree-select.svg';
+import uuid from './form/uuid.svg';
+
+// 其他类组件 icon
+import propertySheet from './other/property-sheet.svg';
+import tooltip from './other/tooltip.svg';
+
+// 功能类组件 icon x 11
+registerIcon('audio-plugin', audio);
+registerIcon('custom-plugin', custom);
+registerIcon('each-plugin', each);
+registerIcon('form-plugin', form);
+registerIcon('nav-plugin', nav);
+registerIcon('qrcode-plugin', qrcode);
+registerIcon('service-plugin', service);
+registerIcon('table-plugin', table);
+registerIcon('tasks-plugin', tasks);
+registerIcon('video-plugin', video);
+
+// 容器类组件 icon x 10
+registerIcon('anchorNav-plugin', anchorNav);
+registerIcon('collapse-plugin', collapse);
+registerIcon('flexContainer-plugin', flexContainer);
+registerIcon('formGroup-plugin', formGroup);
+registerIcon('grid-plugin', grid);
+registerIcon('iframe-plugin', iframe);
+registerIcon('tableView-plugin', tableView);
+registerIcon('tabs-plugin', tabs);
+registerIcon('webComponent-plugin', webComponent);
+
+// 按钮类组件 icon x5
+registerIcon('btnGroup-plugin', btnGroup);
+registerIcon('btnSelect-plugin', btnSelect);
+registerIcon('btnToolbar-plugin', btnToolbar);
+registerIcon('button-plugin', button);
+registerIcon('dropdownBtn-plugin', dropdownBtn);
+
+// 展示类组件 icon x 15
+registerIcon('breadcrumb-plugin', breadcrumb);
+registerIcon('card-plugin', card);
+registerIcon('cards-plugin', cards);
+registerIcon('carousel-plugin', carousel);
+registerIcon('chart-plugin', chart);
+registerIcon('code-plugin', code);
+registerIcon('date-plugin', date);
+registerIcon('datetime-plugin', datetime);
+registerIcon('time-plugin', time);
+registerIcon('image-plugin', image);
+registerIcon('images-plugin', images);
+registerIcon('list-plugin', list);
+registerIcon('log-plugin', log);
+registerIcon('mapping-plugin', mapping);
+registerIcon('sparkline-plugin', sparkline);
+
+// 表单类组件 icon
+registerIcon('chainedSelect-plugin', chainedSelect);
+registerIcon('checkbox-plugin', checkbox);
+registerIcon('checkboxs-plugin', checkboxs);
+registerIcon('combo-plugin', combo);
+registerIcon('conditionBuilder-plugin', conditionBuilder);
+registerIcon('diffEditor-plugin', diffEditor);
+registerIcon('editor-plugin', editor);
+registerIcon('hidden-plugin', hidden);
+registerIcon('inputCity-plugin', inputCity);
+registerIcon('inputColor-plugin', inputColor);
+registerIcon('inputDateRange-plugin', inputDateRange);
+registerIcon('inputDate-plugin', inputDate);
+registerIcon('inputDatetime-plugin', inputDatetime);
+registerIcon('inputEmail-plugin', inputEmail);
+registerIcon('inputExcel-plugin', inputExcel);
+
+registerIcon('inputFile-plugin', inputFile);
+registerIcon('inputGroup-plugin', inputGroup);
+registerIcon('inputImage-plugin', inputImage);
+registerIcon('inputKv-plugin', inputKv);
+registerIcon('inputMonthRange-plugin', inputMonthRange);
+registerIcon('inputNumber-plugin', inputNumber);
+registerIcon('inputPassword-plugin', inputPassword);
+registerIcon('inputQuarterRange-plugin', inputQuarterRange);
+registerIcon('inputRange-plugin', inputRange);
+registerIcon('inputRating-plugin', inputRating);
+registerIcon('inputRepeat-plugin', inputRepeat);
+registerIcon('inputRichText-plugin', inputRichText);
+registerIcon('inputTag-plugin', inputTag);
+registerIcon('inputText-plugin', inputText);
+registerIcon('inputTimeRange-plugin', inputTimeRange);
+
+registerIcon('inputTime-plugin', inputTime);
+registerIcon('inputTree-plugin', inputTree);
+registerIcon('inputUrl-plugin', inputUrl);
+registerIcon('inputYear-plugin', inputYear);
+registerIcon('inputSelect-plugin', inputSelect);
+registerIcon('locationPicker-plugin', locationPicker);
+registerIcon('matrixCheckboxes-plugin', matrixCheckboxes);
+registerIcon('month-plugin', month);
+registerIcon('nestedSelect-plugin', nestedSelect);
+registerIcon('picker-plugin', picker);
+registerIcon('quarter-plugin', quarter);
+registerIcon('radios-plugin', radios);
+registerIcon('select-plugin', select);
+registerIcon('static-plugin', staticIcon);
+registerIcon('subForm-plugin', subForm);
+
+registerIcon('switch-plugin', switchIcon);
+registerIcon('tabsTransfer-plugin', tabsTransfer);
+registerIcon('textarea-plugin', textarea);
+registerIcon('transfer-plugin', transfer);
+registerIcon('treeSelect-plugin', treeSelect);
+registerIcon('uuid-plugin', uuid);
+
+// 其他类组件 icon
+registerIcon('propertySheet-plugin', propertySheet);
+registerIcon('tooltip-plugin', tooltip);
+
+export {Icon};
