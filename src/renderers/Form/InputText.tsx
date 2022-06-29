0017d9c9fe0c56cb50cc9a4b240795e9bceca088
RUNZE LU <36724300+lurunze1226@users.noreply.github.com>
2022-06-29 20:35:44 +0800

    fix: InputPassword组件revealPassword属性没有update问题 (#4738)

--- src/renderers/Form/InputText.tsx
+++ src/renderers/Form/InputText.tsx
@@ -220,6 +220,10 @@ export default class TextControl extends React.PureComponent<
             : this.valueToString(props.value)
       });
     }
+    if (prevProps.revealPassword !== props.revealPassword) {
+      /** 隐藏按钮的同时将密码设置为隐藏态 */
+      !props.revealPassword && this.setState({revealPassword: false});
+    }
   }
 
   componentWillUnmount() {
@@ -835,7 +839,11 @@ export default class TextControl extends React.PureComponent<
           step={step}
           onChange={this.handleNormalInputChange}
           value={this.valueToString(value)}
-          className={cx(type === 'password' && revealPassword && 'TextControl-input-password')}
+          className={cx(
+            type === 'password' &&
+              revealPassword &&
+              'TextControl-input-password'
+          )}
         />
         {clearable && !disabled && !readOnly && value ? (
           <a onClick={this.clearValue} className={`${ns}TextControl-clear`}>
