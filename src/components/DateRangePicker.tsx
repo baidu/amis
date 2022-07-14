d95f3ac797ede45c1c9eae5b5a02679198e749c9
meerkat <kit_hack@outlook.com>
2022-07-14 19:19:28 +0800

    fix: 修复DateTime组件，时间选择器高度与日期对齐

--- src/components/DateRangePicker.tsx
+++ src/components/DateRangePicker.tsx
@@ -1326,7 +1326,7 @@ export class DateRangePicker extends React.Component<
             inputFormat={inputFormat}
             timeFormat={timeFormat}
             viewDate={this.nextMonth}
-            isEndDate
+            // isEndDate
             isValidDate={this.checkEndIsValidDate}
             viewMode={viewMode}
             input={false}
