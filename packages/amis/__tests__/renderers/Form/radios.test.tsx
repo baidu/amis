8799e62f6c5a486ffadccbfbb8997f4f87a97a98
tooeast <liuzedong02@baidu.com>
2022-07-25 19:10:20 +0800

    chore: 补充 radios switch input-text 单测

--- packages/amis/__tests__/renderers/Form/radios.test.tsx
+++ packages/amis/__tests__/renderers/Form/radios.test.tsx
@@ -64,3 +64,89 @@ test('Renderer:radios', async () => {
   });
   expect(container).toMatchSnapshot();
 });
+
+
+test('Renderer:radios source', async () => {
+  const {getByText, container} = render(
+    amisRender(
+      {
+        type: 'form',
+        title: 'The form',
+        data: {
+          items: [
+            {
+              name: 'A',
+              id: 'aa',
+              fill: '11'
+            },
+            {
+              name: 'B',
+              id: 'bb',
+              fill: '12'
+            },
+            {
+              name: 'C',
+              id: 'cc',
+              fill: '13'
+            }
+          ]
+        },
+        body: [
+          {
+            name: 'radios',
+            type: 'radios',
+            label: 'radios',
+            inline: false,
+            columnsCount: 3,
+            selectFirst: true,
+            source: '${items}',
+            labelField: 'name',
+            valueField: 'id',
+            optionClassName: 'class-a',
+            autoFill: {
+              fillFromRadios: "${fill}"
+            }
+          },
+          {
+            type: 'static',
+            name: 'radios',
+            label: '当前值'
+          },
+          {
+            type: 'tpl',
+            className: 'autoFillClass',
+            tpl: '${fillFromRadios}'
+          }
+        ],
+        submitText: null,
+        actions: []
+      },
+      {},
+      makeEnv()
+    )
+  );
+
+  await waitFor(() => {
+    expect(
+      (container.querySelector('.cxd-PlainField') as Element).innerHTML
+    ).toBe('aa');
+
+    expect(
+      container.querySelector('span.class-a') as Element
+    ).toBeInTheDocument();
+
+    expect(
+      container.querySelector('.cxd-Grid-col--sm4') as Element
+    ).toBeInTheDocument();
+  });
+
+  await waitFor(() => {
+    fireEvent.click(getByText(/C/));
+
+    expect(
+      (container.querySelector('.cxd-TplField.autoFillClass span') as Element).innerHTML
+    ).toBe('13');
+  });
+
+  expect(container).toMatchSnapshot();
+});
