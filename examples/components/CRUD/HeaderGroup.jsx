9c316babcf4da552a39a7e9e86823157e3c88c8b
lurunze1226 <lurunze1226@foxmail.com>
2022-08-02 15:38:21 +0800

    fix: CRUD开启超级表头时展开列显示错误，合并列元素只有1个时无法显示remark问题

--- examples/components/CRUD/HeaderGroup.jsx
+++ examples/components/CRUD/HeaderGroup.jsx
@@ -1,8 +1,10 @@
 export default {
+  type: 'page',
   title: '支持表头分组，通过在 cloumn 上设置 groupName 实现。',
   body: [
     {
       type: 'crud',
+      draggable: true,
       bulkActions: [
         {
           label: '批量删除',
@@ -33,117 +35,177 @@ export default {
           }
         }
       ],
-      data: {
-        items: [
-          {
-            engine: 'Trident',
-            browser: 'Internet Explorer 4.2',
-            platform: 'Win 95+',
-            version: '4',
-            grade: 'A',
-            id: 1
-          },
-          {
-            engine: 'Trident',
-            browser: 'Internet Explorer 4.2',
-            platform: 'Win 95+',
-            version: '4',
-            grade: 'B',
-            id: 2
-          },
-          {
-            engine: 'Trident',
-            browser: 'AOL browser (AOL desktop)',
-            platform: 'Win 95+',
-            version: '4',
-            grade: 'C',
-            id: 3
-          },
-          {
-            engine: 'Trident',
-            browser: 'AOL browser (AOL desktop)',
-            platform: 'Win 98',
-            version: '3',
-            grade: 'A',
-            id: 4
-          },
-          {
-            engine: 'Trident',
-            browser: 'AOL browser (AOL desktop)',
-            platform: 'Win 98',
-            version: '4',
-            grade: 'A',
-            id: 5
-          },
-          {
-            engine: 'Gecko',
-            browser: 'Firefox 1.0',
-            platform: 'Win 98+ / OSX.2+',
-            version: '4',
-            grade: 'A',
-            id: 6
-          },
-          {
-            engine: 'Gecko',
-            browser: 'Firefox 1.0',
-            platform: 'Win 98+ / OSX.2+',
-            version: '5',
-            grade: 'A',
-            id: 7
-          },
-          {
-            engine: 'Gecko',
-            browser: 'Firefox 2.0',
-            platform: 'Win 98+ / OSX.2+',
-            version: '5',
-            grade: 'B',
-            id: 8
-          },
-          {
-            engine: 'Gecko',
-            browser: 'Firefox 2.0',
-            platform: 'Win 98+ / OSX.2+',
-            version: '5',
-            grade: 'C',
-            id: 9
-          },
-          {
-            engine: 'Gecko',
-            browser: 'Firefox 2.0',
-            platform: 'Win 98+ / OSX.2+',
-            version: '5',
-            grade: 'D',
-            id: 10
-          }
-        ]
-      },
       columns: [
+        {
+          name: 'id',
+          label: 'ID',
+          remark: 'ID',
+          groupName: 'A'
+        },
         {
           name: 'grade',
           label: 'CSS grade',
+          remark: 'CSS grade',
           groupName: 'A'
         },
         {
           name: 'engine',
           label: 'Rendering engine',
+          remark: 'Rendering engine',
           groupName: 'A'
         },
         {
           name: 'browser',
           label: 'Browser',
-          groupName: 'A'
+          remark: 'Browser'
         },
         {
           name: 'platform',
           label: 'Platform(s)',
+          remark: 'Platform(s)',
           groupName: 'B'
         },
         {
           name: 'version',
           label: 'Engine version',
+          remark: 'Engine version',
           groupName: 'B'
         }
-      ]
+      ],
+      data: {
+        items: [
+          {
+            engine: 'Trident',
+            browser: 'Internet Explorer 4.0',
+            platform: 'Win 95+',
+            version: '4',
+            grade: 'X',
+            id: 1,
+            children: [
+              {
+                engine: 'Trident',
+                browser: 'Internet Explorer 4.0',
+                platform: 'Win 95+',
+                version: '4',
+                grade: 'X',
+                id: 1001
+              },
+              {
+                engine: 'Trident',
+                browser: 'Internet Explorer 5.0',
+                platform: 'Win 95+',
+                version: '5',
+                grade: 'C',
+                id: 1002
+              }
+            ]
+          },
+          {
+            engine: 'Trident',
+            browser: 'Internet Explorer 5.0',
+            platform: 'Win 95+',
+            version: '5',
+            grade: 'C',
+            id: 2,
+            children: [
+              {
+                engine: 'Trident',
+                browser: 'Internet Explorer 4.0',
+                platform: 'Win 95+',
+                version: '4',
+                grade: 'X',
+                id: 2001
+              },
+              {
+                engine: 'Trident',
+                browser: 'Internet Explorer 5.0',
+                platform: 'Win 95+',
+                version: '5',
+                grade: 'C',
+                id: 2002
+              }
+            ]
+          },
+          {
+            engine: 'Trident',
+            browser: 'Internet Explorer 5.5',
+            platform: 'Win 95+',
+            version: '5.5',
+            grade: 'A',
+            id: 3,
+            children: [
+              {
+                engine: 'Trident',
+                browser: 'Internet Explorer 4.0',
+                platform: 'Win 95+',
+                version: '4',
+                grade: 'X',
+                id: 3001
+              },
+              {
+                engine: 'Trident',
+                browser: 'Internet Explorer 5.0',
+                platform: 'Win 95+',
+                version: '5',
+                grade: 'C',
+                id: 3002
+              }
+            ]
+          },
+          {
+            engine: 'Trident',
+            browser: 'Internet Explorer 6',
+            platform: 'Win 98+',
+            version: '6',
+            grade: 'A',
+            id: 4,
+            children: [
+              {
+                engine: 'Trident',
+                browser: 'Internet Explorer 4.0',
+                platform: 'Win 95+',
+                version: '4',
+                grade: 'X',
+                id: 4001
+              },
+              {
+                engine: 'Trident',
+                browser: 'Internet Explorer 5.0',
+                platform: 'Win 95+',
+                version: '5',
+                grade: 'C',
+                id: 4002
+              }
+            ]
+          },
+          {
+            engine: 'Trident',
+            browser: 'Internet Explorer 7',
+            platform: 'Win XP SP2+',
+            version: '7',
+            grade: 'A',
+            id: 5,
+            children: [
+              {
+                engine: 'Trident',
+                browser: 'Internet Explorer 4.0',
+                platform: 'Win 95+',
+                version: '4',
+                grade: 'X',
+                id: 5001
+              },
+              {
+                engine: 'Trident',
+                browser: 'Internet Explorer 5.0',
+                platform: 'Win 95+',
+                version: '5',
+                grade: 'C',
+                id: 5002
+              }
+            ]
+          }
+        ]
+      }
     }
   ]
 };
