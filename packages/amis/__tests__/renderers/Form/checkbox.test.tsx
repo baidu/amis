1349ef4aa0cffaec6454bc86f4e83e41f8b941d6
sansiro <sansiro@sansiro.me>
2022-08-09 10:49:28 +0800

    chore: input-number、input-range、checkbox 单测用例补充 & range style 百分比最多保留两位 (#5076)
    * checkbox 单测补充
    
    * input-number 单测补充
    
    * input-range 单测补充 & range style 百分比最多保留两位
    
    Co-authored-by: tooeast <liuzedong02@baidu.com>

--- packages/amis/__tests__/renderers/Form/checkbox.test.tsx
+++ packages/amis/__tests__/renderers/Form/checkbox.test.tsx
@@ -1,8 +1,8 @@
 import React = require('react');
-import {render, cleanup, fireEvent} from '@testing-library/react';
+import {render, cleanup, fireEvent, waitFor} from '@testing-library/react';
 import '../../../src';
 import {render as amisRender} from '../../../src';
-import {makeEnv} from '../../helper';
+import {makeEnv, wait} from '../../helper';
 import {clearStoresCache} from '../../../src';
 
 afterEach(() => {
@@ -33,5 +33,91 @@ test('Renderer:checkbox', async () => {
       makeEnv()
     )
   );
+  expect(container).toMatchSnapshot();
+});
+
+test('Renderer:checkbox with optionType', async () => {
+  const {container, getByText} = render(
+    amisRender(
+      {
+        type: 'form',
+        title: 'The form',
+        controls: [
+          {
+            name: 'checkbox',
+            type: 'checkbox',
+            label: 'Checkbox',
+            option: '去吃饭',
+            trueValue: '吃了',
+            falseValue: '没吃呢',
+            optionType: 'button',
+            value: '没吃呢'
+          },
+          {
+            type: 'static',
+            label: '您吃了吗',
+            name: 'checkbox'
+          }
+        ],
+        submitText: null,
+        actions: []
+      },
+      {},
+      makeEnv()
+    )
+  );
+
+  await wait(300);
+
+  expect(
+    container.querySelector('.cxd-Checkbox.cxd-Checkbox--button') as Element
+  ).toBeInTheDocument();
+
+
+  fireEvent.click(getByText(/去吃饭/));
+
+  await waitFor(() => {
+    expect(
+      (container.querySelector('.cxd-PlainField') as Element).innerHTML
+    ).toBe('吃了');
+
+    expect(
+      container.querySelector('.cxd-Checkbox.cxd-Checkbox--button.cxd-Checkbox--button--checked') as Element
+    ).toBeInTheDocument();
+  });
+
+  expect(container).toMatchSnapshot();
+});
+
+
+test('Renderer:checkbox with checked', async () => {
+  const {container, getByText} = render(
+    amisRender(
+      {
+        type: 'form',
+        title: 'The form',
+        controls: [
+          {
+            name: 'checkbox',
+            type: 'checkbox',
+            label: 'Checkbox',
+            option: '选项说明',
+            checked: true
+          }
+        ],
+        submitText: null,
+        actions: []
+      },
+      {},
+      makeEnv()
+    )
+  );
+
+  await wait(300);
+
+  expect(
+    container.querySelector('.cxd-Checkbox input[checked]') as Element
+  ).toBeInTheDocument();
+
   expect(container).toMatchSnapshot();
 });
--- packages/amis/__tests__/renderers/Form/range.test.tsx
+++ packages/amis/__tests__/renderers/Form/range.test.tsx
@@ -4,7 +4,7 @@ import '../../../src';
 import {render as amisRender} from '../../../src';
 import {makeEnv, wait} from '../../helper';
 
-test('Renderer:range', async () => {
+test('Renderer:range with showInput', async () => {
   const {container} = render(
     amisRender(
       {
@@ -34,6 +34,10 @@ test('Renderer:range', async () => {
   });
   fireEvent.mouseUp(slider);
 
+  expect(
+    container.querySelector('.cxd-InputRange-input') as Element
+  ).toBeInTheDocument();
+
   const input = container.querySelector('.cxd-InputRange-input input');
   fireEvent.change(input!, {
     target: {
@@ -42,10 +46,16 @@ test('Renderer:range', async () => {
   });
   await wait(200);
 
+  expect(
+    (
+      container.querySelector('.cxd-InputRange-track-active') as Element
+    ).getAttribute('style')
+  ).toContain('width: 7%');
+
   expect(container).toMatchSnapshot();
 });
 
-test('Renderer:range:multiple', async () => {
+test('Renderer:range with multiple & clearable & delimiter', async () => {
   const {container} = render(
     amisRender(
       {
@@ -56,8 +66,14 @@ test('Renderer:range:multiple', async () => {
             type: 'input-range',
             name: 'range',
             multiple: true,
-            value: [10, 20],
-            showInput: true
+            delimiter: '--',
+            value: '10--20',
+            showInput: true,
+            clearable: false
+          },
+          {
+            type: 'static',
+            name: 'range'
           }
         ],
         title: 'The form',
@@ -67,6 +83,10 @@ test('Renderer:range:multiple', async () => {
       makeEnv({})
     )
   );
+
+  expect(
+    container.querySelector('.cxd-InputRange-clear')
+  ).not.toBeInTheDocument();
 
   const inputs = container.querySelectorAll('.cxd-InputRange-input input');
   fireEvent.change(inputs[0], {
@@ -81,15 +101,26 @@ test('Renderer:range:multiple', async () => {
     }
   });
   fireEvent.blur(inputs[1]);
-  const close = container.querySelector('a.cxd-InputRange-clear');
-  fireEvent.click(close!);
 
   await wait(200);
+  expect(
+    (
+      container.querySelector('.cxd-InputRange-track-active') as Element
+    ).getAttribute('style')
+  ).toBe('width: 6%; left: 7%;');
+
+  const icons = container.querySelectorAll('.cxd-InputRange-handle');
+  expect(icons[0].getAttribute('style')).toContain('left: 7%;');
+  expect(icons[1].getAttribute('style')).toContain('left: 13%;');
+
+  expect(
+    (container.querySelector('.cxd-PlainField') as Element).innerHTML
+  ).toBe('7--13');
 
   expect(container).toMatchSnapshot();
 });
 
-test('Renderer:range:showSteps', async () => {
+test('Renderer:range with showSteps', async () => {
   const {container} = render(
     amisRender(
       {
@@ -113,10 +144,16 @@ test('Renderer:range:showSteps', async () => {
   );
 
   await wait(200);
+
+  const dots = container.querySelectorAll('.cxd-InputRange-track-dot');
+
+  expect(dots.length).toBe(9);
+  expect(dots[8].getAttribute('style')).toContain('left: 90%;');
+
   expect(container).toMatchSnapshot();
 });
 
-test('Renderer:range:marks', async () => {
+test('Renderer:range with marks', async () => {
   const {container} = render(
     amisRender(
       {
@@ -133,7 +170,7 @@ test('Renderer:range:marks', async () => {
               '40%': '40Mbps',
               '60%': '60Mbps',
               '80%': '80Mbps',
-              '100': '100'
+              '100%': '100'
             }
           }
         ],
@@ -146,10 +183,17 @@ test('Renderer:range:marks', async () => {
   );
 
   await wait(200);
+
+  const marks = container.querySelectorAll('.cxd-InputRange-marks > div');
+
+  expect(marks.length).toBe(6);
+  expect(marks[3].innerHTML).toBe('<span>60Mbps</span>');
+  expect(marks[3].getAttribute('style')).toContain('left: 60%;');
+
   expect(container).toMatchSnapshot();
 });
 
-test('Renderer:range:tooltipVisible', async () => {
+test('Renderer:range with tooltipVisible & tooltipPlacement', async () => {
   const {container} = render(
     amisRender(
       {
@@ -160,7 +204,8 @@ test('Renderer:range:tooltipVisible', async () => {
             type: 'input-range',
             name: 'range',
             tooltipVisible: true,
-            tooltipPlacement: 'right'
+            tooltipPlacement: 'right',
+            value: 41
           }
         ],
         title: 'The form',
@@ -172,5 +217,76 @@ test('Renderer:range:tooltipVisible', async () => {
   );
 
   await wait(200);
+
+  expect(
+    container.querySelector(
+      '.cxd-InputRange-label.pos-right.cxd-InputRange-label-visible'
+    )
+  ).toBeInTheDocument();
+
+  const labelSpan = container.querySelector('.cxd-InputRange-label span');
+  expect(labelSpan).toBeI;
+
+  expect(labelSpan!.innerHTML).toBe('41');
+
+  expect(container).toMatchSnapshot();
+});
+
+test('Renderer:range with min & max & step & joinValues', async () => {
+  const {container} = render(
+    amisRender(
+      {
+        type: 'form',
+        debug: true,
+        api: '/api/xxx',
+        controls: [
+          {
+            type: 'input-range',
+            name: 'range',
+            min: 0,
+            max: 1,
+            step: 0.1,
+            multiple: true,
+            showInput: true,
+            joinValues: false
+          }
+        ],
+        title: 'The form',
+        actions: []
+      },
+      {},
+      makeEnv({})
+    )
+  );
+
+  const inputs = container.querySelectorAll('.cxd-InputRange-input input');
+  fireEvent.change(inputs[0], {
+    target: {
+      value: '0.2'
+    }
+  });
+  fireEvent.blur(inputs[0]);
+  fireEvent.change(inputs[1], {
+    target: {
+      value: '0.8'
+    }
+  });
+  fireEvent.blur(inputs[1]);
+
+  await wait(200);
+  expect(
+    (
+      container.querySelector('.cxd-InputRange-track-active') as Element
+    ).getAttribute('style')
+  ).toContain('width: 60%; left: 20%;');
+
+  expect((container.querySelector('.cxd-Form--debug') as Element).innerHTML)
+    .toBe(`{
+  "range": {
+    "min": 0.2,
+    "max": 0.8
+  }
+}`);
+
   expect(container).toMatchSnapshot();
 });
