ae210e28facf8db53a3b6c3eae15b1c1143450ab
tooeast <liuzedong02@baidu.com>
2022-08-12 16:54:02 +0800

    chore: input-array 单测补充

--- packages/amis/__tests__/renderers/Form/inputArray.test.tsx
+++ packages/amis/__tests__/renderers/Form/inputArray.test.tsx
@@ -1,8 +1,37 @@
-import React = require('react');
 import {render, fireEvent, waitFor, screen} from '@testing-library/react';
 import '../../../src';
 import {render as amisRender} from '../../../src';
 import {makeEnv, wait} from '../../helper';
+
+const setup = (items: any[] = []) => {
+  const onSubmit = jest.fn();
+  const utils = render(
+    amisRender(
+      {
+        type: 'page',
+        body: {
+          type: 'form',
+          submitText: 'Submit',
+          api: '/api/mock/saveForm?waitSeconds=1',
+          mode: 'horizontal',
+          body: items
+        }
+      },
+      {onSubmit},
+      makeEnv()
+    )
+  );
+
+  const submitBtn = utils.container.querySelector(
+    'button[type="submit"]'
+  ) as HTMLElement;
+
+  return {
+    onSubmit,
+    submitBtn,
+    ...utils
+  };
+};
 
 test('Renderer:inputArray', async () => {
   const onSubmit = jest.fn();
@@ -55,6 +84,141 @@ test('Renderer:inputArray', async () => {
   expect(formData).toEqual({
     array: ['amis']
   });
+
+  expect(container).toMatchSnapshot();
+});
+
+test('Renderer:inputArray with removable & addable & addButtonText', async () => {
+  const {container: containerOne} = setup([
+    {
+      name: 'array',
+      label: '颜色集合',
+      type: 'input-array',
+      inline: true,
+      removable: false,
+      addable: false,
+      items: {
+        type: 'input-text'
+      },
+      value: ['red', 'blue']
+    }
+  ]);
+
+  await wait(500);
+
+  expect(
+    containerOne.querySelector('.cxd-Combo-delBtn')
+  ).not.toBeInTheDocument();
+  expect(
+    containerOne.querySelector('.cxd-Combo-addBtn')
+  ).not.toBeInTheDocument();
+  expect(containerOne).toMatchSnapshot('false');
+
+  const {container, onSubmit, submitBtn} = setup([
+    {
+      name: 'array',
+      label: '颜色集合',
+      type: 'input-array',
+      inline: true,
+      removable: true,
+      addable: true,
+      items: {
+        type: 'input-text'
+      },
+      value: ['red', 'blue'],
+      addButtonText: '我是增加按钮'
+    }
+  ]);
+  await wait(500);
+  const delButton = container.querySelector('a.cxd-Combo-delBtn');
+  expect(container.querySelectorAll('a.cxd-Combo-delBtn').length).toBe(2);
+  const addButton = container.querySelector('.cxd-Combo-addBtn')!;
+
+  expect(delButton).toBeInTheDocument();
+  expect(addButton).toBeInTheDocument();
+  expect((addButton.querySelector('span') as Element).innerHTML).toBe(
+    '我是增加按钮'
+  );
+
+  fireEvent.click(delButton!);
+
+  await wait(500);
+
+  fireEvent.click(submitBtn);
+
+  await wait(500);
+  const formData = onSubmit.mock.calls[0][0];
+  expect(onSubmit).toHaveBeenCalled();
+  expect(formData).toEqual({
+    array: ['blue']
+  });
+
+  expect(container).toMatchSnapshot('false');
+});
+
+test('Renderer:inputArray with minLength & maxLength', async () => {
+  const {container} = setup([
+    {
+      name: 'array',
+      label: '颜色集合',
+      type: 'input-array',
+      inline: true,
+      removable: true,
+      addable: true,
+      minLength: 2,
+      maxLength: 4,
+      items: {
+        type: 'input-text'
+      },
+      value: ['red', 'blue', 'green']
+    }
+  ]);
+  await wait(500);
+
+  // 范围内的，增删都在
+  expect(container.querySelector('a.cxd-Combo-delBtn')).toBeInTheDocument();
+  expect(container.querySelector('.cxd-Combo-addBtn')).toBeInTheDocument();
+  // 最大值
+  fireEvent.click(container.querySelector('.cxd-Combo-addBtn')!);
+  await wait(500);
+  expect(container.querySelector('a.cxd-Combo-delBtn')).toBeInTheDocument();
+  expect(container.querySelector('.cxd-Combo-addBtn')).not.toBeInTheDocument();
+  // 最小值
+  fireEvent.click(container.querySelector('a.cxd-Combo-delBtn')!);
+  await wait(500);
+  fireEvent.click(container.querySelector('a.cxd-Combo-delBtn')!);
+  await wait(500);
+  expect(container.querySelector('a.cxd-Combo-delBtn')).not.toBeInTheDocument();
+  expect(container.querySelector('.cxd-Combo-addBtn')).toBeInTheDocument();
+
+  expect(container).toMatchSnapshot();
+});
+
+test('Renderer:inputArray with draggable & draggableTip', async () => {
+  const {container, onSubmit, submitBtn} = setup([
+    {
+      name: 'array',
+      label: '颜色集合',
+      type: 'input-array',
+      inline: true,
+      draggable: true,
+      draggableTip: '我是拖拽提示',
+      items: {
+        type: 'input-text'
+      },
+      value: ['red', 'blue', 'green']
+    }
+  ]);
+  await wait(500);
+
+  const drager = container.querySelector('.cxd-Combo-itemDrager')!;
+  expect(drager).toBeInTheDocument();
+  expect(container.querySelectorAll('.cxd-Combo-itemDrager').length).toBe(3);
+
+  expect(container.querySelector('.cxd-Combo-dragableTip')).toBeInTheDocument();
+  expect(
+    (container.querySelector('.cxd-Combo-dragableTip') as Element).innerHTML
+  ).toBe('我是拖拽提示');
 
   expect(container).toMatchSnapshot();
 });
