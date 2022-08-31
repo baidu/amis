d5cf92744fb0daff4d8ef23ad6aefe92c17b634c
sansiro <sansiro@sansiro.me>
2022-08-31 10:58:25 +0800

    修改

--- packages/amis-core/src/utils/optionValueCompare.ts
+++ packages/amis-core/src/utils/optionValueCompare.ts
@@ -1,14 +1,20 @@
 import {OptionValue, Option} from '../types';
 import {isObject} from './helper';
+import {isEqual} from 'lodash';
 
 export function matchOptionValue(
   a: OptionValue,
   b: Option,
   valueField: string = 'value'
 ) {
-  return isObject(a)
-    ? a === b[valueField || 'value']
-    : String(b[valueField || 'value']) === String(a);
+  // a 可能为 Option, 此时需要取其value
+  const aValue =
+    isObject(a) && a && (a as Option).hasOwnProperty(valueField)
+      ? (a as Option)[valueField]
+      : a;
+  return isObject(aValue)
+    ? isEqual(aValue, b[valueField || 'value'])
+    : String(b[valueField || 'value']) === String(aValue);
 }
 
 export function optionValueCompare(
