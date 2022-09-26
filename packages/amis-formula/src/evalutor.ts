ffaeb389f1e711852d376460b67d5671bba46e47
liaoxuezhi <2betop.cn@gmail.com>
2022-09-26 17:05:05 +0800

    fix: 修复公式 xor

--- packages/amis-formula/src/evalutor.ts
+++ packages/amis-formula/src/evalutor.ts
@@ -542,7 +542,7 @@ export class Evaluator {
   }
 
   /**
-   * 异或处理，多个表达式有且只有一个为真，则结果为真。
+   * 异或处理，多个表达式组中存在奇数个真时认为真。
    *
    * @example XOR(condition1, condition2)
    * @param {expression} condition1 - 条件表达式1
