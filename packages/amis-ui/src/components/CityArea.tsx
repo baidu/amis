cc1dbcf0a56a32c5d52cc095bfb12e529be75151
zhangxulong <zhangxulong@baidu.com>
2022-10-08 13:41:05 +0800

    fix： 城市选择组件移动端回显问题

--- packages/amis-ui/src/components/CityArea.tsx
+++ packages/amis-ui/src/components/CityArea.tsx
@@ -172,11 +172,12 @@ const CityArea = memo<AreaProps>(props => {
   };
 
   const getPropsValue = () => {
-    // 最后一项的值
+    // 最后一项的值、默认北京东城区
     let code =
       (value && value.code) ||
       (typeof value === 'number' && value) ||
-      (typeof value === 'string' && /(\d{6})/.test(value) && RegExp.$1);
+      (typeof value === 'string' && /(\d{6})/.test(value) && RegExp.$1) ||
+      110101;
     const values: Array<number> = [];
     if (code && db[code]) {
       code = parseInt(code, 10);
@@ -195,6 +196,14 @@ const CityArea = memo<AreaProps>(props => {
         values[2] = code;
       }
       setValues(values);
+
+      if (props.value) {
+        const confirmValues = values.map((item: number) => ({
+          text: db[item],
+          value: item
+        }));
+        setConfirmValues(confirmValues);
+      }
     }
   };
 
@@ -247,8 +256,8 @@ const CityArea = memo<AreaProps>(props => {
   }, []);
 
   useEffect(() => {
-    isOpened && db && getPropsValue();
-  }, [db, isOpened]);
+    db && (props.value || isOpened) && getPropsValue();
+  }, [db, isOpened, props.value]);
 
   useEffect(() => {
     street && propsChange();
