c5ef4b882acdd8e6190dce6e1bb769d31226fc60
RUNZE LU <36724300+lurunze1226@users.noreply.github.com>
2022-10-31 19:14:13 +0800

    chore: ConditionBuilder组件i18n文案补充 (#5648)

--- packages/amis-ui/src/locale/zh-CN.ts
+++ packages/amis-ui/src/locale/zh-CN.ts
@@ -343,6 +343,7 @@ register('zh-CN', {
   'Condition.expression': '表达式',
   'Condition.formula_placeholder': '请输入公式',
   'Condition.fun_error': '方法未定义',
+  'Condition.configured': '已配置',
   'InputTable.uniqueError': '列`{{label}}`没有通过唯一验证',
   'Timeline.collapseText': '收起',
   'Timeline.expandText': '展开',
