d90cbe96c93315a267e1cd133e66eacde9003668
jiatianqi <jiatianqi@baidu.com>
2022-11-07 17:18:07 +0800

    amis-saas-6070 [Bug] 测试：【页面配置，外观下内联】设置占满后与默认展示一致
    Change-Id: Id1c497b3c0f98fdcf290f50c15805e6930fda9c7

--- packages/amis-editor/src/tpl/common.tsx
+++ packages/amis-editor/src/tpl/common.tsx
@@ -120,7 +120,7 @@ setSchemaTpl('formItemSize', {
   name: 'size',
   label: '控件宽度',
   type: 'select',
-  pipeIn: defaultValue(''),
+  pipeIn: defaultValue('full'),
   // mode: 'inline',
   // className: 'w-full',
   options: [
@@ -144,12 +144,8 @@ setSchemaTpl('formItemSize', {
       value: 'lg'
     },
     {
-      label: '占满',
+      label: '默认（占满）',
       value: 'full'
-    },
-    {
-      label: '默认',
-      value: ''
     }
   ]
 });
