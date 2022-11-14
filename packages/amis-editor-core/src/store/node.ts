b98c987b9e17ee2eb15cd528410375288511a42c
wibetter <365533093@qq.com>
2022-11-14 22:21:06 +0800

    fix(amis-saas-7727): 代码编写问题修正
    Change-Id: I42d5b784787917bd270590ae5575cf34be26e0cd

--- packages/amis-editor-core/src/store/node.ts
+++ packages/amis-editor-core/src/store/node.ts
@@ -525,7 +525,7 @@ export const EditorNode = types
 
     function getClosestParentByType(type: string): EditorNodeType | void {
       let node = self;
-      while ((node = node.parent)) {
+      while (node === node.parent) {
         if (node.schema.type === type) {
           return node as EditorNodeType;
         }
