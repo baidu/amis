59339030da4163ef79d4e7d112f5a0b1afc69afc
sansiro <sansiro@sansiro.me>
2022-11-23 10:29:57 +0800

    chore: input-city chained-select 单测补充 (#5777)
    * input-city
    
    * chained-select
    
    * add

--- packages/amis/__tests__/renderers/Form/chainedSelect.test.tsx
+++ packages/amis/__tests__/renderers/Form/chainedSelect.test.tsx
@@ -1,7 +1,13 @@
-import React = require('react');
+/**
+ * 组件名称：Chained-Select 链式下拉框
+ * 单测内容：
+ * 1. 基础使用
+ */
+
 import {
   render,
   waitFor,
+  fireEvent,
   waitForElementToBeRemoved
 } from '@testing-library/react';
 import '../../../src';
@@ -9,6 +15,52 @@ import {render as amisRender} from '../../../src';
 import {makeEnv, wait} from '../../helper';
 
 test('Renderer:chained-select', async () => {
+  const fetcher = jest.fn().mockImplementation((config: any) => {
+    return new Promise(resolve => {
+      const level = parseInt(config.query.level, 10) || 0;
+      const maxLevel = parseInt(config.query.maxLevel, 10) || 0;
+      if (level >= maxLevel) {
+        resolve({
+          status: 200,
+          headers: {},
+          data: {
+            status: 0,
+            data: null
+          }
+        });
+      } else {
+        resolve({
+          status: 200,
+          headers: {},
+          data: {
+            status: 0,
+            msg: '',
+            data: [
+              {
+                label: `A ${level}`,
+                value: 'a'
+              },
+
+              {
+                label: `B ${level}`,
+                value: 'b'
+              },
+
+              {
+                label: `C ${level}`,
+                value: 'c'
+              },
+
+              {
+                label: `D ${level}`,
+                value: 'd'
+              }
+            ]
+          }
+        });
+      }
+    });
+  });
   const {container, findByText, getByText, getByTestId} = render(
     amisRender(
       {
@@ -20,68 +72,51 @@ test('Renderer:chained-select', async () => {
             name: 'select3',
             type: 'chained-select',
             label: '级联下拉',
-            source:
-              '/api/mock2/options/chainedOptions?parentId=$parentId&level=$level&maxLevel=4',
+            source: '/api/xxx?parentId=$parentId&level=$level&maxLevel=4',
             value: 'a,b'
           }
         ]
       },
       {},
       makeEnv({
-        async fetcher(config: any): Promise<any> {
-          const level = parseInt(config.query.level, 10) || 0;
-          const maxLevel = parseInt(config.query.maxLevel, 10) || 0;
-          if (level >= maxLevel) {
-            return {
-              status: 200,
-              headers: {},
-              data: {
-                status: 0,
-                data: null
-              }
-            };
-          } else {
-            return {
-              status: 200,
-              headers: {},
-              data: {
-                status: 0,
-                msg: '',
-                data: [
-                  {
-                    label: `A ${level}`,
-                    value: 'a'
-                  },
-
-                  {
-                    label: `B ${level}`,
-                    value: 'b'
-                  },
-
-                  {
-                    label: `C ${level}`,
-                    value: 'c'
-                  },
-
-                  {
-                    label: `D ${level}`,
-                    value: 'd'
-                  }
-                ]
-              }
-            };
-          }
-        }
+        fetcher
       })
     )
   );
 
   await waitFor(() => {
+    expect(fetcher).toBeCalledTimes(3);
+    expect(fetcher.mock.calls[2][0].query).toMatchObject({
+      parentId: 'b',
+      level: 2,
+      maxLevel: '4'
+    });
     expect(getByText('B 1')).toBeInTheDocument();
     expect(
       container.querySelector('[data-testid="spinner"]')
     ).not.toBeInTheDocument();
   });
+
+  fireEvent.click(getByText('请选择'));
+  expect(getByText('D 2')).toBeInTheDocument();
+  fireEvent.click(getByText('D 2'));
+
+  await wait(100);
+  expect(fetcher).toBeCalledTimes(4);
+  expect(fetcher.mock.calls[3][0].query).toMatchObject({
+    parentId: 'd',
+    level: 3,
+    maxLevel: '4'
+  });
+
+  fireEvent.click(getByText('请选择'));
+  expect(getByText('C 3')).toBeInTheDocument();
+  fireEvent.click(getByText('C 3'));
+
+  await wait(100);
+  expect(fetcher).toBeCalledTimes(5);
+  fireEvent.click(getByText('请选择'));
+  expect(getByText('未找到任何结果')).toBeInTheDocument();
 
   expect(container).toMatchSnapshot();
 });
