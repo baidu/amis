08536ab30f4e418a2f16ad7cb812dce5e97898ba
sansiro <sansiro@sansiro.me>
2022-11-23 10:20:15 +0800

    fix: inputTable组件中嵌套Combo时数据域覆盖问题 (#5791)
    * fix: inputTable 值覆盖问题修复
    
    * 单测补充

--- packages/amis/__tests__/renderers/Form/inputTable.test.tsx
+++ packages/amis/__tests__/renderers/Form/inputTable.test.tsx
@@ -1,4 +1,3 @@
-import React = require('react');
 import {
   render,
   screen,
@@ -134,13 +133,14 @@ test('Renderer: input-table with default value column', async () => {
 
   fireEvent.click(getByText('Submit'));
 
-  waitFor(() => {
+  // 不 await 会把错误报到本文件 别的测试中
+  await waitFor(() => {
     expect(onSubmitCallbackFn).toHaveBeenCalledTimes(1);
     expect(onSubmitCallbackFn.mock.calls[0][0]).toEqual({
       table: [
-        {a: 'a1', b: 'b1', c: 'a1'},
-        {a: 'a2', b: 'b2', c: 'a2'},
-        {a: 'a3', b: 'b3', c: 'a3'}
+        {a: 'a1', b: 'b1' /* c: 'a1' */},
+        {a: 'a2', b: 'b2' /* c: 'a2' */},
+        {a: 'a3', b: 'b3' /* c: 'a3' */}
       ]
     });
   });
@@ -194,4 +194,95 @@ test('Renderer:input table add', async () => {
   // TODO: 这里不对，难道是点击出错了
 
   // expect(container).toMatchSnapshot();
+});
+
+test('Renderer:input-table with combo column', async () => {
+  const onSubmit = jest.fn();
+  const {container, findByText, findByPlaceholderText} = render(
+    amisRender(
+      {
+        type: 'form',
+        submitText: 'SubmitBtn',
+        body: [
+          {
+            type: 'input-table',
+            name: 'table',
+            columns: [
+              {
+                type: 'combo',
+                name: 'q1',
+                label: 'combo column',
+                items: [
+                  {
+                    type: 'input-number',
+                    name: 'score',
+                    label: false,
+                    placeholder: '请手动输入分数'
+                  },
+                  {
+                    type: 'input-text',
+                    name: 'comment',
+                    label: false,
+                    placeholder: '请手动输入意见'
+                  }
+                ],
+                multiple: false
+              }
+            ]
+          }
+        ],
+        data: {
+          table: [
+            {
+              q2: '1',
+              q1: {
+                score: 99
+              }
+            }
+          ]
+        }
+      },
+      {onSubmit},
+      makeEnv({})
+    )
+  );
+
+  const submitBtn = await findByText('SubmitBtn');
+  fireEvent.click(submitBtn);
+
+  await wait(100);
+  expect(onSubmit).toHaveBeenCalled();
+  expect(onSubmit.mock.calls[0][0]).toEqual({
+    table: [
+      {
+        q2: '1',
+        q1: {
+          score: 99
+        }
+      }
+    ]
+  });
+
+  const commentInput = await findByPlaceholderText('请手动输入意见');
+  fireEvent.change(commentInput, {
+    target: {value: 'this is comment msg!'}
+  });
+  // input-table 中套 combo。多次 lazy change， 所以时间需要长点
+  await wait(1000);
+  expect(container).toMatchSnapshot();
+  fireEvent.click(submitBtn);
+
+  await wait(300);
+  expect(onSubmit).toBeCalledTimes(2);
+  expect(onSubmit.mock.calls[1][0]).toEqual({
+    table: [
+      {
+        q2: '1',
+        q1: {
+          score: 99,
+          comment: 'this is comment msg!'
+        }
+      }
+    ]
+  });
 });
