8ce95bfbf08c9610dab67b46516626b8c3b2ca5f
wibetter <365533093@qq.com>
2022-11-28 16:01:23 +0800

    fix(amis-saas-8381): 细节调整
    Change-Id: I74793261c7d7776fd731c5977ed69fa1b65910ad

--- packages/amis-editor/src/renderer/OptionControl.tsx
+++ packages/amis-editor/src/renderer/OptionControl.tsx
@@ -796,7 +796,7 @@ export default class OptionControl extends React.Component<
           </div>
         ) : null}
 
-        {(source === 'api' || source === 'apicenter')  && this.renderApiPanel()}
+        {this.renderApiPanel()}
       </div>
     );
   }
