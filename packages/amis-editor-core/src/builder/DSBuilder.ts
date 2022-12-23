fbc6c319ac2d3d64424b1295803f82802fdef242
tooeast <sansiro@sansiro.me>
2022-12-23 17:28:49 +0800

    [amis-saas-8350]列配置修改
    Change-Id: I6aea8e705ec01000652851fdd7e353a5bc01106a

--- packages/amis-editor-core/src/builder/DSBuilder.ts
+++ packages/amis-editor-core/src/builder/DSBuilder.ts
@@ -249,10 +249,10 @@ export abstract class DSBuilder {
     setting: any;
   }): SchemaObject | void;
 
-  abstract makeTableColumnsByFields(
-    fields: any[],
-    relations: any[]
-  ): SchemaObject[];
+  abstract makeTableColumnsByFields<T, U>(
+    fields: T,
+    relations: U
+  ): Array<SchemaObject>;
 
   /**
    * 当前上下文中使用的字段
