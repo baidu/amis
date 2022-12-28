53212740b4737d25e34be08ab2c01622a6beed88
tooeast <sansiro@sansiro.me>
2022-12-28 17:02:43 +0800

    [amis-saas-8350]类型问题修复
    Change-Id: Icfe4b332136c227b954efca6d75799a9714cdd34

--- packages/amis-editor/src/util.ts
+++ packages/amis-editor/src/util.ts
@@ -7,6 +7,6 @@ export function isCrudContext(
   ctx: BaseEventContext | RendererInfoResolveEventContext
 ) {
   return ['crud', 'crud2'].includes(
-    ctx?.schema?.name ?? ctx?.schema?.$$editor?.rendere?.name
+    ctx?.schema?.name ?? ctx?.schema?.$$editor?.renderer?.name
   );
 }
