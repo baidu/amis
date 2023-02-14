ae1c01de8ba931528d07c97f5515d54263e9e862
jinye <jinye@baidu.com>
2023-02-14 14:33:44 +0800

    feat: Status 组件新增source属性,兼容旧版map和labelMap属性,优化渲染Icon的方式

--- packages/amis/src/renderers/Status.tsx
+++ packages/amis/src/renderers/Status.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import merge from 'lodash/merge';
-import {Renderer, RendererProps} from 'amis-core';
+import assign from 'lodash/assign';
+import {generateIcon, isPureVariable, Renderer, RendererProps, resolveVariableAndFilter} from 'amis-core';
 import {filter} from 'amis-core';
-import cx from 'classnames';
 import {Icon} from 'amis-ui';
 import {BaseSchema} from '../Schema';
 import {getPropValue} from 'amis-core';
+
+export interface StatusSource {
+  [propName: string]: {
+    icon?: string;
+    label?: string;
+    color?: string;
+    className?: string;
+  }
+}
 
 /**
  * 状态展示控件。
@@ -25,6 +34,7 @@ export interface StatusSchema extends BaseSchema {
 
   /**
    * 状态图标映射关系
+   * @deprecated 已废弃，2.7.3 废弃，兼容中
    * @default {
    *    0: 'svg-fail',
    *    1: 'svg-success',
@@ -41,7 +51,7 @@ export interface StatusSchema extends BaseSchema {
 
   /**
    * 文字映射关系
-   *
+   * @deprecated 已废弃，2.7.3 废弃，兼容中
    * @default {
    *     success: '成功',
    *     pending: '运行中',
@@ -53,6 +63,13 @@ export interface StatusSchema extends BaseSchema {
   labelMap?: {
     [propName: string]: string;
   };
+
+  /**
+   * 新版配置映射源的字段
+   * 可以兼容新版icon并且配置颜色
+   * 2.7.3 新增
+   */
+  source?: StatusSource;
 }
 
 export interface StatusProps
@@ -81,67 +98,135 @@ export class StatusField extends React.Component<StatusProps, object> {
   };
 
   render() {
-    const {className, style, placeholder, classnames: cx, data} = this.props;
+    const {
+      defaultValue,
+      className,
+      style,
+      placeholder,
+      classnames: cx,
+      data
+    } = this.props;
     const map = merge(StatusField.defaultProps.map, this.props?.map);
     const labelMap = merge(
       StatusField.defaultProps.labelMap,
       this.props?.labelMap
     );
 
-    let value = getPropValue(this.props);
-    let viewValue: React.ReactNode = (
-      <span className="text-muted" key="status-value">
-        {placeholder}
-      </span>
-    );
-    let wrapClassName: string = '';
+    // 兼容旧版
+    let oldSource: StatusSource = {};
+    map && Object.entries(map).forEach(([value, icon]) => {
+      if (value === '' || value == null) {
+        return;
+      }
+      if (!oldSource[value]) {
+        oldSource[value] = {icon};
+      } else {
+        oldSource[value] = {...oldSource[value], icon};
+      }
+    });
+    labelMap && Object.entries(labelMap).forEach(([value, label]) => {
+      if (value === '' || value == null) {
+        return;
+      }
+      if (!oldSource[value]) {
+        oldSource[value] = {label};
+      } else {
+        oldSource[value] = {...oldSource[value], label};
+      }
+    });
 
-    if (value != undefined && value !== '' && map) {
+    // 合并source
+    let source = this.props.source || {};
+    if (isPureVariable(source)) {
+      source = resolveVariableAndFilter(source, data, '| raw');
+    }
+    source = assign(oldSource, source);
+
+    // 获取默认值，支持表达式
+    let value = getPropValue(this.props);
+    if (defaultValue && isPureVariable(defaultValue)) {
+      value = resolveVariableAndFilter(defaultValue, data, '| raw');
+    }
+
+    if (value != undefined && value !== '') {
       if (typeof value === 'boolean') {
         value = value ? 1 : 0;
       } else if (/^\d+$/.test(value)) {
         value = parseInt(value, 10) || 0;
       }
+    }
 
-      wrapClassName = `StatusField--${value}`;
-      let itemClassName = filter(map[value], data) || '';
-      let svgIcon: string = '';
+    let status = source[value] || {};
 
-      itemClassName = itemClassName.replace(
-        /\bsvg-([^\s|$]+)\b/g,
-        (_: string, icon: string) => {
-          svgIcon = icon;
-          return 'icon';
-        }
+    if (!status.icon && !status.label) {
+      return (
+        <span className={cx(
+          'StatusField',
+          className
+        )} style={style}>
+          <span className="text-muted" key="status-value">
+            {placeholder}
+          </span>
+        </span>
       );
+    }
 
+    let wrapClassName: string = '';
+
+    // icon element
+    let iconElement = null;
+    if (status.icon) {
+      wrapClassName = `StatusField--${value}`;
+      let icon = status.icon;
+      let svgIcon: string = '';
+      let itemClassName: string = '';
+
+      if (typeof icon === 'string') {
+        icon = filter(icon, data) || '';
+        itemClassName = icon.replace(
+          /\bsvg-([^\s|$]+)\b/g,
+          (_: string, svgName: string) => {
+            svgIcon = svgName;
+            return 'icon';
+          }
+        );
+      }
+
+      // 兼容 默认icon 和 旧版 iconfont icon
       if (svgIcon) {
-        viewValue = (
+        iconElement = (
           <Icon
             icon={svgIcon}
             className={cx('Status-icon icon', itemClassName)}
             key="icon"
           />
         );
-      } else if (itemClassName) {
-        viewValue = (
-          <i className={cx('Status-icon', itemClassName)} key="icon" />
-        );
-      }
-
-      if (labelMap && labelMap[value]) {
-        viewValue = [
-          viewValue,
-          <span className={cx('StatusField-label')} key="label">
-            {filter(labelMap[value], data)}
-          </span>
-        ];
+      } else {
+        iconElement = generateIcon(cx, icon, 'Status-icon');
       }
     }
 
+    let labelElement = null;
+    if (status.label !== '' && status.label != null) {
+      labelElement = (
+        <span className={cx('StatusField-label')} key="label">
+          {filter('' + status.label, data)}
+        </span>
+      );
+    }
+
     return (
-      <span className={cx('StatusField', wrapClassName, className)} style={style}>
-        {viewValue}
+      <span className={cx(
+        'StatusField',
+        wrapClassName,
+        className,
+        status.className
+      )} style={{
+        ...style,
+        ...(status.color ? {color: filter(status.color, data)} : {})
+      }}>
+        {iconElement}
+        {labelElement}
       </span>
     );
   }
