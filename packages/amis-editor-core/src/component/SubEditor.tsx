0071722e8895e9963c2c17f531961776d7d359a2
igrowp <164761965@qq.com>
2023-03-09 11:56:06 +0800

    amis-saas-9981 子编辑器中的公式编辑器中没有页面变量信息
    Change-Id: Ic6f723249a2d5831598bf1ebcb28624122d7c6bf

--- packages/amis-editor-core/src/component/SubEditor.tsx
+++ packages/amis-editor-core/src/component/SubEditor.tsx
@@ -107,6 +107,23 @@ export class SubEditor extends React.Component<SubEditorProps> {
         subEditorContext?.data
       );
     }
+    const variables: any = [
+      ...(manager.config?.variables || []).filter(
+        item => item.title !== '页面变量'
+      ),
+      // 解决打开子编辑器 公式输入框汇总没有页面变量的问题
+      {
+        name: 'pageParams',
+        title: '页面变量',
+        parentId: 'root',
+        order: 0,
+        schema: {
+          type: 'object',
+          $id: 'pageParams',
+          properties: {}
+        }
+      }
+    ];
     return {
       size: 'full',
       title: store.subEditorContext?.title,
@@ -154,7 +171,7 @@ export class SubEditor extends React.Component<SubEditorProps> {
                     iframeUrl={config.iframeUrl}
                     ctx={store.ctx}
                     schemas={manager.config?.schemas}
-                    variables={manager.config?.variables}
+                    variables={variables}
                     amisEnv={amisEnv || config.amisEnv}
                     appLocale={config.appLocale}
                     i18nEnabled={config.i18nEnabled}
