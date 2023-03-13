4ff7ca9224da49f22fe5da20366829f9f43104dd
pianruijie <pianruijie@baidu.com>
2023-03-13 19:59:48 +0800

    [amis-saas-10410] fix
    Change-Id: Id7765278111e9f474497d8d14f88504388b16e11

--- packages/amis-editor/src/tpl/common.tsx
+++ packages/amis-editor/src/tpl/common.tsx
@@ -111,7 +111,6 @@ setSchemaTpl(
     pipeOut: (v: string) => (v ? v : undefined)
   })
 );
-
 setSchemaTpl('formulaControl', (schema: object = {}) => {
   return {
     type: 'ae-formulaControl',
@@ -383,25 +382,6 @@ setSchemaTpl('icon', {
   clearable: true,
   description: ''
 });
-
-setSchemaTpl(
-  'remark',
-  remarkTpl({
-    name: 'remark',
-    label: '控件提示',
-    labelRemark:
-      '在输入控件旁展示提示，注意控件宽度需设置，否则提示触发图标将自动换行'
-  })
-);
-
-setSchemaTpl(
-  'labelRemark',
-  remarkTpl({
-    name: 'labelRemark',
-    label: '标题提示',
-    labelRemark: '在标题旁展示提示'
-  })
-);
 
 setSchemaTpl(
   'valueFormula',
