69b1cb3f2997a6a5f474478204ab8f779e4338ae
wanglinfang2014 <w.l.fang@foxmail.com>
2023-03-15 19:52:20 +0800

    fix: table单元格支持自定义样式、crud支持本地数据快速过滤 (#6353)
    Co-authored-by: wanglinfang <wanglinfang@baidu.com>

--- packages/amis/src/renderers/Table/index.tsx
+++ packages/amis/src/renderers/Table/index.tsx
@@ -172,6 +172,13 @@ export type TableColumnObject = {
    * 表格列单元格是否可以获取父级数据域值，默认为true，该配置对当前列内单元格生效
    */
   canAccessSuperData?: boolean;
+
+  /**
+   * 单元格内部组件自定义样式 style作为单元格自定义样式的配置
+   */
+  innerStyle?: {
+    [propName: string]: any;
+  };
 };
 
 export type TableColumnWithType = SchemaObject & TableColumnObject;
