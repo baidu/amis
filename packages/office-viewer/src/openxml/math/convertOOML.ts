868f00e29dc495fc1b1bebf7b4245ed547bddf09
wuduoyi <wuduoyi@baidu.com>
2023-03-23 19:54:40 +0800

    修复编译报错

--- packages/office-viewer/src/openxml/math/convertOOML.ts
+++ packages/office-viewer/src/openxml/math/convertOOML.ts
@@ -3,10 +3,9 @@ import {xsl} from './xsl';
  * 将 officel 的公式 xml 转成 mathml
  */
 
-const xsltProcessor = new XSLTProcessor();
-xsltProcessor.importStylesheet(xsl);
-
 export function convertOOXML(element: Element) {
+  const xsltProcessor = new XSLTProcessor();
+  xsltProcessor.importStylesheet(xsl);
   const fragment = xsltProcessor.transformToFragment(element, document);
   return fragment;
 }
