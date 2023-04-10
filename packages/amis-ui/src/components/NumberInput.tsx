7b06f729afaf97831ad84478c92efd52f059712b
qinhaoyan <30946345+qinhaoyan@users.noreply.github.com>
2023-04-10 13:11:27 +0800

    fix: 修复number组件ts报错

--- packages/amis-ui/src/components/NumberInput.tsx
+++ packages/amis-ui/src/components/NumberInput.tsx
@@ -76,15 +76,15 @@ export interface NumberProps extends ThemeProps {
   inputControlClassName?: string;
 }
 
-export class NumberInput extends React.Component<NumberProps, any> {
-  static defaultProps:
-    | Pick<NumberProps, 'step' | 'readOnly' | 'borderMode' | 'resetValue'>
-    | {focused: boolean} = {
+export class NumberInput extends React.Component<any, any> {
+  static defaultProps: Pick<
+    NumberProps,
+    'step' | 'readOnly' | 'borderMode' | 'resetValue'
+  > = {
     step: 1,
     readOnly: false,
     borderMode: 'full',
-    resetValue: '',
-    focused: false
+    resetValue: ''
   };
 
   /**
