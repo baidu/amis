a881946f225ecedfb7154f9ba7b82f30bcdef20c
wuduoyi <wuduoyi@baidu.com>
2023-04-12 15:14:47 +0800

    修复编译报错

--- packages/ooxml-viewer/src/render/renderGeom.ts
+++ packages/ooxml-viewer/src/render/renderGeom.ts
@@ -2,7 +2,7 @@ import {presetShape} from '../openxml/word/drawing/presetShape';
 import {Geom} from '../openxml/word/drawing/Geom';
 import {ShapePr} from '../openxml/word/drawing/ShapeProperties';
 import {shapeToSVG} from '../openxml/word/drawing/svg/shapeToSVG';
-import {WPSStyle} from 'src/openxml/word/wps/WPSStyle';
+import {WPSStyle} from '../openxml/word/wps/WPSStyle';
 
 export function renderGeom(
   geom: Geom,
