4ddb8a2940b66629fec3311137cffd99c48a6937
Chrysochrome <15140725+Chrysochrome@users.noreply.github.com>
2023-04-13 13:24:19 +0800

    feat: LocationPicker 添加 AMap 支持

--- packages/amis-core/src/utils/helper.ts
+++ packages/amis-core/src/utils/helper.ts
@@ -1429,6 +1429,18 @@ export function loadScript(src: string) {
   });
 }
 
+export function loadStyle(href: string) {
+  return new Promise<void>((ok, fail) => {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.onerror = reason => fail(reason);
+    link.onload = () => ok();
+
+    link.href = href;
+    document.head.appendChild(link);
+  });
+}
+
 export class SkipOperation extends Error {}
 
 /**
