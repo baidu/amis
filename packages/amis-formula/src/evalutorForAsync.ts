1d6baa0e456f759cadb7d532a776761125c11a36
lvxiaojiao <lvxiaojiao@baidu.com>
2023-05-19 22:07:18 +0800

    fix:过滤器filter解析错误

--- packages/amis-formula/src/evalutorForAsync.ts
+++ packages/amis-formula/src/evalutorForAsync.ts
@@ -66,7 +66,6 @@ export class AsyncEvaluator extends (Evaluator as any) {
       context.filter = filter;
 
       const argsRes = await runSequence(filter.args, async item => {
-        // await promise;
         if (item?.type === 'mixed') {
           const res = await runSequence(item.body, item =>
             typeof item === 'string' ? item : this.evalute(item)
