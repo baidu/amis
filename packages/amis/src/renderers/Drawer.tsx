0a471fddc98d52c840352f2e58daa75f2be3d025
liaoxuezhi <2betop.cn@gmail.com>
2023-05-20 09:34:04 +0800

    添加 isAlive 检测

--- packages/amis/src/renderers/Drawer.tsx
+++ packages/amis/src/renderers/Drawer.tsx
@@ -430,7 +430,7 @@ export default class Drawer extends React.Component<DrawerProps> {
 
   handleExited() {
     const {lazySchema, store, statusStore} = this.props;
-    statusStore?.resetAll();
+    statusStore && isAlive(statusStore) && statusStore.resetAll();
     if (isAlive(store)) {
       store.reset();
       store.setEntered(false);
