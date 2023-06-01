433aa9d991bd3182b677eb560c0c513f12a3804f
2betop <2betop.cn@gmail.com>
2023-06-01 10:31:00 +0800

    chore: 调整测试用例

--- packages/amis/__tests__/helper.tsx
+++ packages/amis/__tests__/helper.tsx
@@ -80,3 +80,45 @@ export function formatStyleObject(style: string | null, px2number = true) {
 
   return res;
 }
+
+/**
+ * This function searches for the every react-aria SSR ids in a given HTMLElement node and replace every attribute values with a static id
+ *
+ * This can be usefull when you're trying to generate a snapshot of components using react-aria under the hood
+ *
+ * @ex :
+ * ```
+ * const { container } = render(<Component />);
+ *
+ * replaceReactAriaIds(container);
+ * ```
+ *
+ * @param container The HTMLElement node to search for SSR ids
+ */
+export function replaceReactAriaIds(container: HTMLElement) {
+  const selectors = ['id', 'for', 'aria-labelledby'];
+  const ariaSelector = (el: string) => `[${el}^="react-aria"]`;
+  const regexp = /react-aria\d+-\d+/g;
+  const staticId = 'static-id';
+
+  /**
+   * keep a map of the replaceIds to keep the matching between input "id" and label "for" attributes
+   */
+  const attributesMap: Record<string, string> = {};
+
+  container
+    .querySelectorAll(selectors.map(ariaSelector).join(', '))
+    .forEach((el, index) => {
+      selectors.forEach(selector => {
+        const attr = el.getAttribute(selector);
+
+        if (attr?.match(regexp)) {
+          const newAttr = `${staticId}-${index}`;
+
+          el.setAttribute(selector, attributesMap[attr] || newAttr);
+
+          attributesMap[attr] = newAttr;
+        }
+      });
+    });
+}
