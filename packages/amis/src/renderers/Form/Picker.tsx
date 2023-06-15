ef341cd7666e8481d61bcdea08ac4383144b9528
2betop <2betop.cn@gmail.com>
2023-06-15 19:58:11 +0800

    chore: 补充 picker 值右侧删除按钮不触发 change 事件问题 Close: #7084

--- packages/amis/src/renderers/Form/Picker.tsx
+++ packages/amis/src/renderers/Form/Picker.tsx
@@ -320,7 +320,7 @@ export default class PickerControl extends React.PureComponent<
     }
   }
 
-  removeItem(index: number) {
+  async removeItem(index: number) {
     const {
       selectedOptions,
       joinValues,
@@ -328,10 +328,11 @@ export default class PickerControl extends React.PureComponent<
       delimiter,
       valueField,
       onChange,
-      multiple
+      multiple,
+      dispatchEvent
     } = this.props;
     const items = selectedOptions.concat();
-    items.splice(index, 1);
+    const [option] = items.splice(index, 1);
 
     let value: any = items;
 
@@ -345,6 +346,14 @@ export default class PickerControl extends React.PureComponent<
         : (items[0] && items[0][valueField || 'value']) || '';
     } else {
       value = multiple ? items : items[0];
+    }
+
+    const rendererEvent = await dispatchEvent(
+      'change',
+      resolveEventData(this.props, {value, option, selectedItems: option})
+    );
+    if (rendererEvent?.prevented) {
+      return;
     }
 
     onChange(value);
