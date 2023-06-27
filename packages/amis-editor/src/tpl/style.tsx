60cf06bf4fd038dd37ff1d30213affe355903acc
wibetter <365533093@qq.com>
2023-06-27 16:52:31 +0800

    fix(amis-editor): theme:common tpl 增加collapsed配置项

--- packages/amis-editor/src/tpl/style.tsx
+++ packages/amis-editor/src/tpl/style.tsx
@@ -593,7 +593,8 @@ setSchemaTpl('theme:size', (option: any = {}) => {
 
 setSchemaTpl(
   'theme:common',
-  (exclude: string[] | string, include: string[]) => {
+  (exclude: string[] | string, include: string[], collapsed?: boolean) => {
+    const curCollapsed = collapsed ?? false; // 默认都展开
     // key统一转换成Kebab case，eg: boxShadow => bos-shadow
     exclude = (
       exclude ? (Array.isArray(exclude) ? exclude : [exclude]) : []
@@ -631,6 +632,7 @@ setSchemaTpl(
       {
         header: '布局',
         key: 'layout',
+        collapsed: curCollapsed,
         body: [
           {
             type: 'style-display',
@@ -641,10 +643,12 @@ setSchemaTpl(
       },
       {
         title: '自定义样式',
+        collapsed: curCollapsed,
         body: styles
       },
       {
         title: '样式源码',
+        collapsed: curCollapsed,
         body: [
           {
             type: 'theme-cssCode',
