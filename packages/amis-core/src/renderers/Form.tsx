b3ba5982a70434208781b7444d02c227fb8276ff
殷亮辉 <yinlianghui@hotoa.com>
2023-07-10 14:27:03 +0000

    form组件的messages属性支持表达式 #7443

--- packages/amis-core/src/renderers/Form.tsx
+++ packages/amis-core/src/renderers/Form.tsx
@@ -838,7 +838,7 @@ export default class Form extends React.Component<FormProps, object> {
         this.hooks['validate'] || [],
         forceValidate,
         throwErrors,
-        __(messages && messages.validateFailed)
+        __(filter(messages && messages.validateFailed, store.data))
       )
       .then((result: boolean) => {
         if (result) {
@@ -883,7 +883,7 @@ export default class Form extends React.Component<FormProps, object> {
     return store.submit(
       fn,
       this.hooks['validate'] || [],
-      __(messages && messages.validateFailed),
+      __(filter(messages && messages.validateFailed, store.data)),
       validateErrCb,
       throwErrors
     );
@@ -1163,8 +1163,8 @@ export default class Form extends React.Component<FormProps, object> {
 
           return store
             .saveRemote(action.api || (api as Api), values, {
-              successMessage: saveSuccess,
-              errorMessage: saveFailed,
+              successMessage: filter(saveSuccess, store.data),
+              errorMessage: filter(saveFailed, store.data),
               onSuccess: async (result: Payload) => {
                 // result为提交接口返回的内容
                 const dispatcher = await dispatchEvent(
@@ -1298,10 +1298,16 @@ export default class Form extends React.Component<FormProps, object> {
       return store
         .saveRemote(action.api as Api, data, {
           successMessage: __(
-            (action.messages && action.messages.success) || saveSuccess
+            filter(
+              (action.messages && action.messages.success) || saveSuccess,
+              store.data
+            )
           ),
           errorMessage: __(
-            (action.messages && action.messages.failed) || saveFailed
+            filter(
+              (action.messages && action.messages.failed) || saveFailed,
+              store.data
+            )
           )
         })
         .then(async response => {
