193cdf17a0d34ea5af6e356910a0e5d1ea7dd408
qinhaoyan <30946345+qinhaoyan@users.noreply.github.com>
2023-07-19 20:43:47 +0800

    更新快照

--- packages/amis-ui/src/components/icons.tsx
+++ packages/amis-ui/src/components/icons.tsx
@@ -256,8 +256,6 @@ export function Icon({
   vendor,
   cx: iconCx,
   onClick = () => {},
-  onMouseEnter = () => {},
-  onMouseLeave = () => {},
   style
 }: {
   icon: string;
@@ -282,9 +280,7 @@ export function Icon({
         (icon.props as any).className
       ),
       style,
-      onClick,
-      onMouseEnter,
-      onMouseLeave
+      onClick
     });
   }
 
@@ -310,8 +306,6 @@ export function Icon({
     return (
       <div
         onClick={onClick}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
         className={cx(iconContent, className, classNameProp)}
         ref={refFn}
         style={style}
@@ -325,8 +319,6 @@ export function Icon({
     return (
       <Component
         onClick={onClick}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
         className={cx(className, `icon-${icon}`, classNameProp)}
         // @ts-ignore
         icon={icon}
@@ -353,8 +345,6 @@ export function Icon({
     return (
       <svg
         onClick={onClick}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
         className={cx('icon', 'icon-object', className, classNameProp)}
         style={style}
       >
@@ -371,8 +361,6 @@ export function Icon({
     return (
       <img
         onClick={onClick}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
         className={cx(`${classPrefix}Icon`, className, classNameProp)}
         src={icon}
         style={style}
@@ -398,8 +386,6 @@ export function Icon({
     return (
       <i
         onClick={onClick}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
         className={cx(icon, className, classNameProp, iconPrefix)}
         style={style}
       />
