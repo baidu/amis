278fd9b897b21a8dd634dd538081168a613544ee
YangQi <yangfong2022@qq.com>
2023-07-27 11:31:49 +0800

    docs: 调整 showSteps 注释

--- packages/amis/src/renderers/Form/InputNumber.tsx
+++ packages/amis/src/renderers/Form/InputNumber.tsx
@@ -25,7 +25,7 @@ import {supportStatic} from './StaticHoc';
 
 /**
  * 数字输入框
- * 文档：https://aisuda.bce.baidu.com/amis/zh-CN/components/form/number
+ * 文档：https://aisuda.bce.baidu.com/amis/zh-CN/components/form/input-number
  */
 export interface NumberControlSchema extends FormBaseControlSchema {
   type: 'input-number';
@@ -51,17 +51,20 @@ export interface NumberControlSchema extends FormBaseControlSchema {
   precision?: number;
 
   /**
-   * 默认当然是
+   * 是否显示上下点击按钮
    */
   showSteps?: boolean;
+  
   /**
    * 边框模式，全边框，还是半边框，或者没边框。
    */
   borderMode?: 'full' | 'half' | 'none';
+  
   /**
    * 前缀
    */
   prefix?: string;
+  
   /**
    * 后缀
    */
@@ -103,38 +106,47 @@ export interface NumberProps extends FormControlProps {
   max?: number | string;
   min?: number | string;
   step?: number;
+  
   /**
    *  精度
    */
   precision?: number;
+  
   /**
    * 边框模式，全边框，还是半边框，或者没边框。
    */
   borderMode?: 'full' | 'half' | 'none';
+  
   /**
    * 前缀
    */
   prefix?: string;
+  
   /**
    * 后缀
    */
   suffix?: string;
+  
   /**
    * 是否千分分隔
    */
   kilobitSeparator?: boolean;
+  
   /**
    * 只读
    */
   readOnly?: boolean;
+  
   /**
    * 启用键盘行为，即通过上下方向键控制是否生效
    */
   keyboard?: boolean;
+  
   /**
    * 输入框为基础输入框还是加强输入框
    */
   displayMode?: 'base' | 'enhance';
+  
   /**
    * 是否是大数，如果是的话输入输出都将是字符串
    */
