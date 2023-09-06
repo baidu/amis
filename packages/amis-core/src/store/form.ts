8fe8cf1a2e262ee8acfe16f60a2ef026f3a6e743
2betop <2betop.cn@gmail.com>
2023-09-06 16:00:44 +0800

    chore: 优化 Table 渲染性能

--- packages/amis-core/src/store/form.ts
+++ packages/amis-core/src/store/form.ts
@@ -49,7 +49,7 @@ export const FormStore = ServiceStore.named('FormStore')
       while (pool.length) {
         const current = pool.shift()!;
 
-        if (current.storeType === 'FormItemStore') {
+        if (current.storeType === 'FormItemStore' && !current.isControlled) {
           formItems.push(current);
         } else if (
           !['ComboStore', 'TableStore', 'FormStore'].includes(current.storeType)
