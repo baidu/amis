b84cfbc2e6ff8f9f0210865cfc01325987e5d50f
liaoxuezhi <2betop.cn@gmail.com>
2023-09-08 16:09:43 +0800

    fix: 修复 crud 自动过滤器中 select 下拉 zIndex 问题 Close: #8031 (#8039)

--- packages/amis/src/renderers/Table/index.tsx
+++ packages/amis/src/renderers/Table/index.tsx
@@ -1740,7 +1740,8 @@ export default class Table extends React.Component<TableProps, object> {
         onSubmit: onSearchableFromSubmit,
         onInit: onSearchableFromInit,
         formStore: undefined,
-        data: query ? createObject(data, query) : data
+        data: query ? createObject(data, query) : data,
+        popOverContainer: this.getPopOverContainer
       }
     );
   }
