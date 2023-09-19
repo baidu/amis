d141cbaeb7ebb3abf0ec545a67207c9a7439730f
2betop <2betop.cn@gmail.com>
2023-09-19 20:54:47 +0800

    fix: 修复表单项 requiredOn 初始成立不起作用的问题

--- packages/amis-core/src/renderers/wrapControl.tsx
+++ packages/amis-core/src/renderers/wrapControl.tsx
@@ -192,7 +192,7 @@ export function wrapControl<
               ),
               id,
               type,
-              required,
+              required: props.required || required,
               unique,
               value,
               isValueSchemaExp: isExpression(value),
