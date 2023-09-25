cfcee5425ad32b68b502eb4898f65f581d9fb2f3
wutong25 <wutong25@baidu.com>
2023-09-25 19:10:11 +0800

    fix: 修改组合穿梭器默认脚手架id

--- packages/amis-editor/src/plugin/Form/TabsTransfer.tsx
+++ packages/amis-editor/src/plugin/Form/TabsTransfer.tsx
@@ -79,7 +79,7 @@ export class TabsTransferPlugin extends BasePlugin {
         children: [
           {
             label: '法师',
-            value: 'fashi',
+            value: 'fashi2',
             children: [
               {
                 label: '诸葛亮',
@@ -89,7 +89,7 @@ export class TabsTransferPlugin extends BasePlugin {
           },
           {
             label: '战士',
-            value: 'zhanshi',
+            value: 'zhanshi2',
             children: [
               {
                 label: '曹操',
@@ -103,7 +103,7 @@ export class TabsTransferPlugin extends BasePlugin {
           },
           {
             label: '打野',
-            value: 'daye',
+            value: 'daye2',
             children: [
               {
                 label: '李白',
