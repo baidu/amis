d0724faff2c5232908094ef243f90c97dd9ac342
yinchunyu <yinchunyu@baidu.com>
2023-10-23 14:35:08 +0800

    feat: 组件增加searchKeywords, 使amis文档中的组件名称可以在编辑器中搜索到

--- packages/amis-editor/src/plugin/Each.tsx
+++ packages/amis-editor/src/plugin/Each.tsx
@@ -19,6 +19,7 @@ export class EachPlugin extends BasePlugin {
   isListComponent = true;
   memberImmutable = true;
   description = '功能渲染器，可以基于现有变量循环输出渲染器。';
+  searchKeywords = '循环渲染器';
   tags = ['功能'];
   icon = 'fa fa-repeat';
   pluginIcon = 'each-plugin';
--- packages/amis-editor/src/plugin/Form/InputCity.tsx
+++ packages/amis-editor/src/plugin/Form/InputCity.tsx
@@ -33,6 +33,7 @@ export class CityControlPlugin extends BasePlugin {
   icon = 'fa fa-building-o';
   pluginIcon = 'input-city-plugin';
   description = '可配置是否选择区域或者城市';
+  searchKeywords = '城市选择器';
   docLink = '/amis/zh-CN/components/form/input-city';
   tags = ['表单项'];
   scaffold = {
