2c110c964279e99d5f5b4d8e36c1ab06b5a579ea
RUNZE LU <36724300+lurunze1226@users.noreply.github.com>
2023-11-24 12:26:21 +0800

    feat: Switch组件的onText和offText支持Schema配置 (#8816)

--- packages/amis/__tests__/renderers/Form/switch.test.tsx
+++ packages/amis/__tests__/renderers/Form/switch.test.tsx
@@ -110,3 +110,51 @@ test('Renderer:Switch with loading status', async () => {
 
   expect(loadingDom?.length).toEqual(2);
 });
+
+test('Renderer:Switch onText & offText schema', async () => {
+  const {container} = render(
+    amisRender(
+      {
+        type: 'form',
+        body: [
+          {
+            "name": "switch",
+            "type": "switch",
+            "onText": [
+              {
+                "type": "icon",
+                "icon": "fa fa-plane",
+                "vendor": "",
+                "className": "mr-1"
+              },
+              {
+                "type": "tpl",
+                "tpl": "飞行模式"
+              }
+            ],
+            "offText": [
+              {
+                "type": "icon",
+                "icon": "fa fa-plane",
+                "vendor": "",
+                "className": "mr-1"
+              },
+              {
+                "type": "tpl",
+                "tpl": "飞行模式"
+              }
+            ]
+          }
+        ],
+        actions: []
+      },
+      {},
+      makeEnv()
+    )
+  );
+
+  const text = container.querySelector('.cxd-Switch > span.text')!;
+
+  /** offText的Schema包含了2个元素 */
+  expect(text?.childNodes?.length).toEqual(2);
+});
