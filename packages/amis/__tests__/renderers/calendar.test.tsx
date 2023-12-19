7883c50d0ab8cf8d5c252f14d13002c7e484da06
hongyang03 <hongyang03@baidu.com>
2023-12-19 21:06:39 +0800

    fix: 代码优化

--- packages/amis/__tests__/renderers/calendar.test.tsx
+++ packages/amis/__tests__/renderers/calendar.test.tsx
@@ -1,8 +1,8 @@
 import React = require('react');
-import {render, fireEvent, waitFor} from '@testing-library/react';
+import {render} from '@testing-library/react';
 import '../../src';
 import {render as amisRender} from '../../src';
-import {makeEnv, wait} from '../helper';
+import {makeEnv} from '../helper';
 
 test('Renderer:calendar', () => {
   const {container} = render(
