f1a370ce2a9ea03dbfb2b3b3ec1824c5523a8ce3
wibetter <365533093@qq.com>
2024-04-09 11:05:44 +0800

    fix(amis-core): 事件动作/组件动作/未找到组件提示增加组件ID信息，方便知晓哪个组件未找到。

--- packages/amis-core/src/actions/CmptAction.ts
+++ packages/amis-core/src/actions/CmptAction.ts
@@ -59,8 +59,7 @@ export class CmptAction implements RendererAction {
       : renderer;
     // 如果key指定了，但是没找到组件，则报错
     if (key && !component) {
-      const msg =
-        '尝试执行一个不存在的目标组件动作，请检查目标组件非隐藏状态，且正确指定了componentId或componentName';
+      const msg = `尝试执行一个不存在的目标组件动作（${key}），请检查目标组件非隐藏状态，且正确指定了componentId或componentName`;
       if (action.ignoreError === false) {
         throw Error(msg);
       } else {
