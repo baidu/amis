440aa85062782a414cdc7a0b305e8cc33bbdb54f
fujianchao <fujianchao@baidu.com>
2024-04-26 11:50:39 +0800

    fix: 修复右击菜单后报错

--- packages/amis-editor-core/src/component/Editor.tsx
+++ packages/amis-editor-core/src/component/Editor.tsx
@@ -421,6 +421,7 @@ export default class Editor extends Component<EditorProps> {
   // 右键菜单
   @autobind
   async handleContextMenu(e: React.MouseEvent<HTMLElement>) {
+    e.persist();
     await closeContextMenus();
     let targetId: string = '';
     let region = '';
