bbb1d64c3547c2e1494715aca3b41697af8537e7
2betop <2698393+2betop@users.noreply.github.com>
2024-05-21 11:22:11 +0800

    chore: amis-core 产出存在 esm/node_modules 问题处理

--- packages/amis-core/rollup.config.js
+++ packages/amis-core/rollup.config.js
@@ -39,7 +39,8 @@ const external = id => {
         'prop-types',
         'markdown-it-html5-media',
         'mdurl',
-        'uc.micro'
+        'uc.micro',
+        '@babel/runtime'
       ])
       .map(value =>
         value.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&').replace(/-/g, '\\x2d')
