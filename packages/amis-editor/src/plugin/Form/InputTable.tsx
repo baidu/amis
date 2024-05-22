8d19de388f890374d0a1051379a2c52eeada860a
2betop <2698393+2betop@users.noreply.github.com>
2024-05-22 19:20:11 +0800

    fix: 修复编辑器模拟数据逻辑导致的组件无限刷新问题

--- packages/amis-editor/src/plugin/Form/InputTable.tsx
+++ packages/amis-editor/src/plugin/Form/InputTable.tsx
@@ -1411,29 +1411,33 @@ export class TableControlPlugin extends BasePlugin {
     ]);
   };
 
-  filterProps(props: any) {
-    const arr = resolveArrayDatasource(props);
+  filterProps(props: any, node: EditorNodeType) {
+    if (!node.state.value) {
+      const arr = resolveArrayDatasource(props);
+      let value: Array<any> = [];
 
-    /** 可 */
-    if (!Array.isArray(arr) || !arr.length) {
-      const mockedData: any = {};
+      /** 可 */
+      if (!Array.isArray(arr) || !arr.length) {
+        const mockedData: any = {};
 
-      if (Array.isArray(props.columns)) {
-        props.columns.forEach((column: any) => {
-          /** 可编辑状态下不写入 Mock 数据，避免误导用户 */
-          if (column.name && !props.editable) {
-            setVariable(mockedData, column.name, mockValue(column));
-          }
-        });
+        if (Array.isArray(props.columns)) {
+          props.columns.forEach((column: any) => {
+            /** 可编辑状态下不写入 Mock 数据，避免误导用户 */
+            if (column.name && !props.editable) {
+              setVariable(mockedData, column.name, mockValue(column));
+            }
+          });
+        }
+
+        value = repeatArray(mockedData, 1).map((item, index) => ({
+          ...item,
+          id: index + 1
+        }));
+      } else {
+        // 只取10条预览，否则太多卡顿
+        value = arr.slice(0, 10);
       }
-
-      props.value = repeatArray(mockedData, 1).map((item, index) => ({
-        ...item,
-        id: index + 1
-      }));
-    } else {
-      // 只取10条预览，否则太多卡顿
-      props.value = arr.slice(0, 10);
+      node.updateState({value});
     }
 
     return {
