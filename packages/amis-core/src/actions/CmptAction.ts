a9fb1c2b4797d774496d1a74e3293e5da4332f74
allenve <yupeng.fe@qq.com>
2024-07-29 10:45:01 +0800

    feat: 增加自定义表单项校验方案

--- packages/amis-core/src/actions/CmptAction.ts
+++ packages/amis-core/src/actions/CmptAction.ts
@@ -99,7 +99,9 @@ export class CmptAction implements RendererAction {
     ) {
       const {dispatchEvent, data} = component?.props || {};
       try {
-        const valid = await component?.props.onValidate?.();
+        const valid =
+          (await component?.props.onValidate?.()) ||
+          (await component?.validate?.());
         if (valid) {
           event.setData(
             createObject(event.data, {
