a057a7fc4b1a28aadac7625edb0da0a901ca74b6
SiynMa <mrtatekii33@gmail.com>
2024-08-13 23:15:56 +0800

    feat: 完善表达式缓存逻辑

--- packages/amis-core/__tests__/utils/visitedCache.test.ts
+++ packages/amis-core/__tests__/utils/visitedCache.test.ts
@@ -1,6 +1,6 @@
 import VisitedCache from '../../src/utils/visitedCache';
 
-describe('测试访问次数缓存store', () => {
+describe('VisitedCache', () => {
   let cache: VisitedCache<string, number>;
 
   beforeEach(() => {
@@ -33,22 +33,75 @@ describe('测试访问次数缓存store', () => {
     expect(cache.get('b')).toBeUndefined();
   });
 
-  it('should update the least visited entry when an existing entry is set', () => {
+  it('should handle cache misses correctly', () => {
+    expect(cache.get('non-existent-key')).toBeUndefined();
+  });
+
+  it('should handle multiple entries with the same visit count', () => {
     cache.set('a', 1);
     cache.set('b', 2);
     cache.set('c', 3);
+    cache.set('d', 4);
+    cache.set('e', 5);
 
-    // Access 'b' to increase its visit count
+    // Access 'a', 'b', 'c' to make them have the same visit count
+    cache.get('a');
     cache.get('b');
+    cache.get('c');
 
-    cache.set('a', 10);
+    cache.set('f', 6);
 
-    expect(cache.get('a')).toBe(10);
+    // 动态清理的个数为1，清理掉最少访问最旧的d项
+    expect(cache.get('a')).toBe(1);
     expect(cache.get('b')).toBe(2);
-    expect(cache.get('c')).toBeUndefined();
+    expect(cache.get('c')).toBe(3);
+    expect(cache.get('d')).toBeUndefined();
+    expect(cache.get('e')).toBe(5);
+    expect(cache.get('f')).toBe(6);
   });
 
-  it('should handle cache misses correctly', () => {
-    expect(cache.get('non-existent-key')).toBeUndefined();
+  it('should handle dynamic release count', () => {
+    cache = new VisitedCache<string, number>(10, 1 / 5);
+    // 释放数量为 10 * 1/5 >> 0 || 1 = 2
+
+    cache.set('a', 1);
+    cache.set('b', 2);
+    cache.set('c', 3);
+    cache.set('d', 4);
+    cache.set('e', 5);
+    cache.set('f', 6);
+    cache.set('g', 7);
+    cache.set('h', 8);
+    cache.set('i', 9);
+    cache.set('j', 10);
+
+    // 提升除了a之外的访问次数
+    cache.get('b');
+    cache.get('c');
+    cache.get('d');
+    cache.get('e');
+    cache.get('f');
+    cache.get('g');
+    cache.get('h');
+    cache.get('i');
+    cache.get('j');
+
+    // 再次提升高b的访问次数
+    cache.get('b');
+
+    // 此时应该清理掉两项,a和c
+    cache.set('k', 11);
+
+    expect(cache.get('a')).toBeUndefined();
+    expect(cache.get('b')).toBe(2);
+    expect(cache.get('c')).toBeUndefined();
+    expect(cache.get('d')).toBe(4);
+    expect(cache.get('e')).toBe(5);
+    expect(cache.get('f')).toBe(6);
+    expect(cache.get('g')).toBe(7);
+    expect(cache.get('h')).toBe(8);
+    expect(cache.get('i')).toBe(9);
+    expect(cache.get('j')).toBe(10);
+    expect(cache.get('k')).toBe(11);
   });
 });
--- packages/amis-core/src/utils/visitedCache.ts
+++ packages/amis-core/src/utils/visitedCache.ts
@@ -7,80 +7,149 @@ export type CacheEntry<V> = {
 };
 
 /**
- * 自动清理访问次数最少key的Map
- * //TODO 考虑上次访问时间？
- * @class CombinedCache
+ * 自动清理访问次数最少key的Map，次数相同时优先淘汰旧项
+ * 每次触发清理的计数基于容量的百分比
+ * @class VisitedCache
  * @template K - 缓存key的类型
  * @template V - 缓存value的类型
  */
-export default class CombinedCache<K, V> {
+export default class VisitedCache<K, V> {
   private capacity: number;
   private cache: Map<K, CacheEntry<V>>;
-  private leastVisitedKey: K | undefined;
-  private leastVisitedCount: number = Infinity;
+  private visitCountOrder: number[] = [];
+  private keyOrderMatrixForByCount: Record<number, K[]> = {};
+  private releaseCount: number;
 
   /**
-   * Creates an instance of CombinedCache
-   * @param {number} capacity - 最大缓存容量
+   *
+   * @param capacity 容量
+   * @param releasePercent 清理数量占容量百分比
    */
-  constructor(capacity: number) {
+  constructor(capacity: number, releasePercent?: number) {
     this.capacity = capacity;
     this.cache = new Map();
+    this.releaseCount = (this.capacity * (releasePercent || 1 / 8)) >> 0 || 1;
   }
 
   /**
-   * 从Map中获取value，并更新访问次数与最少访问项
+   * 更新现存项目的缓存顺序
+   * @param key
+   * @param entry
+   */
+  private updateCacheEntryOrder(
+    key: K,
+    entry: CacheEntry<V>,
+    nextVisitCount: number
+  ) {
+    const {visitCount: oldVisitCount} = entry;
+
+    const oldKeyOrder = this.keyOrderMatrixForByCount[oldVisitCount];
+
+    if (Array.isArray(oldKeyOrder)) {
+      // 从key为旧访问次数的顺序数组中删除
+      const oldKeyIndex = oldKeyOrder.indexOf(key);
+
+      if (oldKeyIndex !== -1) {
+        oldKeyOrder.splice(oldKeyIndex, 1);
+      }
+    }
+
+    // 相同访问量，更新访问顺序
+    this.keyOrderMatrixForByCount[nextVisitCount] = Array.isArray(
+      this.keyOrderMatrixForByCount[nextVisitCount]
+    )
+      ? [...this.keyOrderMatrixForByCount[nextVisitCount], key]
+      : [key];
+
+    entry.visitCount = nextVisitCount;
+
+    // 重新按照访问量排序
+    this.visitCountOrder = [
+      ...new Set([...this.visitCountOrder, nextVisitCount])
+    ].sort((a, b) => a - b);
+  }
+
+  /**
+   *
    * @param {K} key
    * @returns {(V | undefined)}
    */
   get(key: K): V | undefined {
     if (this.cache.has(key)) {
-      const entry = this.cache.get(key)!;
-      this.cache.delete(key);
-      this.cache.set(key, entry);
-      entry.visitCount++;
+      const entry = this.cache.get(key);
 
-      if (entry.visitCount < this.leastVisitedCount) {
-        this.leastVisitedKey = key;
-        this.leastVisitedCount = entry.visitCount;
+      if (entry !== undefined) {
+        const {visitCount} = entry;
+        // 更新访问顺序
+        this.updateCacheEntryOrder(key, entry, visitCount + 1);
+
+        return entry.value;
       }
-
-      return entry.value;
     }
     return undefined;
   }
 
   /**
+   *
    * @param {K} key
    * @param {V} value
    */
   set(key: K, value: V): void {
+    // 更新现存项的值和访问次数及新鲜度
     if (this.cache.has(key)) {
-      const entry = this.cache.get(key)!;
-      this.cache.delete(key);
-      entry.value = value;
-      entry.visitCount++;
+      const entry = this.cache.get(key);
 
-      // Update the least visited entry
-      if (entry.visitCount < this.leastVisitedCount) {
-        this.leastVisitedKey = key;
-        this.leastVisitedCount = entry.visitCount;
+      if (entry !== undefined) {
+        const {visitCount} = entry;
+
+        this.updateCacheEntryOrder(key, entry, visitCount + 1);
+
+        entry.value = value;
       }
-
-      this.cache.set(key, entry);
     } else {
+      // 先进行清理
       if (this.cache.size === this.capacity) {
-        // 删除最少访问项
-        if (this.leastVisitedKey) {
-          this.cache.delete(this.leastVisitedKey);
+        // TODO 依据最大容量的1/10进行释放,试试效果
+        const dynamicReleaseCount = this.releaseCount;
+
+        // const dynamicReleaseCount =
+        // 	(this.keyOrderMatrixForByCount[this.visitCountOrder[this.visitCountOrder.length - 1]].length / (this.capacity)) >>
+        // 		0 || 1
+
+        let findIndex = 0;
+        let released = 0;
+
+        while (
+          released < dynamicReleaseCount &&
+          findIndex <= this.visitCountOrder.length - 1
+        ) {
+          // 最少访问的次数
+          const targetCount = this.visitCountOrder[findIndex];
+          // 查看其中有没有项
+          const targetKeyOrder = this.keyOrderMatrixForByCount[targetCount];
+
+          if (!targetKeyOrder.length) {
+            findIndex++;
+          } else {
+            while (
+              targetKeyOrder.length > 0 &&
+              released < dynamicReleaseCount
+            ) {
+              this.cache.delete(targetKeyOrder.shift()!);
+              released++;
+            }
+          }
         }
       }
 
-      this.cache.set(key, {value, visitCount: 1});
+      const newEntry: CacheEntry<V> = {
+        visitCount: 1,
+        value
+      };
 
-      // 更新最少访问的key及次数
-      this.leastVisitedKey = key;
-      this.leastVisitedCount = 1;
+      this.cache.set(key, newEntry);
+
+      this.updateCacheEntryOrder(key, newEntry, 1);
     }
   }
 
