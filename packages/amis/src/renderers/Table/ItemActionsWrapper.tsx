6f12a855c773cdeaa4e1ca6a6b7a3250ffd874da
2betop <2698393+2betop@users.noreply.github.com>
2024-08-13 21:21:45 +0800

    fix: 修复 itemActdions 位置错误当表格内容太宽滚动到右侧时

--- packages/amis/src/renderers/Table/ItemActionsWrapper.tsx
+++ packages/amis/src/renderers/Table/ItemActionsWrapper.tsx
@@ -15,27 +15,53 @@ function ItemActionsWrapper(props: ItemActionsProps) {
   const store = props.store;
   const divRef = useRef<HTMLDivElement>(null);
 
+  const updatePosition = React.useCallback(
+    (id: string = store.hoverRow?.id || '') => {
+      const frame = divRef.current!.parentElement?.querySelector(
+        'table'
+      ) as HTMLElement;
+
+      const dom = frame?.querySelector(`tr[data-id="${id}"]`) as HTMLElement;
+      if (!dom) {
+        return;
+      }
+
+      const rect = dom.getBoundingClientRect();
+      const height = rect.height;
+      const top =
+        rect.top -
+        frame.getBoundingClientRect().top +
+        parseInt(getComputedStyle(frame)['marginTop'], 10);
+      divRef.current!.style.cssText += `top: ${top}px;height: ${height}px; left: ${
+        frame.parentElement!.scrollLeft
+      }px;`;
+    },
+    []
+  );
+
   useEffect(() => {
     const row = store.hoverRow!;
     if (!row) {
       return;
     }
 
-    const frame = divRef.current!.parentElement?.querySelector(
-      'table'
-    ) as HTMLElement;
-    const dom = frame?.querySelector(`tr[data-id="${row.id}"]`) as HTMLElement;
-    if (!dom) {
+    updatePosition(row.id);
+  }, [store.hoverRow?.id]);
+
+  useEffect(() => {
+    const frame = divRef.current!.parentElement as HTMLElement;
+    if (!frame) {
       return;
     }
-    const rect = dom.getBoundingClientRect();
-    const height = rect.height;
-    const top =
-      rect.top -
-      frame.getBoundingClientRect().top +
-      parseInt(getComputedStyle(frame)['marginTop'], 10);
-    divRef.current!.style.cssText += `top: ${top}px;height: ${height}px;`;
-  }, [store.hoverRow?.id]);
+    let onScroll = () => {
+      updatePosition(store.hoverRow?.id);
+    };
+
+    frame.addEventListener('scroll', onScroll);
+    return () => {
+      frame.removeEventListener('scroll', onScroll);
+    };
+  });
 
   return (
     <div className={cx('Table-itemActions-wrap')} ref={divRef}>
