f8f8f594c193fd47f886ac47e62e82087655b908
jinye <jinye@baidu.com>
2024-10-11 11:35:08 +0800

    feat: crud2支持配置移动端展示为卡片或自定义schema,支持上滑加载更多

---
+++ packages/amis-ui/src/hooks/use-on-screen.ts
@@ -0,0 +1,28 @@
+/**
+ * 检测元素是否在屏幕中
+ */
+import {useEffect, useState} from 'react';
+
+const useOnScreen = (ref: any) => {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting);
+    });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref]);
+
+  return isIntersecting;
+};
+
+export default useOnScreen;
