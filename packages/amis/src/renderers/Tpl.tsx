a36bfc8e3f9ed384a17f4f3c3d78d1421103a7af
2betop <2698393+2betop@users.noreply.github.com>
2024-10-11 17:06:41 +0800

    fix: 修复 tpl 渲染时序问题导致的渲染错误

--- packages/amis/src/renderers/Tpl.tsx
+++ packages/amis/src/renderers/Tpl.tsx
@@ -73,6 +73,7 @@ export class Tpl extends React.Component<TplProps, TplState> {
 
   dom: any;
   mounted: boolean;
+  sn: number = 0;
 
   constructor(props: TplProps) {
     super(props);
@@ -102,7 +103,13 @@ export class Tpl extends React.Component<TplProps, TplState> {
 
   @autobind
   async updateContent() {
+    let sn = ++this.sn;
     const content = await this.getAsyncContent();
+
+    // 解决异步时序问题，防止较早的运算覆盖较晚的运算结果
+    if (sn !== this.sn) {
+      return;
+    }
     this.mounted && this.setState({content});
   }
 
