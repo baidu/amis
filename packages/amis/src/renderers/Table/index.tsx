7e8554aceec3d2615a0843795570998c7c476141
kzl <kangzhanlei@qq.com>
2024-10-15 11:39:56 +0800

    fit： 解决表格行编辑的时候的弹窗，无法获取返回值的问题

--- packages/amis/src/renderers/Table/index.tsx
+++ packages/amis/src/renderers/Table/index.tsx
@@ -220,6 +220,7 @@ export type TableColumnWithType = SchemaObject & TableColumnObject;
 export type TableColumn = TableColumnWithType | TableColumnObject;
 
 type AutoFillHeightObject = Record<'height' | 'maxHeight', number>;
+
 /**
  * Table 表格渲染器。
  * 文档：https://aisuda.bce.baidu.com/amis/zh-CN/components/table
@@ -1025,7 +1026,7 @@ export default class Table extends React.Component<TableProps, object> {
     const {onAction} = this.props;
 
     // todo
-    onAction(e, action, ctx);
+    return onAction(e, action, ctx);
   }
 
   async handleCheck(item: IRow, value?: boolean, shift?: boolean) {
@@ -1375,6 +1376,7 @@ export default class Table extends React.Component<TableProps, object> {
   }
 
   tableUnWatchResize?: () => void;
+
   tableRef(ref: HTMLTableElement) {
     this.table = ref;
     isAlive(this.props.store) && this.props.store.setTable(ref);
