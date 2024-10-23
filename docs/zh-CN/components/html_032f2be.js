744636714db1ce145d174d88b89f954bd514d6ac
qkiroc <30946345+qkiroc@users.noreply.github.com>
2024-10-23 09:26:09 +0000

    Deploying to gh-pages from  @ 434a328619e6e8d477baff1bbf0cbd2cc2618d5f 🚀

---
+++ docs/zh-CN/components/html_032f2be.js
@@ -0,0 +1,27 @@
+amis.define('docs/zh-CN/components/html.md', function(require, exports, module, define) {
+
+  module.exports = {
+    "title": "Html",
+    "description": null,
+    "type": 0,
+    "group": "⚙ 组件",
+    "menuName": "Html",
+    "icon": null,
+    "order": 49,
+    "html": "<div class=\"markdown-body\"><h2><a class=\"anchor\" name=\"%E5%9F%BA%E6%9C%AC%E7%94%A8%E6%B3%95\" href=\"#%E5%9F%BA%E6%9C%AC%E7%94%A8%E6%B3%95\" aria-hidden=\"true\"><svg aria-hidden=\"true\" class=\"octicon octicon-link\" height=\"16\" version=\"1.1\" viewBox=\"0 0 16 16\" width=\"16\"><path d=\"M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z\"></path></svg></a>基本用法</h2><p>渲染一段 HTML</p>\n</div><div class=\"amis-preview\" style=\"min-height: undefinedpx\"><script type=\"text/schema\" undefined>{\n  \"body\": {\n    \"type\": \"html\",\n    \"html\": \"支持 <code>Html</code>\"\n  }\n}\n</script></div><div class=\"markdown-body\">\n<blockquote>\n<p>当需要获取数据域中变量时，使用 <a href=\"./tpl\">Tpl</a> 。</p>\n</blockquote>\n</div>",
+    "toc": {
+      "label": "目录",
+      "type": "toc",
+      "children": [
+        {
+          "label": "基本用法",
+          "fragment": "%E5%9F%BA%E6%9C%AC%E7%94%A8%E6%B3%95",
+          "fullPath": "#%E5%9F%BA%E6%9C%AC%E7%94%A8%E6%B3%95",
+          "level": 2
+        }
+      ],
+      "level": 0
+    }
+  };
+
+});
