cd1093f38b36337ca049a5ec26bbeb4d72e420a3
2betop <2698393+2betop@users.noreply.github.com>
2024-10-31 11:02:37 +0800

    fix: 修复获取 context 数据过时的问题

--- packages/amis-core/src/RootRenderer.tsx
+++ packages/amis-core/src/RootRenderer.tsx
@@ -95,16 +95,17 @@ export class RootRenderer extends React.Component<RootRendererProps> {
   componentDidUpdate(prevProps: RootRendererProps) {
     const props = this.props;
 
-    if (props.data !== prevProps.data) {
-      this.store.initData(props.data);
-    }
-
     if (props.location !== prevProps.location) {
       this.store.updateLocation(props.location, this.props.env?.parseLocation);
     }
 
     if (props.context !== prevProps.context) {
       this.store.updateContext(props.context);
+    }
+
+    // 一定要最后处理，否则 downStream 里面的上层数据 context 还是老的。
+    if (props.data !== prevProps.data) {
+      this.store.initData(props.data);
     }
   }
 
