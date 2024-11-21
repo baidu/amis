d938e410a9e78117282bbf8536284ab4aed9c24f
2betop <2698393+2betop@users.noreply.github.com>
2024-11-21 14:12:00 +0800

    fix: 修复 popOver 可能不显示的问题

--- packages/amis-core/src/components/Overlay.tsx
+++ packages/amis-core/src/components/Overlay.tsx
@@ -83,13 +83,7 @@ class Position extends React.Component<any, any> {
 
     if (!target || !target.offsetWidth) {
       // 靠这个 re-render 来重置 position
-      return this.setState({
-        ready: false,
-        positionLeft: 0,
-        positionTop: 0,
-        arrowOffsetLeft: null,
-        arrowOffsetTop: null
-      });
+      return this.setState({});
     }
 
     const watchTargetSizeChange = this.props.watchTargetSizeChange;
