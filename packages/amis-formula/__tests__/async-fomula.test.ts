d63fae411a1272784c8e54a649ccf2cf7f100612
liaoxuezhi <2698393+2betop@users.noreply.github.com>
2024-11-21 16:31:06 +0800

    chore: test case update

--- packages/amis-formula/__tests__/async-fomula.test.ts
+++ packages/amis-formula/__tests__/async-fomula.test.ts
@@ -342,9 +342,9 @@ test('formula:date', async () => {
       'CONCATENATE(STARTOF("2023-02-28", "day"), "," ,ENDOF("2023-02-28", "day"))'
     )
   ).toBe(
-    `${moment('2023-02-28').startOf('day').toDate()},${moment('2023-02-28')
+    `${moment('2023-02-28').startOf('day').format()},${moment('2023-02-28')
       .endOf('day')
-      .toDate()}`
+      .format()}`
   );
   expect(
     await evalFormual(
