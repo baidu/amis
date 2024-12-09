f5dff4afa1c9488843326388a37293130ad83953
qinhaoyan <qkiroc@126.com>
2024-12-09 17:27:11 +0800

    入场动画支持配置重复

--- packages/amis-core/src/components/Animations.tsx
+++ packages/amis-core/src/components/Animations.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import {useEffect, useRef} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {CSSTransition} from 'react-transition-group';
 import {Schema} from '../types';
 import {formateId} from '../utils';
@@ -15,98 +14,130 @@ function Animations({
   component: any;
   show: boolean;
 }) {
-  const animationClassNames = useRef<{
-    appear?: string;
-    enter?: string;
-    exit?: string;
-  }>({});
-  const animationTimeout = useRef<{
-    enter?: number;
-    exit?: number;
-  }>({});
-  const ref = useRef<HTMLDivElement>(null);
-  const [animationShow, setAnimationShow] = React.useState(false);
+  const idRef = useRef<string>(formateId(schema.id));
+  const id = idRef.current;
+  const {enter} = schema.animations || {};
+  const [animationShow, setAnimationShow] = React.useState(!enter?.inView);
+  const [placeholderShow, setPlaceholderShow] = React.useState(!!enter?.inView);
 
-  useEffect(() => {
+  const [animationClassNames] = useState(() => {
     const animations = schema?.animations;
+    const animationClassNames = {
+      appear: '',
+      enter: '',
+      exit: ''
+    };
     if (animations) {
-      let id = schema.id;
-      id = formateId(id);
       if (animations.enter) {
-        animationTimeout.current.enter =
-          ((animations.enter.duration || 1) + (animations.enter.delay || 0)) *
-          1000;
-        animationClassNames.current.enter = `${animations.enter.type}-${id}-enter`;
-        animationClassNames.current.appear = animationClassNames.current.enter;
+        animationClassNames.enter = `${animations.enter.type}-${id}-enter`;
+        animationClassNames.appear = animationClassNames.enter;
       }
       if (animations.exit) {
-        animationTimeout.current.exit =
+        animationClassNames.exit = `${animations.exit.type}-${id}-exit`;
+      }
+    }
+    return animationClassNames;
+  });
+  const [animationTimeout] = useState(() => {
+    const animations = schema?.animations;
+    const animationTimeout = {
+      enter: 1000,
+      exit: 1000
+    };
+    if (animations) {
+      if (animations.enter) {
+        animationTimeout.enter =
+          ((animations.enter.duration || 1) + (animations.enter.delay || 0)) *
+          1000;
+      }
+      if (animations.exit) {
+        animationTimeout.exit =
           ((animations.exit.duration || 1) + (animations.exit.delay || 0)) *
           1000;
-        animationClassNames.current.exit = `${animations.exit.type}-${id}-exit`;
       }
-      createAnimationStyle(id, animations);
     }
+    return animationTimeout;
+  });
+
+  useEffect(() => {
+    createAnimationStyle(id, schema.animations!);
     return () => {
       if (schema.animations) {
-        let {id} = schema;
-        id = formateId(id);
         styleManager.removeStyles(id);
       }
     };
   }, []);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries, observer) => {
-        entries.forEach(entry => {
+  function refFn(ref: HTMLDivElement) {
+    if (ref) {
+      const observer = new IntersectionObserver(
+        ([entry], observer) => {
           if (entry.isIntersecting) {
             setAnimationShow(true);
+            setPlaceholderShow(false);
             observer.disconnect();
           }
-        });
-      },
-      {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.1
+        },
+        {
+          root: null,
+          rootMargin: '0px',
+          threshold: 0.1
+        }
+      );
+      if (ref) {
+        observer.observe(ref);
       }
-    );
-    if (ref.current) {
-      observer.observe(ref.current);
-    }
-  }, [show]);
-
-  function addAnimationAttention(node: HTMLElement) {
-    const {attention} = schema.animations || {};
-    if (attention) {
-      let {id} = schema;
-      id = formateId(id);
-      node.classList.add(`${attention.type}-${id}-attention`);
     }
   }
-  function removeAnimationAttention(node: HTMLElement) {
+
+  function handleEntered(node: HTMLElement) {
+    const {attention, exit, enter} = schema.animations || {};
+    if (attention) {
+      node.classList.add(`${attention.type}-${id}-attention`);
+    }
+
+    if (exit?.outView || enter?.repeat) {
+      const observer = new IntersectionObserver(
+        ([entry], observer) => {
+          if (!entry.isIntersecting) {
+            setAnimationShow(false);
+            observer.disconnect();
+          }
+        },
+        {
+          root: null,
+          rootMargin: '0px',
+          threshold: 0.1
+        }
+      );
+      observer.observe(node);
+    }
+  }
+  function handleExit(node: HTMLElement) {
     const {attention} = schema.animations || {};
     if (attention) {
-      let {id} = schema;
-      id = formateId(id);
       node.classList.remove(`${attention.type}-${id}-attention`);
     }
   }
 
+  function handleExited() {
+    setPlaceholderShow(true);
+  }
+
   return (
     <>
-      {!animationShow && show && (
-        <div ref={ref} className="amis-animation-placeholder">
+      {!animationShow && show && placeholderShow && (
+        <div ref={refFn} className="amis-animation-placeholder">
           {component}
         </div>
       )}
       <CSSTransition
         in={animationShow && show}
-        timeout={animationTimeout.current}
-        classNames={animationClassNames.current}
-        onEntered={addAnimationAttention}
-        onExit={removeAnimationAttention}
+        timeout={animationTimeout}
+        classNames={animationClassNames}
+        onEntered={handleEntered}
+        onExit={handleExit}
+        onExited={handleExited}
         appear
         unmountOnExit
       >
