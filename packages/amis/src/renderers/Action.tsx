b18679795c087dd4e96095e0134b64f2538a5588
fujianchao <fujianchao@baidu.com>
2024-12-13 15:11:22 +0800

    feat: crud的卡片风格支持行点击，选择表格项，行排序事件

--- packages/amis/src/renderers/Action.tsx
+++ packages/amis/src/renderers/Action.tsx
@@ -993,6 +993,7 @@ export class ActionRenderer extends React.Component<ActionRendererProps> {
     let confirmText: string = '';
     // 有些组件虽然要求这里忽略二次确认，但是如果配了事件动作还是需要在这里等待二次确认提交才可以
     if (
+      !this.props.customVerify && // customVerify 外部透传过来的验证,勿删
       (!ignoreConfirm || hasOnEvent) &&
       action.confirmText &&
       env.confirm &&
