b02712698af7e02bac53eef6e8744959a937784a
Qnets <43038692+hzh11012@users.noreply.github.com>
2024-12-25 16:34:38 +0800

    fix: overrideSupportStatic函数未导出 (#11429)

--- packages/amis/src/index.tsx
+++ packages/amis/src/index.tsx
@@ -26,6 +26,7 @@ import type {TabsSchema} from './renderers/Tabs';
 import {availableLanguages as EditorAvailableLanguages} from './renderers/Form/Editor';
 import type {Action} from './types';
 import type {SchemaType} from './Schema';
+import {overrideSupportStatic} from './renderers/Form/StaticHoc';
 export * from './renderers/Form/IconPickerIcons';
 export * from './renderers/Form/IconSelectStore';
 
@@ -47,5 +48,6 @@ export {
   SchemaType,
   EditorAvailableLanguages,
   registerFilter,
-  registerFormula
+  registerFormula,
+  overrideSupportStatic
 };
