89e0f3d05f5764cd0dd412eab16ff8cf9a795af5
2betop <2698393+2betop@users.noreply.github.com>
2025-01-03 02:34:25 +0000

    Deploying to gh-pages from  @ a87143e884648028c079c3ac4aedb80e85dfe360 🚀

---
+++ packages/amis-ui/scss/helper/typography/_text-decoration_2611ab6.js
@@ -0,0 +1,14 @@
+amis.define('packages/amis-ui/scss/helper/typography/_text-decoration.md', function(require, exports, module, define) {
+
+  module.exports = {
+    "title": "Text Decoration",
+    "html": "<div class=\"markdown-body\"><table>\n<thead>\n<tr>\n<th>Class</th>\n<th>Properties</th>\n</tr>\n</thead>\n<tbody><tr>\n<td>underline</td>\n<td>text-decoration: underline</td>\n</tr>\n<tr>\n<td>line-through</td>\n<td>text-decoration: line-through</td>\n</tr>\n<tr>\n<td>no-underline</td>\n<td>text-decoration: none</td>\n</tr>\n</tbody></table>\n</div>",
+    "toc": {
+      "label": "目录",
+      "type": "toc",
+      "children": [],
+      "level": 0
+    }
+  };
+
+});
