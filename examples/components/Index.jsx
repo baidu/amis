0e91577c7fdff555ddbfce9fa18793b35c33096f
yangxu23 <yangxu23@baidu.com>
2025-01-15 13:39:37 +0800

    还原example/index

--- examples/components/Index.jsx
+++ examples/components/Index.jsx
@@ -1,43 +1,16 @@
 export default {
-  "type": "page",
-  "data": {
-    "images": [
-      {
-        "image": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395692722/4f3cb4202335.jpeg@s_0,w_216,l_1,f_jpg,q_80",
-        "src": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395692722/4f3cb4202335.jpeg",
-        "a": "aaa1",
-        "b": "bbb1"
-      },
-      {
-        "image": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395692942/d8e4992057f9.jpeg@s_0,w_216,l_1,f_jpg,q_80",
-        "src": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395692942/d8e4992057f9.jpeg",
-        "a": "aaa2",
-        "b": "bbb2"
-      },
-      {
-        "image": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395693148/1314a2a3d3f6.jpeg@s_0,w_216,l_1,f_jpg,q_80",
-        "src": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395693148/1314a2a3d3f6.jpeg",
-        "a": "aaa3",
-        "b": "bbb3"
-      },
-      {
-        "image": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395693379/8f2e79f82be0.jpeg@s_0,w_216,l_1,f_jpg,q_80",
-        "src": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395693379/8f2e79f82be0.jpeg",
-        "a": "aaa4",
-        "b": "bbb4"
-      },
-      {
-        "image": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395693566/552b175ef11d.jpeg@s_0,w_216,l_1,f_jpg,q_80",
-        "src": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395693566/552b175ef11d.jpeg",
-        "a": "aaa5",
-        "b": "bbb5"
-      }
-    ]
+  type: 'page',
+  title: '标题',
+  remark: {
+    title: '标题',
+    body: '这是一段描述问题，注意到了没，还可以设置标题。而且只有点击了才弹出来。',
+    icon: 'question-mark',
+    placement: 'right',
+    trigger: 'click',
+    rootClose: true
   },
-  "body": {
-    "type": "images",
-    "sortType": "l-t-2m",
-    "source": "${images}",
-    "enlargeAble": true
-  }
-}
+  body: '内容部分. 可以使用 \\${var} 获取变量。如: `\\$date`: ${date}',
+  aside: '边栏部分',
+  toolbar: '工具栏',
+  initApi: '/api/mock2/page/initData'
+};
