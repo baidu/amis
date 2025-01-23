ef6972b36dd4d5ff32bea94c52c4db01a3ef4937
wangxiufeng01 <wangxiufeng01@baidu.com>
2025-01-23 09:58:34 +0800

    feat: 时间轴升级支持横向交叉展示以及样式升级

---
+++ packages/amis-ui/src/components/Timeline/type.ts
@@ -0,0 +1,9 @@
+export type DotSize = 'sm' | 'md' | 'lg' | 'xl';
+
+export enum DirectionMode {
+  left = 'left',
+  right = 'right',
+  top = 'top',
+  bottom = 'bottom',
+  alternate = 'alternate'
+}
