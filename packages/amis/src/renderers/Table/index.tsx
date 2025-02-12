d3acfe650f1a9f2986e5a4a86740e8b1b268ffbd
chenwenhao <chenwenhao@lattebank.com>
2025-02-12 20:18:50 +0800

    fix: #11535 crud的source切换后分页失效问题修复

--- packages/amis/src/renderers/Table/index.tsx
+++ packages/amis/src/renderers/Table/index.tsx
@@ -703,24 +703,25 @@ export default class Table extends React.Component<TableProps, object> {
     const value = getPropValue(props, (props: TableProps) => props.items);
     let rows: Array<object> = [];
     let updateRows = false;
-    const resolved = resolveVariableAndFilter(source, props.data, '| raw');
-    const prev = prevProps
-      ? resolveVariableAndFilter(source, prevProps.data, '| raw')
-      : null;
 
     // 要严格比较前后的value值，否则某些情况下会导致循环update无限渲染
-    if (
-      Array.isArray(value) &&
-      (!prevProps ||
+    if (Array.isArray(value)) {
+      if (
+        !prevProps ||
         !isEqual(
           getPropValue(prevProps, (props: TableProps) => props.items),
           value
-        ) ||
-        resolved !== prev)
-    ) {
-      updateRows = true;
-      rows = value;
+        )
+      ) {
+        updateRows = true;
+        rows = value;
+      }
     } else if (typeof source === 'string') {
+      const resolved = resolveVariableAndFilter(source, props.data, '| raw');
+      const prev = prevProps
+        ? resolveVariableAndFilter(source, prevProps.data, '| raw')
+        : null;
+
       if (prev === resolved) {
         updateRows = false;
       } else {
