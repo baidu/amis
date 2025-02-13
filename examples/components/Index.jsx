cf3724152cfccd37bd96eba224e7d1277e7c2ed9
yangxu23 <yangxu23@baidu.com>
2025-02-13 14:39:22 +0800

    feat:修改comment提出的问题

--- examples/components/Index.jsx
+++ examples/components/Index.jsx
@@ -9,19 +9,19 @@ export default {
         "b": "bbb1"
       },
       {
-        "image": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395692942/d8e4992057f9.jpeg@s_0,w_216,l_1,f_jpg,q_80",
+        "image": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395692722/4f3cb4202335.jpeg@s_0,w_216,l_1,f_jpg,q_80",
         "source": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395692722/4f3cb4202335.jpeg",
         "a": "aaa2",
         "b": "bbb2"
       },
       {
-        "image": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395693148/1314a2a3d3f6.jpeg@s_0,w_216,l_1,f_jpg,q_80",
+        "image": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395692722/4f3cb4202335.jpeg@s_0,w_216,l_1,f_jpg,q_80",
         "source": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395692722/4f3cb4202335.jpeg",
         "a": "aaa3",
         "b": "bbb3"
       },
       {
-        "image": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395693379/8f2e79f82be0.jpeg@s_0,w_216,l_1,f_jpg,q_80",
+        "image": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395692722/4f3cb4202335.jpeg@s_0,w_216,l_1,f_jpg,q_80",
         "source": "https://internal-amis-res.cdn.bcebos.com/images/2020-1/1578395692722/4f3cb4202335.jpeg",
         "a": "aaa4",
         "b": "bbb4"
@@ -37,6 +37,7 @@ export default {
   "body": {
     "type": "images",
     "source": "${images}",
+    "thumbMode": "cover",
     "originalSrc": "${source}",
     "sortType": "sm-ss-sss-m",
     "enlargeAble": true
