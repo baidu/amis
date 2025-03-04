e0c6e0a79d189d2c9075768039f84a33862575c6
2betop <2698393+2betop@users.noreply.github.com>
2025-03-04 10:59:29 +0800

    style: 调整 crud 的自动高度撑开模式的样式, 去掉多余滚动条

--- packages/amis/src/renderers/Table/index.tsx
+++ packages/amis/src/renderers/Table/index.tsx
@@ -948,7 +948,7 @@ export default class Table<
     const tableContentHeight = heightValue
       ? `${heightValue}px`
       : `${Math.round(
-          viewportHeight - tableContentTop - tableContentBottom
+          viewportHeight - tableContentTop - tableContentBottom - 1
         )}px`;
 
     tableContent.style[heightField] = tableContentHeight;
