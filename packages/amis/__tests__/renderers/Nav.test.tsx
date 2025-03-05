7e543aba76f94a32c661b729c1c16a2e955bfcf7
wangxiufeng01 <wangxiufeng01@baidu.com>
2025-03-05 16:22:32 +0800

    fix: 修复事件动作失效，增加文档说明，增加UT

--- packages/amis/__tests__/renderers/Nav.test.tsx
+++ packages/amis/__tests__/renderers/Nav.test.tsx
@@ -1216,6 +1216,184 @@ test('Render Nav with panel mode', async () => {
       makeEnv({})
     )
   );
-  let panelItemsSelector = '.cxd-Nav-Menu-panel-wrapper .cxd-Nav-Menu-panel-group-item';
+  let panelItemsSelector =
+    '.cxd-Nav-Menu-panel-wrapper .cxd-Nav-Menu-panel-group-item';
   expect(container.querySelectorAll(panelItemsSelector).length).toBe(2);
+});
+
+// 15. 横向滚动模式
+test('Render Nav with swipe mode', async () => {
+  const {container} = render(
+    amisRender(
+      {
+        type: 'nav',
+        stacked: false,
+        overflow: {
+          enable: true,
+          mode: 'swipe'
+        },
+        links: [
+          {
+            label: 'Nav 1',
+            to: '?to=nav1'
+          },
+          {
+            label: 'Nav 2',
+            to: '?to=nav2'
+          },
+          {
+            label: 'Nav 3',
+            to: '?to=nav3'
+          },
+          {
+            label: 'Nav 4',
+            to: '?to=nav4'
+          },
+          {
+            label: 'Nav 5',
+            to: '?to=nav5'
+          },
+          {
+            label: 'Nav 6',
+            to: '?to=nav6'
+          },
+          {
+            label: 'Nav 7',
+            to: '?to=nav7'
+          },
+          {
+            label: 'Nav 8',
+            to: '?to=nav8'
+          },
+          {
+            label: 'Nav 9',
+            to: '?to=nav9'
+          },
+          {
+            label: 'Nav 10',
+            to: '?to=nav10'
+          },
+          {
+            label: 'Nav 11',
+            to: '?to=nav11'
+          },
+          {
+            label: 'Nav 12',
+            to: '?to=nav12'
+          },
+          {
+            label: 'Nav 13',
+            to: '?to=nav13',
+            children: [
+              {
+                label: 'Nav 14',
+                to: '?to=nav14',
+                children: [
+                  {
+                    label: 'Nav 20',
+                    to: '?to=nav20'
+                  },
+                  {
+                    label: 'Nav 21',
+                    to: '?to=nav21'
+                  },
+                  {
+                    label: 'Nav 22',
+                    to: '?to=nav22'
+                  },
+                  {
+                    label: 'Nav 23',
+                    to: '?to=nav23'
+                  },
+                  {
+                    label: 'Nav 24',
+                    to: '?to=nav24'
+                  },
+                  {
+                    label: 'Nav 25',
+                    to: '?to=nav25'
+                  }
+                ]
+              },
+              {
+                label: 'Nav 15',
+                to: '?to=nav15'
+              },
+              {
+                label: 'Nav 16',
+                to: '?to=nav16'
+              },
+              {
+                label: 'Nav 17',
+                to: '?to=nav17',
+                children: [
+                  {
+                    label: 'Nav 26',
+                    to: '?to=nav26'
+                  },
+                  {
+                    label: 'Nav 27',
+                    to: '?to=nav27'
+                  },
+                  {
+                    label: 'Nav 28',
+                    to: '?to=nav28'
+                  },
+                  {
+                    label: 'Nav 29',
+                    to: '?to=nav29'
+                  },
+                  {
+                    label: 'Nav 30',
+                    to: '?to=nav30'
+                  },
+                  {
+                    label: 'Nav 31',
+                    to: '?to=nav31'
+                  }
+                ]
+              },
+              {
+                label: 'Nav 18',
+                to: '?to=nav18'
+              },
+              {
+                label: 'Nav 19',
+                to: '?to=nav19'
+              }
+            ]
+          }
+        ]
+      },
+      {},
+      makeEnv({})
+    )
+  );
+
+  expect(container).toMatchSnapshot();
+
+  // const horizontalRootDom = container.querySelector(
+  //   '.cxd-Nav-horizontal .cxd-HorizontalScroll-Container'
+  // );
+
+  // await wait(1000);
+  // expect(horizontalRootDom).toBeTruthy();
+
+  // expect(horizontalRootDom?.children.length).toBe(3);
+
+  // const [leftArrowDom, navDomWrapper, rightArrowDom] = Array.from(
+  //   horizontalRootDom?.children || []
+  // );
+
+  // expect(
+  //   leftArrowDom.classList.contains('cxd-HorizontalScroll-arrow--left')
+  // ).toBeTruthy();
+  // expect(
+  //   rightArrowDom.classList.contains('cxd-HorizontalScroll-arrow--right')
+  // ).toBeTruthy();
+
+  // const navRoot = navDomWrapper.querySelector('.cxd-Nav-Menu-root');
+  // expect(navRoot).toBeTruthy();
+
+  // TODO: 模拟点击，测量offsetLeft
 });
