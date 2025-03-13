e8514ab45247348b6dc57dfb0ab3fef3115dba51
2betop <2698393+2betop@users.noreply.github.com>
2025-03-13 13:56:05 +0000

    Deploying to gh-pages from  @ fd650d1725a9195ba547db0a826468a162684577 🚀

---
+++ packages/amis-ui/scss/helper/box-alignment/_justify-self_284d300.js
@@ -0,0 +1,14 @@
+amis.define('packages/amis-ui/scss/helper/box-alignment/_justify-self.md', function(require, exports, module, define) {
+
+  module.exports = {
+    "title": "Jsutify Self",
+    "html": "<div class=\"markdown-body\"><table>\n<thead>\n<tr>\n<th>Class</th>\n<th>Properties</th>\n</tr>\n</thead>\n<tbody><tr>\n<td>justify-self-auto</td>\n<td>justify-self: auto</td>\n</tr>\n<tr>\n<td>justify-self-start</td>\n<td>justify-self: start</td>\n</tr>\n<tr>\n<td>justify-self-end</td>\n<td>justify-self: end</td>\n</tr>\n<tr>\n<td>justify-self-center</td>\n<td>justify-self: center</td>\n</tr>\n<tr>\n<td>justify-self-stretch</td>\n<td>justify-self: stretch</td>\n</tr>\n</tbody></table>\n</div>",
+    "toc": {
+      "label": "目录",
+      "type": "toc",
+      "children": [],
+      "level": 0
+    }
+  };
+
+});
