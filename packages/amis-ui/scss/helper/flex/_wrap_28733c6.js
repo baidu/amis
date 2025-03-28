a8c43838be30f4940d8c95f72989e40ab76e01bb
2betop <2698393+2betop@users.noreply.github.com>
2025-03-28 07:22:21 +0000

    Deploying to gh-pages from  @ a91217bbbe4a84383b08a32cdd2532d632530ce7 🚀

---
+++ packages/amis-ui/scss/helper/flex/_wrap_28733c6.js
@@ -0,0 +1,14 @@
+amis.define('packages/amis-ui/scss/helper/flex/_wrap.md', function(require, exports, module, define) {
+
+  module.exports = {
+    "title": "Flex Wrap",
+    "html": "<div class=\"markdown-body\"><table>\n<thead>\n<tr>\n<th>Class</th>\n<th>Properties</th>\n</tr>\n</thead>\n<tbody><tr>\n<td>flex-wrap</td>\n<td>flex-wrap: wrap</td>\n</tr>\n<tr>\n<td>flex-wrap-reverse</td>\n<td>flex-wrap: wrap-reverse</td>\n</tr>\n<tr>\n<td>flex-nowrap</td>\n<td>flex-wrap: nowrap</td>\n</tr>\n</tbody></table>\n</div>",
+    "toc": {
+      "label": "目录",
+      "type": "toc",
+      "children": [],
+      "level": 0
+    }
+  };
+
+});
