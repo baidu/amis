c70f3910c1d33d4639f387e28926a8732342bf31
F-jianchao <161407305+F-jianchao@users.noreply.github.com>
2025-04-03 10:16:29 +0800

    fix: 修复crud点击行会跳出个error,修复crud2多选后样式无效的问题 (#11807)

--- packages/amis/src/renderers/PopOver.tsx
+++ packages/amis/src/renderers/PopOver.tsx
@@ -170,9 +170,10 @@ export const HocPopOver =
         const e = event.currentTarget;
         // 如果内容不超出，不需要弹出
         if (
-          this.getClassName() === 'ellipsis' &&
-          e &&
-          e.clientHeight >= e.scrollHeight
+          (this.getClassName() === 'ellipsis' &&
+            e &&
+            e.clientHeight >= e.scrollHeight) ||
+          this.getClassName() === 'noWrap'
         ) {
           return;
         }
