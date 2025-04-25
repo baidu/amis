0714032030e0b5486036660cc10077bd9f36cbbb
yangxu23 <yangxu23@baidu.com>
2025-04-25 15:44:27 +0800

    feat: inputexcel组件支持多选，支持上传多个excel文件并解析
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析
    
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析
    
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析
    
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析
    
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析
    
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析
    
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析
    
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析
    
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析
    
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析
    
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析
    
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析
    
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析
    
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析
    
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析
    
    [amis-saas-22106]-inputexcel组件支持多选，同时上传多个excel并解析

--- packages/amis/__tests__/renderers/Form/mobileNestSelect.test.tsx
+++ packages/amis/__tests__/renderers/Form/mobileNestSelect.test.tsx
@@ -21,8 +21,8 @@ test('Renderer:mobile nested select', async () => {
     amisRender(
       {
         type: 'form',
-        api: '/api/mock2/form/saveForm',
-        body: [
+        api: '/api/xxx',
+        controls: [
           {
             type: 'nested-select',
             name: 'nestedSelect',
