8205107a3736683f53f4bff371bd348d6d085f35
2betop <2698393+2betop@users.noreply.github.com>
2025-09-16 19:50:31 +0800

    feat: OptionAutoFill 支持外部传 formula 对象

--- packages/amis-editor/src/component/OptionAutoFill.tsx
+++ packages/amis-editor/src/component/OptionAutoFill.tsx
@@ -10,6 +10,7 @@ interface OptionAutoFillProps extends RendererProps {
   // 编辑器里面的东西，用来获取上下文数据
   manager: any;
   node: any;
+  formula: any;
 }
 
 export function OptionAutoFill(props: OptionAutoFillProps) {
@@ -21,39 +22,42 @@ export function OptionAutoFill(props: OptionAutoFillProps) {
     node,
     manager,
     appLocale,
-    appCorpusData
+    appCorpusData,
+    formula: propFormula
   } = props;
   const [variableSchema, setVariableSchema] = useState<any>({
     type: 'object',
     properties: {}
   });
   const formula: any = React.useMemo(() => {
-    return {
-      variables: async () => {
-        const variables: Array<any> = await getVariables({
-          props: {node, manager},
-          appLocale,
-          appCorpusData
-        });
-        // todo 根据接口返回的数据生成
-        return [
-          {
-            label: '选项内数据(请根据实际数据自行填写)',
-            children: [
-              {
-                label: 'Label',
-                value: 'label'
-              },
-              {
-                label: 'Value',
-                value: 'value'
-              }
-            ]
-          }
-        ].concat(variables);
+    return (
+      propFormula ?? {
+        variables: async () => {
+          const variables: Array<any> = await getVariables({
+            props: {node, manager},
+            appLocale,
+            appCorpusData
+          });
+          // todo 根据接口返回的数据生成
+          return [
+            {
+              label: '选项内数据(请根据实际数据自行填写)',
+              children: [
+                {
+                  label: 'Label',
+                  value: 'label'
+                },
+                {
+                  label: 'Value',
+                  value: 'value'
+                }
+              ]
+            }
+          ].concat(variables);
+        }
       }
-    };
-  }, [manager, node]);
+    );
+  }, [manager, node, propFormula]);
 
   React.useEffect(() => {
     manager?.getContextSchemas(node, true).then((schemas: any) =>
