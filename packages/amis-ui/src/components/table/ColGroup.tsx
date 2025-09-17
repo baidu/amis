6b53e5b51496853531ee0d12cd991c83d34eb1ef
2betop <2698393+2betop@users.noreply.github.com>
2025-09-17 17:25:41 +0800

    style: 优化 table2 的 fixed 样式解决对不齐的问题

--- packages/amis-ui/src/components/table/ColGroup.tsx
+++ packages/amis-ui/src/components/table/ColGroup.tsx
@@ -20,7 +20,7 @@ export default function ColGroup({
 }: {
   columns: Array<ColumnProps>;
   colWidths: {
-    [key: string]: {
+    [key: string | number]: {
       width: number;
       realWidth: number;
       minWidth: number;
@@ -44,29 +44,43 @@ export default function ColGroup({
   const domRef = React.createRef<HTMLTableColElement>();
 
   React.useEffect(() => {
-    if (domRef.current) {
-      initTableWidth?.();
-      syncTableWidth?.();
-    }
-  }, []);
+    initTableWidth?.();
 
-  React.useEffect(() => {
     if (!syncTableWidth) {
       return;
     }
     const table = domRef.current!.parentElement!;
-    const observer = new MutationObserver(() => {
+    let trs: Array<HTMLElement> = [];
+
+    function reConnect() {
+      // 整体监听不准，因为整体可能不会宽度变化
+      // 监控 thead 下面所有的 th 的 resize 变化
+      // 如果变化了，需要重新更新表格宽度计算
+      const doms: Array<HTMLElement> = [].slice.call(
+        table.querySelectorAll(':scope > tbody > tr:first-child > *')
+      );
+
+      // 先看 th 本身有没有变化，如果没变化，就不要重新监听了
+      if (doms.some((d, index) => trs[index] !== d)) {
+        observer.disconnect();
+        trs = doms;
+        doms.forEach((dom: any) => {
+          observer.observe(dom);
+        });
+      }
+    }
+
+    const observer = new ResizeObserver(() => {
+      reConnect();
       syncTableWidth();
     });
-    observer.observe(table, {
-      attributes: true,
-      childList: true,
-      subtree: true
-    });
+
+    reConnect();
+    syncTableWidth();
     return () => {
       observer.disconnect();
     };
-  }, []);
+  }, [columns.length]);
 
   return (
     <colgroup ref={domRef}>
