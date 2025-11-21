74f31be9eb74bf7dbf616cb8d83b60653bd4c368
2betop <2698393+2betop@users.noreply.github.com>
2025-11-21 05:48:26 +0000

    Deploying to gh-pages from  @ 8fc2555ca6fc2f116d6924b74c0e27a18ac8a3ee 🚀

---
+++ docs/zh-CN/components/index_424af73.js
@@ -0,0 +1,20 @@
+amis.define('docs/zh-CN/components/index.md', function(require, exports, module, define) {
+
+  module.exports = {
+    "title": "组件介绍",
+    "description": null,
+    "type": 0,
+    "group": "⚙ 组件",
+    "menuName": "组件介绍",
+    "icon": null,
+    "order": 21,
+    "html": "<div class=\"markdown-body\"><p>从这个章节开始，我们将会介绍 amis 中内置的所有组件的使用方法</p>\n</div>",
+    "toc": {
+      "label": "目录",
+      "type": "toc",
+      "children": [],
+      "level": 0
+    }
+  };
+
+});
